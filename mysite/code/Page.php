31f76b64cd01fd3afcd1cb06225937f4aa097436
Guillaume <Guillaume@intern.deskall.ch>
2018-02-14 13:47:40 +0100

    theme primaklima

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -4,15 +4,27 @@ use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\Assets\Folder;
 use SilverStripe\Assets\File;
 use SilverStripe\ORM\DataObject;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\FieldList;
 
 class Page extends SiteTree
 {
-    private static $db = [];
+    private static $db = [
+        'ShowInMainMenu' => 'Boolean(1)'
+    ];
 
     private static $has_one = [];
 
     public function ThemeDir(){
     	return ThemeResourceLoader::inst()->getThemePaths(SSViewer::get_themes())[0];
+    }
+
+    public function getSettingsFields(){
+        $fields = parent::getSettingsFields();
+        $field = CheckboxField::create('ShowInMainMenu','Diese Seite im Hauptmenu anzeigen?');
+        $fields->insertAfter($field,'ShowInMenus');
+
+        return $fields;
     }
 
 
@@ -32,29 +44,31 @@ class Page extends SiteTree
     }
 
     public function onBeforeWrite(){
-    	
-    	$changedFields = $this->getChangedFields();
-    	//Update Folder Name
-    	if ($this->isChanged('URLSegment') && ($changedFields['URLSegment']['before'] != $changedFields['URLSegment']['after'])){
-    		$oldFolderPath = ($this->ParentID > 0 ) ? $this->Parent()->generateFolderName()."/".$changedFields['URLSegment']['before'] : "Uploads/".$changedFields['URLSegment']['before'];
-    		$newFolder = Folder::find_or_make($oldFolderPath);
-    		$newFolder->Name = $changedFields['URLSegment']['after'];
-    		$newFolder->Title = $changedFields['URLSegment']['after'];
-    		$newFolder->write();
-    	}
-    	//Update Folder Structure
-    	if($this->isChanged('ParentID')){
-    		$oldParent = ($changedFields['ParentID']['before'] == 0) ? null : DataObject::get_by_id(SiteTree::class,$changedFields['ParentID']['before']);
-    		$oldFolderPath = ($oldParent) ? $oldParent->generateFolderName()."/".$this->URLSegment : "Uploads/".$this->URLSegment;
-    		$oldFolder = Folder::find_or_make($oldFolderPath);
+    	if ($this->ID > 0){
+            $changedFields = $this->getChangedFields();
+            //Update Folder Name
+            if ($this->isChanged('URLSegment') && ($changedFields['URLSegment']['before'] != $changedFields['URLSegment']['after'])){
+                $oldFolderPath = ($this->ParentID > 0 ) ? $this->Parent()->generateFolderName()."/".$changedFields['URLSegment']['before'] : "Uploads/".$changedFields['URLSegment']['before'];
+                $newFolder = Folder::find_or_make($oldFolderPath);
+                $newFolder->Name = $changedFields['URLSegment']['after'];
+                $newFolder->Title = $changedFields['URLSegment']['after'];
+                $newFolder->write();
+            }
+            //Update Folder Structure
+            if($this->isChanged('ParentID')){
+                $oldParent = ($changedFields['ParentID']['before'] == 0) ? null : DataObject::get_by_id(SiteTree::class,$changedFields['ParentID']['before']);
+                $oldFolderPath = ($oldParent) ? $oldParent->generateFolderName()."/".$this->URLSegment : "Uploads/".$this->URLSegment;
+                $oldFolder = Folder::find_or_make($oldFolderPath);
 
-    		$newParent = ($changedFields['ParentID']['after'] == 0) ? null : DataObject::get_by_id(SiteTree::class,$changedFields['ParentID']['after']);
-    		$newParentFolderPath = ($newParent) ? $newParent->generateFolderName() : "Uploads";
-    		$newParentFolder = Folder::find_or_make($newParentFolderPath);
-    		
-    		$oldFolder->ParentID = $newParentFolder->ID;
-    		$oldFolder->write();
-    	}
+                $newParent = ($changedFields['ParentID']['after'] == 0) ? null : DataObject::get_by_id(SiteTree::class,$changedFields['ParentID']['after']);
+                $newParentFolderPath = ($newParent) ? $newParent->generateFolderName() : "Uploads";
+                $newParentFolder = Folder::find_or_make($newParentFolderPath);
+                
+                $oldFolder->ParentID = $newParentFolder->ID;
+                $oldFolder->write();
+            }
+        }
+    	
     	parent::onBeforeWrite();
     }
 }
--- themes/primaklima/templates/Includes/Header.ss
+++ themes/primaklima/templates/Includes/Header.ss
@@ -1,22 +1,45 @@
-<nav class="uk-navbar-container uk-navbar" uk-navbar>
-	<div class="uk-navbar-left">
-		<a href="" class="uk-navbar-item uk-logo"><img src="$ThemeDir/img/logo.png" alt="$SiteConfig.Title Logo" title="Home" /></a>
+<header class="dk-background-header">
+	<div class="uk-container uk-container-medium">
+		<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+			<div class="uk-navbar-right uk-visible@m">
+			    <ul class="uk-subnav uk-padding-small uk-margin-remove">
+			    	<% loop Menu(1).filter('ShowInMainMenu',0) %>
+			    	<li class="$LinkingMode <% if LinkingMode == "current" %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a>
+			    		<% if Children %>
+			    		<div class="uk-navbar-dropdown uk-margin-remove">
+		                    <ul class="uk-nav uk-navbar-dropdown-nav">
+		                    	<% loop Children %>
+		                    	  <li class="$LinkingMode <% if Current %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a></li>
+		                    	<% end_loop %>
+		                    </ul>
+		                </div>
+		                <% end_if %>
+		            </li>
+			    	<% end_loop %>
+			    </ul>
+			</div>
+		</nav>
+		<nav class="uk-navbar-container uk-navbar-transparent" uk-navbar>
+			<div class="uk-navbar-left">
+				<a href="" class="uk-navbar-item uk-logo"><img src="$ThemeDir/img/logo.png" alt="$SiteConfig.Title Logo" title="Home" /></a>
+			</div>
+			<div class="uk-navbar-right uk-visible@m">
+			    <ul class="uk-navbar-nav">
+			    	<% loop Menu(1).filter('ShowInMainMenu',1) %>
+			    	<li class="$LinkingMode <% if LinkingMode == "current" %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a>
+			    		<% if Children %>
+			    		<div class="uk-navbar-dropdown uk-margin-remove">
+		                    <ul class="uk-nav uk-navbar-dropdown-nav">
+		                    	<% loop Children %>
+		                    	  <li class="$LinkingMode <% if Current %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a></li>
+		                    	<% end_loop %>
+		                    </ul>
+		                </div>
+		                <% end_if %>
+		            </li>
+			    	<% end_loop %>
+			    </ul>
+			</div>
+		</nav>
 	</div>
-	<div class="uk-navbar-right uk-visible@m">
-	    <ul class="uk-navbar-nav">
-	    	<% loop Menu(1) %>
-	    	<li class="$LinkingMode <% if LinkingMode == "current" %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a>
-	    		<% if Children %>
-	    		<div class="uk-navbar-dropdown uk-margin-remove">
-                    <ul class="uk-nav uk-navbar-dropdown-nav">
-                    	<% loop Children %>
-                    	  <li class="$LinkingMode <% if Current %>uk-active<% end_if %>"><a href="$Link" title="$Title.XML">$MenuTitle.XML</a></li>
-                    	<% end_loop %>
-                    </ul>
-                </div>
-                <% end_if %>
-            </li>
-	    	<% end_loop %>
-	    </ul>
-	</div>
-</nav>
+</header>
