3677c2db44abdbe23739fa93a21923f3c77dc41c
Guillaume <Guillaume@intern.deskall.ch>
2018-02-27 15:37:44 +0100

    s

--- deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
+++ deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
@@ -3,6 +3,7 @@
 
 class UserDefinedFormControllerExtension extends Extension
 {
+	private static $finished_anchor = '#anfrage-gesendet';
 
 	public function updateEmail($email, $recipient, $emailData){
 
