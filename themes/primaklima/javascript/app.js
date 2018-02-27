a98adc333d920880b7609c5ddc9bf088fb59ce46
Guillaume <Guillaume@intern.deskall.ch>
2018-02-27 11:29:05 +0100

    s

--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -1,8 +1,5 @@
 $('.uk-icon').each(function(){
 	var size = $(this).css("font-size"); 
 	console.log(size);
-	UIkit.icon('.uk-icon',{
-		height: size,
-		width: size
-	});
+	UIkit.icon('.uk-icon').svg.then(function(svg) { svg.setAttribute('height',size).setAttribute('width',size); })
 });
