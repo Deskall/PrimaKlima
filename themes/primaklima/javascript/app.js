2477fa6291f06a8ad52c10a9f8571491c31b64de
YoupiXxl <guillaume.pacilly@gmail.com>
2018-02-27 18:29:22 +0100

    f

--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 //Recaptcha validation
 $(document).ready(function(){
 	if ($(".g-recaptcha").length > 0){
-		$('<script src="https://www.google.com/recaptcha/api.js" async defer></script>').appendTo($("head"));
+		//$('<script src="https://www.google.com/recaptcha/api.js" async defer></script>').appendTo($("head"));
 	}
 	$(".g-recaptcha").on("click",function(event){
 		event.preventDefault();
