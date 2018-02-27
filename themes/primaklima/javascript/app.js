d85ffbd7bf2cab71f76d0eebe1aa9daab9a2caf1
Guillaume <Guillaume@intern.deskall.ch>
2018-02-27 11:38:50 +0100

    dsa

--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -1,7 +1,7 @@
 $('.uk-icon').each(function(){
 	var size = $(this).css("font-size"); 
 	console.log(size);
-	var svg = $(this).find('svg');
+	var svg = $(this).find('svg')[0];
 	console.log(svg);
-	svg.setAttribute('height',size).setAttribute('width',size);
+	$(svg).setAttribute('height',size).setAttribute('width',size);
 });
