bb851140cb77839f974ae7dfd29525eaaf8e23d3
Guillaume <Guillaume@intern.deskall.ch>
2018-02-27 11:44:47 +0100

    dsa

--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -1,7 +1,11 @@
-// $('.uk-icon').each(function(){
-// 	var size = $(this).css("font-size"); 
-// 	console.log(size);
-// 	var svg = $(this).find('svg')[0];
-// 	console.log(svg);
-// 	$(svg).setAttribute('height',size).setAttribute('width',size);
-// });
+$('.dk-responsive-icon').each(function(){
+	var size = $(this).css("font-size"); 
+	console.log(size);
+	var svg = $(this).find('svg')[0];
+	console.log(svg);
+	if (svg){
+		$(svg).setAttribute('height',size);
+		$(svg).setAttribute('width',size);
+	}
+	
+});
