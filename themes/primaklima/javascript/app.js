ce6bc7a7a70c9eb59da91738c820c353dd4785ba
Guillaume <Guillaume@intern.deskall.ch>
2018-02-27 17:50:42 +0100

    test

--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
 		dateFormat: "d.m.Y"
 	});
 });
+
+//Recaptcha validation
+$(document).ready(function(){
+	if ($(".g-recaptcha").length > 0){
+		$('<script src="https://www.google.com/recaptcha/api.js" async defer></script>').appendTo($("head"));
+	}
+	$(".g-recaptcha").on("click",function(event){
+		event.preventDefault();
+		grecaptcha.execute();
+	});
+
+	function onSubmit(token) {
+	    alert('thanks ');
+	}
+});
