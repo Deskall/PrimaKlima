4126ad374792f63e022bb3e9822fb8d3b8e3ff16
Guillaume <Guillaume@intern.deskall.ch>
2018-02-28 11:03:04 +0100

    s

--- deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
+++ deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
@@ -7,18 +7,13 @@ class UserDefinedFormControllerExtension extends Extension
 {
 	private static $finished_anchor = '';
 
+
+	public function updateEmailData($emailData, $attachments){
+		$emailData['AbsoluteThemeDir'] = Director::protocolAndHost()."/".singleton('Page')->ThemeDir();
+		$emailData['AbsoluteBaseUrl'] = Director::AbsoluteBaseUrl();
+	}
+
 	public function updateEmail($email, $recipient, $emailData){
 		$email->setHTMLTemplate('email/base_email');
-		//to do plain template
-
-                  
-		$AbsoluteThemeDir = Director::protocolAndHost()."/".singleton('Page')->ThemeDir();
-		$emailData['AbsoluteThemeDir'] = $AbsoluteThemeDir;
-		$emailData['AbsoluteBaseUrl'] = Director::AbsoluteBaseUrl();
-  			
-  			ob_start();
-                    print_r($emailData);
-                    $result = ob_get_clean();
-                    file_put_contents('log.txt',$result);
 	}
 }
