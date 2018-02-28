972ed64035811687275696b75e9250b7e92866b9
YoupiXxl <guillaume.pacilly@gmail.com>
2018-02-28 07:50:28 +0100

    map block

--- deskall-page-blocks/src/Model/MapBlock.php
+++ deskall-page-blocks/src/Model/MapBlock.php
@@ -2,9 +2,14 @@
 
 use SilverStripe\Forms\HTMLEditor\HtmlEditorField;
 use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\LiteralField;
 use SilverStripe\ORM\FieldType\DBField;
 use DNADesign\Elemental\Models\BaseElement;
 use SilverStripe\Assets\Image;
+use SilverStripe\ORM\ArrayList;
 
 class MapBlock extends BaseElement
 {
@@ -16,8 +21,27 @@ class MapBlock extends BaseElement
 
     private static $db = [
         'HTML' => 'HTMLText',
-        'Adresse' => 'Varchar(255)'
+        'Adresse' => 'Varchar(255)',
+        'Height' => 'Varchar(255)',
+        'Styles' => 'Text',
+        'disableDefaultUI' => 'Boolean(0)',
+        'mapTypeControl' => 'Boolean(1)',
+        'mapTypeControlOptions' => 'Text',
+        'ZoomControl' => 'Boolean(1)',
+        'ZoomControlOptions' => 'Text',
+        'streetViewControl' => 'Boolean(1)',
+        'streetViewControlOptions' => 'Text',
+        'scaleControl' => 'Boolean(1)',
+        'fullscreenControl' => 'Boolean(1)',
+
     ];
+
+    private static $block_heights = [
+        'uk-height-small' => 'klein',
+        'uk-height-medium' => 'medium',
+        'uk-height-large' => 'gross'
+    ];
+
 
    
     private static $table_name = 'MapBlock';
@@ -34,13 +58,33 @@ class MapBlock extends BaseElement
     {
 
         $this->beforeUpdateCMSFields(function ($fields) {
+            
             $fields
                 ->fieldByName('Root.Main.HTML')
                 ->setTitle(_t(__CLASS__ . '.ContentLabel', 'Content'));
-            $fields->addFieldToTab('Root.Settings',LayoutField::create('Layout','Format', self::$block_layouts));
+                
+            $fields->addFieldToTab('Root.Main',TextField::create('Adresse','Adresse'),'HTML');
+            $fields->addFieldToTab('Root.Settings',LayoutField::create('Height','Format', self::$block_heights));
 
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('disableDefaultUI',_t(__CLASS__.'disableControls','Kontrol deaktivieren')));
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('mapTypeControl',_t(__CLASS__.'mapControls','Map Kontrol'))->displayIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', TextareaField::create('mapTypeControlOptions',_t(__CLASS__.'mapControlOptions','Map Kontrol Optionen'))->displayIf('mapTypeControl')->isChecked()->andIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('ZoomControl',_t(__CLASS__.'zoomControls','Zoom Kontrol'))->displayIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', TextareaField::create('ZoomControlOptions',_t(__CLASS__.'zoomControlOptions','Zoom Kontrol Optionen'))->displayIf('ZoomControl')->isChecked()->andIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('streetViewControl',_t(__CLASS__.'streetViewControls','StreetView Kontrol'))->displayIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', TextareaField::create('streetViewControlOptions',_t(__CLASS__.'streetViewControlOptions','StreetView Kontrol Optionen'))->displayIf('streetViewControl')->isChecked()->andIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('scaleControl',_t(__CLASS__.'scaleControls','Scale Kontrol'))->displayIf('disableDefaultUI')->isNotChecked()->end());
+            $fields->addFieldToTab('Root.MapOptions', CheckboxField::create('fullscreenControl',_t(__CLASS__.'fullscreenControls','Fullscreen Kontrol'))->displayIf('disableDefaultUI')->isNotChecked()->end());
+            
+
+            $fields->addFieldToTab('Root.MapOptions',LiteralField::create('Styles generieren','<a href="https://mapstyle.withgoogle.com/" target="_blank">'._t(__CLASS__.'LinkToStyle','Styles generieren').'</a>'));
+            $fields->addFieldToTab('Root.MapOptions',TextareaField::create('Styles','Google Map Styles'));
         });
-        return parent::getCMSFields();
+        $fields = parent::getCMSFields();
+        $fields->removeByName('Layout');
+        $fields->removeByName('Background');
+        $fields->removeByName('BackgroundImage');
+        return $fields;
     }
 
     public function getSummary()
@@ -51,6 +95,40 @@ class MapBlock extends BaseElement
     public function getType()
     {
         return _t(__CLASS__ . '.BlockType', 'Map');
+    }
+
+    public function NiceStyles(){
+        return preg_replace( "/\r|\n/", "", $this->Styles );
+    }
+
+    public function getMapOptions(){
+        $options = [];
+        $options['zoom'] = 15;
+        $options['disableDefaultUI'] = ($this->disableDefaultUI) ? true : false;
+        if(!$this->disableDefaultUI){
+            $options['mapTypeControl'] = ($this->mapTypeControl) ? true : false;
+            if ($this->mapTypeControlOptions){
+                $options['mapTypeControlOptions'] = json_decode(preg_replace( "/\r|\n/", "", $this->mapTypeControlOptions ));
+            }
+            
+            $options['zoomControl'] = ($this->ZoomControl) ? true : false;
+            if ($this->ZoomControlOptions){
+                $options['zoomControlOptions'] = json_decode(preg_replace( "/\r|\n/", "", $this->zoomControlOptions ));
+            }
+            
+            $options['streetViewControl'] = ($this->streetViewControl) ? true : false;
+            if ($this->streetViewControlOptions){
+                $options['streetViewControlOptions'] = json_decode(preg_replace( "/\r|\n/", "", $this->streetViewControlOptions ));
+            }
+
+            $options['scaleControl'] = ($this->scaleControl) ? true : false;
+            $options['fullscreenControl'] = ($this->fullscreenControl) ? true : false;
+        }
+
+        if($this->Styles){
+            $options['styles'] = json_decode($this->NiceStyles());
+        }
+        return json_encode($options);
     }
 
 }
--- themes/primaklima/javascript/app.js
+++ themes/primaklima/javascript/app.js
@@ -18,3 +18,43 @@ $(document).ready(function(){
 // 	    alert('thanks ');
 // 	}
 // });
+
+//Google Maps
+	function loadmap() {
+      $('[data-google-map]').each(function(){
+            initGmaps('googlemap_'+$(this).attr('data-google-map'), $(this).attr('data-google-map-address'), $(this).attr('data-google-map-options'), $(this).html());
+            $(this).fadeIn();
+        });
+    }
+
+	function initGmaps(id, address,options, label){
+
+        var geocoder = new google.maps.Geocoder();
+        var latlng = new google.maps.LatLng(0,0);
+        options = $.parseJSON(options);
+        console.log(options);
+        var map = new google.maps.Map(
+            document.getElementById(id),
+            options
+        );
+
+        geocoder.geocode( { 'address': address}, function(results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              map.setCenter(results[0].geometry.location);
+            } else {
+              alert('Geocode was not successful for the following reason: ' + status);
+            }
+
+            var infowindow = new google.maps.InfoWindow({
+              content: label,
+              position: results[0].geometry.location
+            });
+
+            infowindow.open(map);
+        });
+    }
+	$(document).ready(function(){
+	    if ($(".google-map").length > 0){
+	 		$('<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCbrDquBmMxiRMZz6itPir8xKX7HLa7xZE&callback=loadmap"></script>').appendTo($("body"));
+	 	}
+	});
