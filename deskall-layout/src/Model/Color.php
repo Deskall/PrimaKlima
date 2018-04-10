21235f419e1a875b08f4ee8a8cd0d40c69a25033
Guillaume <Guillaume@intern.deskall.ch>
2018-04-10 11:43:39 +0200

    s

--- deskall-layout/src/Model/Color.php
+++ deskall-layout/src/Model/Color.php
@@ -75,7 +75,7 @@ class Color extends DataObject{
 
     public function getHTMLOption(){
         $html = '<div class="option-html">
-            <p style="background-color:'.$this->Color.';color:'.$this->FontColor.';">'.$this->FontTitle.'</p>
+            <p style="background-color:#'.$this->Color.';color:#'.$this->FontColor.';">'.$this->FontTitle.'</p>
           </div>';
         return $html;
     }
