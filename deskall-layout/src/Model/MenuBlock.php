8a268faae2c62d5541488f849388131105012400
Guillaume <Guillaume@intern.deskall.ch>
2018-04-10 14:34:10 +0200

    t

--- deskall-layout/src/Model/MenuBlock.php
+++ deskall-layout/src/Model/MenuBlock.php
@@ -80,7 +80,7 @@ class MenuBlock extends LayoutBlock{
 		$fields->insertAfter(CompositeField::create(
 			CheckboxField::create('UseMenu',_t(__CLASS__.'.UseMenu','Site Struktur benutzen'))->displayIf('Type')->isEqualTo('links')->end(),
 			DropdownField::create('UseMenuOption',_t(__CLASS__.'.UseMenu','Welche Menu benutzen'),$this->getTranslatedSourceFor(__CLASS__,'menu_options'))->displayIf('UseMenu')->isChecked()->end(),
-			CheckboxField::create('ShowSubLevels',_t(__CLASS__.'.ShowSubLevels','Untenmenu anzeigen'))->displayIf('UseMenu')->isChecked()->end(),
+			CheckboxField::create('ShowSubLevels',_t(__CLASS__.'.ShowSubLevels','Untenmenu anzeigen'))->displayIf('UseMenu')->isChecked()->andIf('UseMenuOption')->isEqualTo('main')->end(),
 			NumericField::create('ShowSubLevelsBis',_t(__CLASS__.'.ShowSubLevelsBis','Navigation Stufen'))->displayIf('ShowSubLevels')->isChecked()->end()
 		)->setTitle(_t(__CLASS__.'.MenuSettings','Menu Einstellungen'))->setName('MenuSettings'),'Width');
 
