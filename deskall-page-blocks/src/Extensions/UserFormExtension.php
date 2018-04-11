61ef64d627ca0d7b46fbc0450f801435ce5e5541
Guillaume <Guillaume@intern.deskall.ch>
2018-04-11 11:35:58 +0200

    h

--- deskall-page-blocks/src/Extensions/UserFormExtension.php
+++ deskall-page-blocks/src/Extensions/UserFormExtension.php
@@ -27,9 +27,16 @@ class UserFormExtension extends DataExtension
    private static $controller_class = ElementFormControllerExtension::class;
 
    public function updateCMSFields(FieldList $fields){
+    $fields->removeByName('Layout');
 
      $fields->addFieldToTab('Root.FormOptions',CheckboxField::create('hasCaptcha', _t(__CLASS__.'.WITHCAPTCHA', 'mit Google recaptcha Prüfung?')));
      $fields->fieldByName('Root.Main.RedirectPageID')->setTitle(_t(__CLASS__.'.RedirectPage', 'erfolgreiche Einreichungsseite'));
+     if ($this->owner->ID == 0){ 
+      $fields->removeByName('FormFields');
+      $fields->removeByName('Submissions');
+      $fields->removeByName('FormOptions');
+      $fields->removeByName('Recipients');
+     }
    
    }
 
