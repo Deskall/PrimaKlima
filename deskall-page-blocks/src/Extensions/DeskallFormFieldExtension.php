73822237cc5ef65e88117a2e9ba56d46d5572ee8
Guillaume <Guillaume@intern.deskall.ch>
2018-04-11 14:06:41 +0200

    s

--- deskall-page-blocks/src/Extensions/DeskallFormFieldExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallFormFieldExtension.php
@@ -5,7 +5,7 @@ use SilverStripe\ORM\DataExtension;
 
 class DeskallFormFieldExtension extends DataExtension{
 	public function ShowLabels(){
-		print_r($this->owner->form->Controller->data()->record['ShowLabels']);
+		print_r($this->owner->form->record['ShowLabels']);
 		return $this->owner->form->Controller->data()->record['ShowLabels'];
 	}
 }
