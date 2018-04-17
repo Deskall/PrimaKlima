67dbbba7a7aa4117c88005d102fc44a43d7b090a
Guillaume <Guillaume@intern.deskall.ch>
2018-04-17 13:08:14 +0200

    s

--- deskall-page-blocks/src/Model/VirtualBlock.php
+++ deskall-page-blocks/src/Model/VirtualBlock.php
@@ -5,7 +5,7 @@ use DNADesign\ElementalVirtual\Model\ElementVirtual;
 class VirtualBlock extends ElementVirtual{
 	private static $icon = 'font-icon-link';
 
-    private static $description = 'Verknüpfen Sie einen bestehenden Block.'
+    private static $description = 'Verknüpfen Sie einen bestehenden Block.';
 
     public function getType()
     {
