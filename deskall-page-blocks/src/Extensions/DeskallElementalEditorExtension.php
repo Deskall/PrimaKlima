54edef6e7bd5d8c79811b447852de335308c789c
Guillaume <Guillaume@intern.deskall.ch>
2018-04-17 15:28:13 +0200

    s

--- deskall-page-blocks/src/Extensions/DeskallElementalEditorExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallElementalEditorExtension.php
@@ -40,7 +40,7 @@ class DeskallElementalEditorExtension extends DataExtension
         $gridfield->getConfig()->removeComponentsByType(GridFieldDeleteAction::class)
             ->addComponent($autocomplete = new DeskallGridFieldAddExistingBlockAutocompleter('before'))
             ->addComponent(new ElementalGridFieldDeleteAction());
-        $autocomplete->setSearchFields(['NiceTitle']);
+        $autocomplete->setSearchFields(['Title','Parent.Title']);
         if ($gridfield->name == "Elements"){
             $gridfield->getConfig()->addComponent(new GridFieldBlockOrderAction());
         }
