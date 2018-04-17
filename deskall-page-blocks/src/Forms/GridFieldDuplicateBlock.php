66c1217df140ee1bb49eaea074d94675db362f16
Guillaume <Guillaume@intern.deskall.ch>
2018-04-17 13:05:56 +0200

    w

--- deskall-page-blocks/src/Forms/GridFieldDuplicateBlock.php
+++ deskall-page-blocks/src/Forms/GridFieldDuplicateBlock.php
@@ -142,7 +142,7 @@ class GridFieldDuplicateBlock implements GridField_HTMLProvider, GridField_URLHa
 		Requirements::javascript('deskall-page-blocks/javascript/gridfieldduplicateblock.js');
 
 		$blockfield = GroupedDropdownField::create('Block', '', $this->getBlockTree());
-		$blockfield->addExtraClass('no-change-track hidden');
+		$blockfield->addExtraClass('no-change-track dk-hidden');
 
 		$data = new ArrayData(array(
 			'Title'      => $this->getTitle(),
