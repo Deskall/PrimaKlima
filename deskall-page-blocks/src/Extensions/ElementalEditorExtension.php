50c0281c8de635c9dd987a7880cec01559f5796a
Guillaume <Guillaume@intern.deskall.ch>
2018-04-17 11:26:39 +0200

    d

--- deskall-page-blocks/src/Extensions/ElementalEditorExtension.php
+++ deskall-page-blocks/src/Extensions/ElementalEditorExtension.php
@@ -2,7 +2,7 @@
 
 use SilverStripe\ORM\DataExtension;
 use Symbiote\GridFieldExtensions\GridFieldAddNewMultiClass;
-
+use DNADesign\ElementalVirtual\Forms\ElementalGridFieldAddExistingAutocompleter;
 
 class ElementalEditorExtension extends DataExtension 
 {
@@ -30,7 +30,7 @@ class ElementalEditorExtension extends DataExtension
         }
     	$gridfield->getConfig()->removeComponentsByType(GridFieldAddNewMultiClass::class)
         ->addComponent(new DeskallGridFieldAddNewMultiClass());
-        $gridfield->getConfig()->getComponentByType(DeskallGridFieldAddNewMultiClass::class)->setClasses($types);
+        $gridfield->getConfig()->getComponentByType(DeskallGridFieldAddNewMultiClass::class)->setClasses($types)->removeComponentsByType(ElementalGridFieldAddExistingAutocompleter::class);;
         $gridfield->getConfig()->addComponent(new GridFieldShowHideAction());
         if ($this->owner->getArea()->getOwnerPage()->ClassName == "ParentBlock" && $this->owner->getArea()->getOwnerPage()->CollapsableChildren){
             $gridfield->getConfig()->addComponent(new GridFieldCollapseUncollapseAction());
--- deskall-page-blocks/src/Forms/DeskallGridFieldAddNewMultiClass.php
+++ deskall-page-blocks/src/Forms/DeskallGridFieldAddNewMultiClass.php
@@ -14,7 +14,6 @@ use SilverStripe\Forms\GridField\GridField_URLHandler;
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
 use SilverStripe\View\ArrayData;
 use Symbiote\GridFieldExtensions\GridFieldExtensions;
-use DNADesign\ElementalVirtual\Forms\ElementalGridFieldAddExistingAutocompleter;
 
 /**
  * A component which lets the user select from a list of classes to create a new record form.
@@ -207,9 +206,6 @@ class DeskallGridFieldAddNewMultiClass implements GridField_HTMLProvider, GridFi
         $classes   = $this->getClasses($grid);
         /** @var GridFieldDetailForm $component */
         $component = $grid->getConfig()->getComponentByType(GridFieldDetailForm::class);
-
-        /*** Remove virtual block existing search field **/
-        $grid->getConfig()->removeComponentsByType(ElementalGridFieldAddExistingAutocompleter::class);
 
         if (!$component) {
             throw new Exception('The add new multi class component requires the detail form component.');
