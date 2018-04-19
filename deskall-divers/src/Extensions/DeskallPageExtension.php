04117e63f21468c083849e1efdbd6dd965d9257e
Guillaume <Guillaume@intern.deskall.ch>
2018-04-19 15:14:51 +0200

    w

--- deskall-divers/src/Extensions/DeskallPageExtension.php
+++ deskall-divers/src/Extensions/DeskallPageExtension.php
@@ -32,7 +32,7 @@ class DeskallPageExtension extends DataExtension
     public function updateCMSFields(FieldList $fields){
         if ($this->owner->ShowInMenus){
             $field = OptionsetField::create('ShowInMainMenu','In welchem Menu sollt diese Seite anzeigen ?', $this->owner->getTranslatedSourceFor(__CLASS__,'menu_level'));
-            $fields->insertAfter($field,'MenuTitle');
+            $fields->insertBefore($field,'BlockTitle');
 
         }
     }
