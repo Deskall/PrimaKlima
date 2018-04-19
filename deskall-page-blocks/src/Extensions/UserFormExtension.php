ba4bd2e7ebd206cda446369de86000574b7f5ab3
Guillaume <Guillaume@intern.deskall.ch>
2018-04-19 13:49:53 +0200

    s

--- deskall-page-blocks/src/Extensions/UserFormExtension.php
+++ deskall-page-blocks/src/Extensions/UserFormExtension.php
@@ -50,9 +50,9 @@ class UserFormExtension extends DataExtension
      $fields->addFieldToTab('Root.FormOptions',CheckboxField::create('hasCaptcha', _t(__CLASS__.'.WITHCAPTCHA', 'mit Google recaptcha Prüfung?')));
      $fields->addFieldToTab('Root.FormOptions',CheckboxField::create('ShowLabels', _t(__CLASS__.'.ShowLabels', 'Feld Titel anzeigen?')));
      $fields->fieldByName('Root.Main.RedirectPageID')->setTitle(_t(__CLASS__.'.RedirectPage', 'erfolgreiche Einreichungsseite'));
-     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('ButtonBackground',_t(__CLASS__.'.ButtonBackground','Button Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors()));
+     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('ButtonBackground',_t(__CLASS__.'.ButtonBackground','Button Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
      $fields->addFieldToTab('Root.LayoutTab',HTMLOptionsetField::create('Layout',_t(__CLASS__.'.Layout','Layout'),$this->owner->stat('block_layouts')));
-     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('StepTitleBackground',_t(__CLASS__.'.StepTitleBackground','Hintergrundfarbe den Seite Titel'),SiteConfig::current_site_config()->getBackgroundColors()));
+     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('StepTitleBackground',_t(__CLASS__.'.StepTitleBackground','Hintergrundfarbe den Seite Titel'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
      if ($this->owner->ID == 0){ 
       $fields->removeByName('FormFields');
       $fields->removeByName('Submissions');
