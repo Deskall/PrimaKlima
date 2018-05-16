9d42452afab2eb17a648018c7442dca3371e0991
YoupiXxl <guillaume.pacilly@gmail.com>
2018-05-16 10:03:06 +0200

    ok

--- themes/standard/css/CompileLess.php
+++ themes/standard/css/CompileLess.php
@@ -12,7 +12,7 @@ if($css_compiled){
 	// set correct paths
 	$fontdir = str_replace("/css","/fonts", dirname($_SERVER['REQUEST_URI']));
 	$css_compiled = str_replace("url('/fonts","url('".$fontdir,$css_compiled);
-
+	//$css_compiled = str_replace('replace("url("\'', 'filter:url(\'', $css_minified);
 
 	// // optimize file
 	// $url = 'http://optimizer-deskall.rhcloud.com/css';
