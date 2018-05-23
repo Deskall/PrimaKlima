67c238b46d0dfa245b0d70c0a95a6d9bed7eb84a
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-23 16:52:57 +0200

    d

--- deskall-page-blocks/src/Model/VirtualBlock.php
+++ deskall-page-blocks/src/Model/VirtualBlock.php
@@ -10,10 +10,6 @@ class VirtualBlock extends ElementVirtual{
 
     private static $singular_name = 'Virtual block';
 
-    private static $controller_template = 'DefaultHolder';
-
-    private static $controller_class = BlockController::class;
-
     private static $plural_name = 'Virtual blocks';
 
     private static $description = 'Verknüpfen Sie einen bestehenden Block.';
@@ -23,19 +19,5 @@ class VirtualBlock extends ElementVirtual{
         return _t(__CLASS__ . '.BlockType', 'Block verknüpfen');
     }
 
-    public function RenderPreview(){
-    	return $this->LinkedElement()->getEditorPreview();
-    }
 
-     /**
-     * @return \SilverStripe\ORM\FieldType\DBHTMLText
-     */
-    public function getEditorPreview()
-    {
-    	print_r('ici');
-        $templates = [];
-        $templates[] = __CLASS__ . '_EditorPreview';
-
-        return $this->renderWith($templates);
-    }
 }
--- deskall-page-blocks/templates/DNADesign/ElementalVirtual/Model/ElementVirtual.ss
+++ deskall-page-blocks/templates/DNADesign/ElementalVirtual/Model/ElementVirtual.ss
@@ -1,1 +1,2 @@
 <% include Layout/BlockHolder Element=$LinkedElement %>
+ici
