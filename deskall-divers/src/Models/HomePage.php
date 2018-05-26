34b48e9554ef763629a2fa9b9c38bfb645f932f0
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 12:00:10 +0200

    s

--- deskall-divers/src/Models/HomePage.php
+++ deskall-divers/src/Models/HomePage.php
@@ -2,4 +2,10 @@
 
 class HomePage extends Page {
 	private static $icon = "home";
+
+	private static $description = "Startseite";
+
+	public function canCreate($member = null, $context = null){
+		return HomePage::get()->count() > 0;
+	}
 }
