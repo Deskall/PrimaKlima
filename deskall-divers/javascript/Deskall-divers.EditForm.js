d8d229075f77d78125e60d76391943118df5fff7
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 12:29:39 +0200

    s

---
+++ deskall-divers/javascript/Deskall-divers.EditForm.js
@@ -0,0 +1,30 @@
+/**
+ * File: Deskall-divers.EditForm.js
+ */
+import $ from 'jquery';
+import i18n from 'i18n';
+
+$.entwine('ss', function($){
+
+	$('.cms-edit-form input[name=Title]').entwine({
+		
+
+		/**
+		 * Function: updateURLSegment
+		 *
+		 * Update the URLSegment
+		 * (String) title
+		 */
+		updateURLSegment: function(title) {
+
+			var urlSegmentField = urlSegmentInput.closest('.field.urlsegment');
+			if (!urlSegmentField.hasClass("no-edit")){
+				this._super();
+			}
+			else{
+				return false;
+			}
+			
+		},
+	});
+});
