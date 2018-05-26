ed5607cce89c875a06e6066e153cf9c24452537d
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 14:20:30 +0200

    s

--- deskall-images/src/DeskallImageOptimizer.php
+++ deskall-images/src/DeskallImageOptimizer.php
@@ -9,8 +9,25 @@ class DeskallImageOptimiser {
 		// $result = ob_get_clean();
 		// file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt",$result);
 
-		\Tinify\setKey($this->api_key);
-		$source = \Tinify\fromUrl($path);
-		$source->toFile($path);
+		try {
+    		\Tinify\setKey($this->api_key);
+			$source = \Tinify\fromUrl($path);
+			$source->toFile($path);
+		} catch(\Tinify\AccountException $e) {
+		    print("account: " . $e->getMessage());
+		    // Verify your API key and account limit.
+		} catch(\Tinify\ClientException $e) {
+		    // Check your source image and request options.
+		    print("client: " . $e->getMessage());
+		} catch(\Tinify\ServerException $e) {
+		    // Temporary issue with the Tinify API.
+		     print("tiny server: " . $e->getMessage());
+		} catch(\Tinify\ConnectionException $e) {
+		    // A network connection error occurred.
+		     print("connection: " . $e->getMessage());
+		} catch(Exception $e) {
+		    // Something else went wrong, unrelated to the Tinify API.
+		     print("other: " . $e->getMessage());
+		}
 	}
 } 
