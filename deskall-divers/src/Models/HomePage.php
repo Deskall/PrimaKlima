a5086dfc84303483d9b4835c1ff92f54be9246ed
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 12:22:57 +0200

    s

--- deskall-divers/src/Models/HomePage.php
+++ deskall-divers/src/Models/HomePage.php
@@ -1,5 +1,4 @@
 <?php
-use SilverStripe\CMS\Forms\SiteTreeURLSegmentField_Readonly;
 
 class HomePage extends Page {
 	private static $icon = "home";
@@ -12,7 +11,7 @@ class HomePage extends Page {
 
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
-		$fields->replaceField('Root.Main.URLSegment',SiteTreeURLSegmentField_Readonly::create('URLSegment',_t('SiteTree.URLSegment','URL-Segment'))->performReadonlyTransformation());
+		$fields->FieldByName('Root.Main.URLSegment')->setReadonly(true)->addExtraClass("readonly");
 
 		return $fields;
 	}
