23519aed966facbd7b4a22501fb716f2d8dd701c
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 14:29:55 +0200

    s

--- deskall-images/src/DeskallImageOptimizer.php
+++ deskall-images/src/DeskallImageOptimizer.php
@@ -10,26 +10,32 @@ class DeskallImageOptimiser {
 		// file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt",$result);
 
 		file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt",$path);
-
-		try {
-    		\Tinify\setKey($this->api_key);
-			$source = \Tinify\fromUrl($path);
-			$source->toFile($path);
-		} catch(\Tinify\AccountException $e) {
-		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","account: " . $e->getMessage(), FILE_APPEND);
-		    // Verify your API key and account limit.
-		} catch(\Tinify\ClientException $e) {
-		    // Check your source image and request options.
-		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","client: " . $e->getMessage(), FILE_APPEND);
-		} catch(\Tinify\ServerException $e) {
-		    // Temporary issue with the Tinify API.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","tiny server: " . $e->getMessage(), FILE_APPEND);
-		} catch(\Tinify\ConnectionException $e) {
-		    // A network connection error occurred.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","connection: " . $e->getMessage(), FILE_APPEND);
-		} catch(Exception $e) {
-		    // Something else went wrong, unrelated to the Tinify API.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","other: " . $e->getMessage(), FILE_APPEND);
+		//Check if file exist
+		if (!file_exists($path)){
+			//if not published check protected
+			$path = str_replace("assets","assets/.protected",$path);
+		}
+		if (file_exists($path)){
+			try {
+	    		\Tinify\setKey($this->api_key);
+				$source = \Tinify\fromUrl($path);
+				$source->toFile($path);
+			} catch(\Tinify\AccountException $e) {
+			    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","account: " . $e->getMessage(), FILE_APPEND);
+			    // Verify your API key and account limit.
+			} catch(\Tinify\ClientException $e) {
+			    // Check your source image and request options.
+			    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","client: " . $e->getMessage(), FILE_APPEND);
+			} catch(\Tinify\ServerException $e) {
+			    // Temporary issue with the Tinify API.
+			     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","tiny server: " . $e->getMessage(), FILE_APPEND);
+			} catch(\Tinify\ConnectionException $e) {
+			    // A network connection error occurred.
+			     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","connection: " . $e->getMessage(), FILE_APPEND);
+			} catch(Exception $e) {
+			    // Something else went wrong, unrelated to the Tinify API.
+			     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","other: " . $e->getMessage(), FILE_APPEND);
+			}
 		}
 	}
 } 
