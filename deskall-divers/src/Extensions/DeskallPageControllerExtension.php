15208c9d4dd9c2a7b78b7dd185a19cc6e65f9a4a
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-05-26 18:30:06 +0200

    s

--- deskall-divers/src/Extensions/DeskallPageControllerExtension.php
+++ deskall-divers/src/Extensions/DeskallPageControllerExtension.php
@@ -11,11 +11,10 @@ class DeskallPageControllerExtension extends Extension
         return Director::isLive();
     }
 
-    // public function Css(){
-    // 	$css_compiled = file_get_contents(Director::baseFolder().'/'.$this->owner->ThemeDir().'/css/main.min.css');
-    // 	//$css_compiled = str_replace("url('../fonts","url('".$this->owner->ThemeDir()."/fonts",$css_compiled);
-    //     $css = new DBHTMLText();
-    //     $css->setValue(htmlspecialchars_decode('<style>'.$css_compiled.'</style>'));
-    //     return $css;
-    // }
+    public function Css(){
+    	$css_compiled = file_get_contents(Director::baseFolder().'/deskall-layout/templates/Includes/Css.ss');
+        $css = new DBHTMLText();
+        $css->setValue($css_compiled);
+        return $css;
+    }
 }
