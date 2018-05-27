439d1160b966bfdb8711767cf20f576128c18da4
YoupiXxl <guillaume.pacilly@gmail.com>
2018-05-27 21:39:39 +0200

    f

--- deskall-page-blocks/src/Extensions/BaseBlockExtension.php
+++ deskall-page-blocks/src/Extensions/BaseBlockExtension.php
@@ -211,6 +211,22 @@ class BaseBlockExtension extends DataExtension implements i18nEntityProvider, St
         parent::onBeforeWrite();
     }
 
+    /*** Loop recursively until we reach first parent page then publish it *****/
+    public function onAfterPublish(){
+        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log.txt','ici');
+        if (!$this->owner instanceof SiteTree && !$this->owner instanceof ElementalArea ){
+            if ($toStage == "Live"){
+                if ($parent = $this->owner->getPage()){
+                    while(!$parent instanceof SiteTree){
+                        $parent = $parent->getPage();
+                    }
+                    $parent->publishSingle();
+                    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log.txt','ici');
+                }
+            }    
+        }
+    }
+
     public function isChildren(){
         return $this->owner->Parent()->OwnerClassName == "ParentBlock";
     }
