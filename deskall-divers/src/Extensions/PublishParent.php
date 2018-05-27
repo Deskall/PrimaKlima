1fe2f2eefe1f0a1e713382f2d1771d3a6e2a8da6
YoupiXxl <guillaume.pacilly@gmail.com>
2018-05-27 21:26:30 +0200

    f

--- deskall-divers/src/Extensions/PublishParent.php
+++ deskall-divers/src/Extensions/PublishParent.php
@@ -8,10 +8,6 @@ use DNADesign\Elemental\Models\ElementalArea;
 class PublishParent extends DataExtension
 {
 
-    public function updateCMSFields(FieldList $fields){
-        print_r('yo');
-    }
-
     /*** Loop recursively until we reach first parent page then publish it *****/
     public function onAfterVersionedPublish($fromStage, $toStage, $createNewVersion){
                             file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log.txt','ici');
