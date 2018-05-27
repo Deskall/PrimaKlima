9a434225b662dd3029603ee6298e1ef88f942f9e
YoupiXxl <guillaume.pacilly@gmail.com>
2018-05-27 21:37:46 +0200

    z

--- deskall-divers/src/Extensions/PublishParent.php
+++ deskall-divers/src/Extensions/PublishParent.php
@@ -9,7 +9,7 @@ class PublishParent extends DataExtension
 {
 
     /*** Loop recursively until we reach first parent page then publish it *****/
-    public function onAfterVersionedPublish($fromStage, $toStage, $createNewVersion){
+    public function onAfterPublish(){
                             file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log.txt','ici');
 
         if (!$this->owner instanceof SiteTree && !$this->owner instanceof ElementalArea ){
