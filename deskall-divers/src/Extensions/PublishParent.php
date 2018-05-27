2f0670f19219bf8f5e545d1dda2b06484d613207
YoupiXxl <guillaume.pacilly@gmail.com>
2018-05-27 21:25:39 +0200

    f

--- deskall-divers/src/Extensions/PublishParent.php
+++ deskall-divers/src/Extensions/PublishParent.php
@@ -8,8 +8,14 @@ use DNADesign\Elemental\Models\ElementalArea;
 class PublishParent extends DataExtension
 {
 
+    public function updateCMSFields(FieldList $fields){
+        print_r('yo');
+    }
+
     /*** Loop recursively until we reach first parent page then publish it *****/
     public function onAfterVersionedPublish($fromStage, $toStage, $createNewVersion){
+                            file_put_contents($_SERVER['DOCUMENT_ROOT'].'/log.txt','ici');
+
         if (!$this->owner instanceof SiteTree && !$this->owner instanceof ElementalArea ){
             if ($toStage == "Live"){
                 if ($parent = $this->owner->getPage()){
