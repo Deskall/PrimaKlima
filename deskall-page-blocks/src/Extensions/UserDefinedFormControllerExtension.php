8d86cda78d81e28e5436c46adcaad7b334896cbc
Guillaume <Guillaume@intern.deskall.ch>
2018-05-30 13:07:36 +0200

    d

--- deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
+++ deskall-page-blocks/src/Extensions/UserDefinedFormControllerExtension.php
@@ -2,13 +2,26 @@
 
 use SilverStripe\Core\Extension;
 use SilverStripe\Control\Director;
+use SilverStripe\View\ThemeResourceLoader;
+use SilverStripe\View\SSViewer;
 
 class UserDefinedFormControllerExtension extends Extension
 {
 	private static $finished_anchor = '';
+
+	public function updateEmailData($emailData, $attachments){
+		$emailData['AbsoluteThemeDir'] = $this->owner->AbsoluteThemeDir();
+		$emailData['AbsoluteBaseUrl'] = Director::AbsoluteBaseUrl();
+		$emailData['SiteConfig'] = SiteConfig::current_site_config(); 
+	}
+
+	public function AbsoluteThemeDir(){
+        return Director::AbsoluteBaseUrl().ThemeResourceLoader::inst()->getThemePaths(SSViewer::get_themes())[0];
+    }
+
 	
 	public function updateEmail($email, $recipient, $emailData){
-		$email->setHTMLTemplate('email/base_email');
+		$email->setHTMLTemplate('emails/userform_email');
 	}
 
 }
