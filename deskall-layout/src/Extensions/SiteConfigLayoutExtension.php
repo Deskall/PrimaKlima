45019dc3b3fa78815e3cf387a266527df5222f3d
YoupiXxl <guillaume.pacilly@gmail.com>
2018-07-06 08:20:29 +0200

    z

--- deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
+++ deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
@@ -56,6 +56,8 @@ class SiteConfigLayoutExtension extends DataExtension
     'HeaderOpacity' => 'Varchar(255)',
     'HeaderFormat' => 'Varchar(255)',
     'StickyHeader' => 'Boolean(0)',
+    'HeaderLogoHeight' => 'Varchar(255)',
+    'DropdownSubMenuWidth' => 'Varchar(255)',
 
     'FooterBackground' => 'Varchar(255)',
     'FooterLogoWidth' => 'Varchar(255)',
@@ -96,6 +98,8 @@ class SiteConfigLayoutExtension extends DataExtension
     'HeaderFontSize' => '@main-nav-font-size',
     'HeaderMenuItemSize' => '@navbar-nav-item-height',
     'HeaderCollapsedHeight' => '@header-menu-collapsed-height',
+    'HeaderLogoHeight' => '@header-logo-height',
+    'DropdownSubMenuWidth' => '@navbar-dropdown-width',
 
     'FooterLogoWidth' => '@footer-logo-width',
     'FooterBackground' => '@footer-background-color',
@@ -224,7 +228,11 @@ class SiteConfigLayoutExtension extends DataExtension
         TextField::create('HeaderHeight',_t(__CLASS__.'.HeaderHeight','Höhe')),
         TextField::create('HeaderCollapsedHeight',_t(__CLASS__.'.HeaderCollapsedHeight','Mobile Höhe')),
         TextField::create('HeaderMenuItemSize',_t(__CLASS__.'.HeaderItemHeight','Menu Item Höhe')),
-        TextField::create('HeaderFontSize',_t(__CLASS__.'.HeaderFontSize','Navigation Schriftgrösse'))
+        TextField::create('HeaderFontSize',_t(__CLASS__.'.HeaderFontSize','Navigation Schriftgrösse')),
+        TextField::create('HeaderLogoHeight',_t(__CLASS__.'.HeaderLogHeight','Header Logo Höhe'))
+      ),
+      FieldGroup::create(
+        TextField::create('DropdownSubMenuWidth',_t(__CLASS__.'.DropdownSubMenuWidth','Breite der Dropdown-Navigation'))
       ),
       CheckboxField::create('StickyHeader',_t(__CLASS__.'.StickyHeader','Sticky Header'))
     )->setTitle(_t(__CLASS__.'.HeaderLayout','Header Layout'))->setName('HeaderBackgroundFields'));
