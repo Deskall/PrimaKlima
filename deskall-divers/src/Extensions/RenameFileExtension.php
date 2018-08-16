4f5ac8bde776792a7c0caa4d9d87bf78d79f6e8d
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-08-16 08:59:43 +0200

    s

--- deskall-divers/src/Extensions/RenameFileExtension.php
+++ deskall-divers/src/Extensions/RenameFileExtension.php
@@ -7,10 +7,8 @@ class RenameFileExtension extends DataExtension
 
     public function onBeforeWrite(){
        
-        $filename = pathinfo($this->owner->Filename,PATHINFO_FILENAME);
-        $ext = pathinfo($this->owner->Filename,PATHINFO_EXTENSION);
-        $filename = str_replace('.',"-",$filename);
-        $this->owner->Filename = $filename.".".$ext;
+        $name = str_replace('.',"-",$this->owner->Name);
+        $this->owner->Name = $name;
         parent::onBeforeWrite();
         
     }
