919decf80ee74b6aab71a584f5c37528ffb54509
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-08-16 08:43:29 +0200

    s

---
+++ deskall-divers/src/Extensions/RenameFileExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+use SilverStripe\ORM\DataExtension;
+
+class RenameFileExtension extends DataExtension
+{
+
+    public function onBeforeWrite(){
+       
+        $filename = pathinfo($this->owner->Filename,PATHINFO_FILENAME);
+        $ext = pathinfo($this->owner->Filename,PATHINFO_EXTENSION)
+        $filename = str_replace('.',"-",$filename);
+        $this->owner->Filename = $filename.".".$ext;
+
+        parent::onBeforeWrite();
+    }
+
+}
