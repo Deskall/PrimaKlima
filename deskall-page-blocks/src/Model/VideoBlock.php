0d33da24622996fa5598e5bfefa25ad698738c1b
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-09-12 10:43:43 +0200

    s

--- deskall-page-blocks/src/Model/VideoBlock.php
+++ deskall-page-blocks/src/Model/VideoBlock.php
@@ -51,7 +51,7 @@ class VideoBlock extends BaseElement implements Searchable
         'uk-child-width-1-3@s' => '3'
     ];
 
-    private static $has_many = ['Videos' => 'Video'];
+    private static $has_many = ['Videos' => VideoObject::class];
 
     private static $cascade_duplicates = [];
 
