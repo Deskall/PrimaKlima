a01172b09e1fb6b075dff4a01cd0db46d111c62a
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-09-12 13:39:21 +0200

    s

--- deskall-page-blocks/src/Extensions/DeskallEditableFormFieldExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallEditableFormFieldExtension.php
@@ -3,13 +3,10 @@
 use SilverStripe\ORM\DataExtension;
 
 class DeskallEditableFormFieldExtension extends DataExtension{
-	public function onBeforeWrite(){
-		if (!$this->owner->Placeholder){
-			 $this->owner->Placeholder = $this->owner->Title;
-			 if ($this->owner->Required){
-			 	$this->Placeholder .= " *";
-			 }	
-		}
-		parent::onBeforeWrite();
-	}
+	// public function onBeforeWrite(){
+	// 	if (!$this->owner->Placeholder){
+	// 		 $this->owner->Placeholder = $this->owner->Title;
+	// 	}
+	// 	parent::onBeforeWrite();
+	// }
 }
