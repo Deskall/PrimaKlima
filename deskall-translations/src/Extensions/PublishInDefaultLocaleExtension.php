37de3c15f5693d4241156a0bc35e635e07553505
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-01 09:57:25 +0100

    s

--- deskall-translations/src/Extensions/PublishInDefaultLocaleExtension.php
+++ deskall-translations/src/Extensions/PublishInDefaultLocaleExtension.php
@@ -11,12 +11,11 @@ class PublishInDefaultLocaleExtension extends DataExtension{
 		parent::onAfterWrite();
 		if ($this->owner->IsGlobalDefault){
 			ob_start();
-			print_r('ici');
-			$result = ob_get_clean();
-			file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
+
 			foreach(ClassInfo::subclassesFor('DataObject') as $class ) {
 				if (singleton($class)->hasExtension("TractorCow\Fluent\Extension\FluentFilteredExtension")) {
 					$objects = DataList::create($class);
+					print_r($class."\n");
 					foreach($objects as $obj){
 						if (!$obj->Locales()->exists()){
 							$obj->Locales()->add($this->owner);
@@ -25,6 +24,9 @@ class PublishInDefaultLocaleExtension extends DataExtension{
 					}
 				}
 			}
+
+			$result = ob_get_clean();
+			file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
 		}
 		
 	}
