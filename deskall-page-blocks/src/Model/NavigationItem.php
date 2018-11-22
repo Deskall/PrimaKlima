cbec623df6996b4bee431d0a33b8af95d12e4c22
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-22 13:56:08 +0100

    e

--- deskall-page-blocks/src/Model/NavigationItem.php
+++ deskall-page-blocks/src/Model/NavigationItem.php
@@ -58,6 +58,7 @@ class NavigationItem extends DataObject{
 		$fields->addFieldToTab('Root.Main',DropdownField::create('ItemType','Menu Typ',['block' => 'Scroll zu Element','target' => 'Aktion', 'link' => 'Link']));
 		$fields->addFieldToTab('Root.Main',Wrapper::create(DropdownField::create('ActionID','Aktion',$this->Parent()->Parent()->Elements()->exclude(['ID' => $this->ParentID, 'ClassName' => 'HiddenActionBlock'])->map('ID','Title')))->displayIf('ItemType')->isEqualTo('target')->end());
 		$fields->addFieldToTab('Root.Main',Wrapper::create(DropdownField::create('TargetID','Seite Block',$this->Parent()->Parent()->Elements()->filter('ClassName','HiddenActionBlock')->exclude('ID',$this->ParentID)->map('ID','Title')))->displayIf('ItemType')->isEqualTo('block')->end());
+		$fields->addFieldToTab('Root.Main',HTMLDropdownField::create('BackgroundColor',_t(__CLASS__.'.BackgroundColor','Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
 		return $fields;
 	}
 }
