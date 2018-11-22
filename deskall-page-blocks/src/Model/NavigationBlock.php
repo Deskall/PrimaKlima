b187cfa08df71d13bf07fa8893232daeddc96d36
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-22 13:33:15 +0100

    e

--- deskall-page-blocks/src/Model/NavigationBlock.php
+++ deskall-page-blocks/src/Model/NavigationBlock.php
@@ -139,40 +139,6 @@ class NavigationBlock extends BaseElement
         //To do : filter relevant icons
         return HTMLDropdownField::getSourceIcones();
     }
-
-        /**
-     * For the frontend, return a parsed set of data for use in templates
-     *
-     * @return ArrayData|null
-     */
-    public function CallToActionLink()
-    {
-        return $this->decodeLinkData($this->getField('Link'));
-    }
-
-
-
-    /**
-     * Given a set of JSON data, decode it, attach the relevant Page object and return as ArrayData
-     *
-     * @param string $linkJson
-     * @return ArrayData|null
-     */
-    protected function decodeLinkData($linkJson)
-    {
-        if (!$linkJson || $linkJson === 'null') {
-            return;
-        }
-
-        $data = ArrayData::create(Convert::json2obj($linkJson));
-
-        // Link page, if selected
-        if ($data->PageID) {
-            $data->setField('Page', self::get_by_id(SiteTree::class, $data->PageID));
-        }
-
-        return $data;
-    }
  
 
 }
--- deskall-page-blocks/src/Model/NavigationItem.php
+++ deskall-page-blocks/src/Model/NavigationItem.php
@@ -2,6 +2,10 @@
 
 use SilverStripe\ORM\DataObject;
 use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\Forms\FormField;
+use SilverStripe\View\ArrayData;
+use SilverStripe\Core\Convert;
+use SilverStripe\CMS\Model\SiteTree;
 
 class NavigationItem extends DataObject{
 	private static $db = [
@@ -43,5 +47,39 @@ class NavigationItem extends DataObject{
 				return null;
 				break;
 		}
+	}
+
+	    /**
+	 * For the frontend, return a parsed set of data for use in templates
+	 *
+	 * @return ArrayData|null
+	 */
+	public function CallToActionLink()
+	{
+	    return $this->decodeLinkData($this->getField('Link'));
+	}
+
+
+
+	/**
+	 * Given a set of JSON data, decode it, attach the relevant Page object and return as ArrayData
+	 *
+	 * @param string $linkJson
+	 * @return ArrayData|null
+	 */
+	protected function decodeLinkData($linkJson)
+	{
+	    if (!$linkJson || $linkJson === 'null') {
+	        return;
+	    }
+
+	    $data = ArrayData::create(Convert::json2obj($linkJson));
+
+	    // Link page, if selected
+	    if ($data->PageID) {
+	        $data->setField('Page', self::get_by_id(SiteTree::class, $data->PageID));
+	    }
+
+	    return $data;
 	}
 }
