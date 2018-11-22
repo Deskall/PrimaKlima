4a94d7cff9ab7fbf4a68c4fd1d32d2beb33ce5de
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-22 13:51:38 +0100

    e

--- deskall-page-blocks/src/Model/NavigationItem.php
+++ deskall-page-blocks/src/Model/NavigationItem.php
@@ -3,6 +3,10 @@
 use SilverStripe\ORM\DataObject;
 use DNADesign\Elemental\Models\BaseElement;
 use Sheadawson\Linkable\Models\Link;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\DropdownField;
 
 class NavigationItem extends DataObject{
 	private static $db = [
@@ -13,11 +17,11 @@ class NavigationItem extends DataObject{
 
 	private static $has_one = [
 		'Target' => BaseElement::class,
-		'Parent' => NavigationBlock::class,
-		'LinkableLink' => Link::class
+		'Action' => BaseElement::class,
+		'Parent' => NavigationBlock::class
 	];
 
-	private static $extensions = ['Sortable','Activable'];
+	private static $extensions = ['Sortable','Activable','Linkable'];
 
 	public function TargetLink(){
 		switch ($this->ItemType) {
@@ -44,5 +48,14 @@ class NavigationItem extends DataObject{
 				return null;
 				break;
 		}
+	}
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->addFieldToTab('Root.Main',TextField::create('Title',_t(__CLASS__."Title",'Titel'))->setAttribute('Placeholder','wird den Block- /Seitetitel benutzen falls leer.'));
+		$fields->addFieldToTab('Root.Main',DropdownField::create('ItemType','Menu Typ',['block' => 'Scroll zu Element','target' => 'Aktion', 'link' => 'Link']));
+		$fields->addFieldToTab('Root.Main',Wrapper::create(DropdownField::create('ActionID','Aktion',$this->Parent()->Elements()->exclude(['ID' => $this->ParentID, 'ClassName' => 'HiddenActionBlock'])->map('ID','Title')))->displayIf('ItemType')->isEqualTo('target')->end());
+		$fields->addFieldToTab('Root.Main',Wrapper::create(DropdownField::create('TargetID','Seite Block',$this->Parent()->Elements()->filter('ClassName','HiddenActionBlock')->exclude('ID',$this->ParentID)->map('ID','Title')))->displayIf('ItemType')->isEqualTo('target')->end());
+		return $fields;
 	}
 }
