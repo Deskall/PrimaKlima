d2775f5516e5ffdccb65ee794a3636aac1290189
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-27 11:55:37 +0100

    s

--- deskall-layout/src/Model/LayoutBlock.php
+++ deskall-layout/src/Model/LayoutBlock.php
@@ -112,7 +112,7 @@ class LayoutBlock extends DataObject{
 			$title = $fields->fieldByName('Root.Main.Title');
 			$fields->removeByName('Links');
 			$fields->removeByName('SiteConfigID');
-			$title->displayIf('Type')->isEqualTo('links')->orIf('Type')->isEqualTo('content')->orIf('Type')->isEqualTo('partners');
+			$title->displayIf('Type')->isEqualTo('links')->orIf('Type')->isEqualTo('content')->orIf('Type')->isEqualTo('items');
 			
 			if ($this->ID > 0){
 				$fields->fieldByName('Root.Main.Type')->setDisabled(true);
