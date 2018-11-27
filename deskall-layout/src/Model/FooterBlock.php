f86d77cad79bacdfa7012a55e9e441b904b17a20
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2018-11-27 11:53:03 +0100

    w

--- deskall-layout/src/Model/FooterBlock.php
+++ deskall-layout/src/Model/FooterBlock.php
@@ -7,6 +7,9 @@ use SilverStripe\SiteConfig\SiteConfigLeftAndMain;
 use SilverStripe\Assets\Image;
 use Bummzack\SortableFile\Forms\SortableUploadField;
 use UncleCheese\DisplayLogic\Forms\Wrapper;
+use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\GridField\GridField;
+use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 class FooterBlock extends LayoutBlock{
 
@@ -14,12 +17,9 @@ class FooterBlock extends LayoutBlock{
 	];
 
     private static $has_many = [
-        'Links' => LayoutLink::class
+        'Links' => LayoutLink::class,
+        'Items' => ListItem::class
     ];
-
-    private static $many_many = ['Items' => ListItem::class];
-
-    private static $many_many_extraFields = ['Items' => ['SortOrder' => 'Int']];
 
     private static $block_types = [
         'address' => 'Adresse',
@@ -53,7 +53,7 @@ class FooterBlock extends LayoutBlock{
         $fields->removeByName('Type');
         $fields->removeByName('Items');
         $fields->addFieldToTab('Root.Main', DropdownField::create('Type',_t('LayoutBlock.Type','BlockTyp'),$this->owner->getTranslatedSourceFor('FooterBlock','block_types'))->setEmptyString(_t('LayoutBlock.TypeLabel','Wählen Sie den Typ aus')),'Title');
-        $fields->insertAfter('Title',Wrapper::create(GridField::create('Items',_t(__CLASS__.".Items",'Items'),$this->Items()))->displayIf('Type')->isEqualTo('items')->end());
+        $fields->insertAfter('Title',Wrapper::create(GridField::create('Items',_t(__CLASS__.".Items",'Items'),$this->Items(), GridFieldConfig_RecordEditor::create()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction())))->displayIf('Type')->isEqualTo('items')->end());
 
 		return $fields;
 	}
