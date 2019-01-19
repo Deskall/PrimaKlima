67e58cb9e56632998e0cf979fccd4d8f5751b408
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2019-01-19 15:55:20 +0100

    e

--- deskall-page-blocks/src/Extensions/FormBlockExtension.php
+++ deskall-page-blocks/src/Extensions/FormBlockExtension.php
@@ -67,7 +67,7 @@ class FormBlockExtension extends DataExtension
      $fields->addFieldToTab('Root.FormOptions',CheckboxField::create('hasCaptcha', _t(__CLASS__.'.WITHCAPTCHA', 'mit Google recaptcha Prüfung?')));
      $fields->addFieldToTab('Root.FormOptions',CheckboxField::create('ShowLabels', _t(__CLASS__.'.ShowLabels', 'Feld Titel anzeigen?')));
      $fields->addFieldToTab('Root.Main',TreeDropdownField::create('RedirectPageID',_t(__CLASS__.'.RedirectPage', 'erfolgreiche Einreichungsseite'), SiteTree::class));
-     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('ButtonBackground',$this->owner->fieldLabels(false)['ButtonBackground']),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
+     $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('ButtonBackground',_t(__CLASS__.'.ButtonBackground','Button Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
     // TO DO $fields->addFieldToTab('Root.LayoutTab',HTMLOptionsetField::create('Layout',_t(__CLASS__.'.Layout','Layout'),$this->owner->stat('block_layouts')));
      // $fields->addFieldToTab('Root.LayoutTab',HTMLDropdownField::create('StepTitleBackground',_t(__CLASS__.'.StepTitleBackground','Hintergrundfarbe den Seite Titel'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
      if ($this->owner->ID == 0){ 
