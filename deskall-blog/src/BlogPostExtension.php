e3ab0614f5a4ab2363183bee21c50c141e6f57ea
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-01-22 20:58:47 +0100

    d

--- deskall-blog/src/BlogPostExtension.php
+++ deskall-blog/src/BlogPostExtension.php
@@ -5,10 +5,16 @@ use SilverStripe\Forms\FieldList;
 
 class BlogPostExtension extends DataExtension{
 
-	private static $db = ['displayEntryMeta' => 'Boolean(0)'];
+	private static $db = [
+		'displayEntryMeta' => 'Boolean(0)',
+		'displayCommentsCount' => 'Boolean(0)',
+		'displayShareButtons' => 'Boolean(0)'
+	];
 
 	public function updateFieldLabels(&$labels){
 		$labels['displayEntryMeta'] = _t('BlogPost.DisplayEntryMeta','Artikel Infos anzeigen?');
+		$labels['displayCommentsCount'] = _t('BlogPost.displayCommentsCount','Kommentare Zahl anzeigen?');
+		$labels['displayShareButtons'] = _t('BlogPost.displayShareButtons','Share Button anzeigen?');
 	}
 
 	public function updateCMSFields(FieldList $fields){
