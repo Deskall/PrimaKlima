9241d9fd9a4313f3f0d0f7cf511cbb02c92dc689
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-01-22 20:41:21 +0100

    d

--- deskall-blog/src/BlogPostExtension.php
+++ deskall-blog/src/BlogPostExtension.php
@@ -5,6 +5,12 @@ use SilverStripe\Forms\FieldList;
 
 class BlogPostExtension extends DataExtension{
 
+	private static $db = ['displayEntryMeta' => 'Boolean(0)'];
+
+	public function updateCMSFields(&$labels){
+		$labels['displayEntryMeta'] = _t('BlogPost.DisplayEntryMeta','Artikel Infos anzeigen?');
+	}
+
 	public function updateCMSFields(FieldList $fields){
 		$fields->FieldByName('Root.Main.FeaturedImage')->setFolderName($this->owner->generateFolderName());
 	}
