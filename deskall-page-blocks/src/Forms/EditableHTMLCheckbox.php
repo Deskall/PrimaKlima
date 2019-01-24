b7417d2119549de2c246ba61871120a594b44123
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-01-24 20:01:00 +0100

    idee html checkbox

---
+++ deskall-page-blocks/src/Forms/EditableHTMLCheckbox.php
@@ -0,0 +1,70 @@
+<?php
+
+
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\UserForms\Model\EditableFormField;
+use SilverStripe\UserForms\Model\EditableFormField\EditableCheckbox;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
+/**
+ * EditableCheckbox
+ *
+ * A user modifiable checkbox on a UserDefinedForm
+ *
+ * @package userforms
+ */
+
+class EditableHTMLCheckbox extends EditableCheckbox
+{
+    private static $singular_name = 'Checkbox Field with HTML Label';
+
+    private static $plural_name = 'Checkboxes';
+
+    protected $jsEventHandler = 'click';
+
+    private static $db = [
+        'HTMLLabel' => 'HTMLText' // from CustomSettings
+    ];
+
+    private static $table_name = 'EditableHTMLCheckbox';
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+
+        $fields->replaceField('HTMLLabel', HTMLEditorField::create(
+            "HTMLLabel",
+            _t('SilverStripe\\UserForms\\Model\\EditableFormField.HTMLLabel', 'Label (HTML)')
+        ));
+
+        return $fields;
+    }
+
+    public function getFormField()
+    {
+        $field = CheckboxField::create($this->Name, $this->Title ?: false, $this->CheckedDefault)
+            ->setFieldHolderTemplate(__CLASS__ . '_holder')
+            ->setTemplate(__CLASS__);
+
+        $this->doUpdateFormField($field);
+
+        return $field;
+    }
+
+    public function getValueFromData($data)
+    {
+        $value = (isset($data[$this->Name])) ? $data[$this->Name] : false;
+
+        return ($value)
+            ? _t('SilverStripe\\UserForms\\Model\\EditableFormField.YES', 'Yes')
+            : _t('SilverStripe\\UserForms\\Model\\EditableFormField.NO', 'No');
+    }
+
+    public function isCheckBoxField()
+    {
+        return true;
+    }
+}
