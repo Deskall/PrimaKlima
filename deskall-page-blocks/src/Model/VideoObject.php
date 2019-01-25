6b4fe4f5353f50f70f09da3f31d2d7bee011a686
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-01-25 18:38:23 +0100

    Update VideoObject.php

--- deskall-page-blocks/src/Model/VideoObject.php
+++ deskall-page-blocks/src/Model/VideoObject.php
@@ -2,6 +2,8 @@
 
 use SilverStripe\ORM\DataObject;
 use SilverStripe\Assets\File;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 
 class VideoObject extends DataObject{
 
@@ -16,7 +18,9 @@ class VideoObject extends DataObject{
 	private static $db = [
 		'HTML' => 'HTMLText',
         'Title' => 'Text',
-        'URL' => 'Varchar(255)'
+        'Player' => 'Varchar',
+        'VideoID' => 'Varchar(255)',
+        'URL' => 'Varchar',
 	];
 
 	private static $has_one = [
@@ -29,10 +33,17 @@ class VideoObject extends DataObject{
 
 		$labels['Title'] = _t(__CLASS__.".Title",'Titel');
 		$labels['HTML'] = _t(__CLASS__.".HTML",'Inhalt');
-		$labels['URL'] = _t(__CLASS__.".URL",'URL der Video');
+		$labels['VideoID'] = _t(__CLASS__.".VideoID",'ID der Video');
 		$labels['File'] = _t(__CLASS__.".File",'Datei');
 
 		return $labels;
+	}
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->removeByName('URL');
+		$fields->addFieldToTab('Root.Main',DropdownField::create('Player',_t(__CLASS__.'Player','Player'),['youtube'=>'You Tube','vimeo' => 'Vimeo', 'dailymotion' => 'Dailymotion'])->setEmptyString('Player wählen'),'Title');
+		$fields->addFieldToTab('Root.Main',HTMLEditorField::create('HTML',_t(__CLASS__.'HTML','Beschreibung'))->setRows(3),'Player');
 	}
 
 	public function getSrc(){
