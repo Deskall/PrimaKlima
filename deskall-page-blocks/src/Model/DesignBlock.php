dbbeb1ad5c1b51fd82ded1f06ddf96704b1f0519
Guillaume <31889656+Deskall@users.noreply.github.com>
2019-03-12 17:37:44 +0100

    grafik

---
+++ deskall-page-blocks/src/Model/DesignBlock.php
@@ -0,0 +1,131 @@
+<?php
+
+use SilverStripe\Forms\HTMLEditor\HtmlEditorField;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\CheckboxField;
+use SilverStripe\Forms\OptionsetField;
+use SilverStripe\Forms\CompositeField;
+use SilverStripe\ORM\FieldType\DBField;
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\Assets\Image;
+use UncleCheese\DisplayLogic\Forms\Wrapper;
+use g4b0\SearchableDataObjects\Searchable;
+
+class DesignBlock extends BaseElement implements Searchable
+{
+    private static $icon = 'font-icon-picture';
+    
+    private static $controller_template = 'BlockHolder';
+
+    private static $controller_class = BlockController::class;
+
+    private static $table_name = 'DesignBlock';
+
+    private static $singular_name = 'Design block';
+
+    private static $plural_name = 'Design blocks';
+
+    private static $description = 'Inhalt Block mit höher Design';
+
+
+    private static $db = [
+        'HTML' => 'HTMLText'
+        
+    ];
+
+    private static $has_many = [
+        'Resources' => GrafikElement::class
+    ];
+
+
+
+    private static $cascade_duplicates = [];
+
+
+
+
+
+/***********************************************************************/
+    
+    public function fieldLabels($includerelations = true ){
+        $labels = parent::fieldLabels($includerelations );
+        $labels['Trigger'] = _t(__CLASS__.'.TriggeringText', 'Text der Öffnen-Button');
+
+        return $labels;
+    }
+    
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+      
+
+        return $fields;
+    }
+
+    public function getSummary()
+    {
+        return DBField::create_field('HTMLText', $this->HTML)->Summary(20);
+    }
+
+    public function getType()
+    {
+        return _t(__CLASS__ . '.BlockType', 'Block mit Design Element');
+    }
+
+   
+
+    /************* TRANLSATIONS *******************/
+    public function provideI18nEntities(){
+        $entities = [];
+        foreach($this->stat('block_actions') as $key => $value) {
+          $entities[__CLASS__.".block_actions_{$key}"] = $value;
+        }
+        
+        return $entities;
+    }
+
+/************* END TRANLSATIONS *******************/
+
+
+/************* SEARCHABLE FUNCTIONS ******************/
+
+
+    /**
+     * Filter array
+     * eg. array('Disabled' => 0);
+     * @return array
+     */
+    public static function getSearchFilter() {
+        return array();
+    }
+
+    /**
+     * FilterAny array (optional)
+     * eg. array('Disabled' => 0, 'Override' => 1);
+     * @return array
+     */
+    public static function getSearchFilterAny() {
+        return array();
+    }
+
+
+    /**
+     * Fields that compose the Title
+     * eg. array('Title', 'Subtitle');
+     * @return array
+     */
+    public function getTitleFields() {
+        return array('Title');
+    }
+
+    /**
+     * Fields that compose the Content
+     * eg. array('Teaser', 'Content');
+     * @return array
+     */
+    public function getContentFields() {
+        return array('HTML');
+    }
+/************ END SEARCHABLE ***************************/
+}
---
+++ deskall-page-blocks/src/Model/GrafikElement.php
@@ -0,0 +1,126 @@
+<?php
+
+use SilverStripe\ORM\DataObject;
+use SilverStripe\Assets\Image;
+use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\Versioned\Versioned;
+use SilverStripe\Forms\DropdownField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\ORM\FieldType\DBField;
+
+class GrafikElement extends DataObject
+{
+
+    private static $db = [
+        'Title' => 'Text',
+        'Type' => 'Varchar',
+        'Content' => 'HTMLText',
+        'Effect' => 'Varchar(255)',
+        'EffectOptions' => 'Varchar(255)',
+        'Position' => 'Varchar',
+        'ExtraCssClass' => 'Varchar'
+    ];
+
+    private static $has_one = [
+        'Parent' => DesignBlock::class,
+        'Image' => Image::class
+    ];
+
+    private static $extensions = [
+        Versioned::class,
+        'Activable',
+        'Linkable',
+        'Sortable'
+    ];
+
+    private static $effects = [
+        'none' => 'kein',
+        'kenburns' => 'ken burns',
+        'parallax' => 'parallax'
+    ];
+
+
+    private static $owns = [
+        'Image',
+    ];
+
+    private static $summary_fields = [
+        'Title' ,
+        'ImageThumbnail' ,
+        'getSummary'
+    ];
+
+    function fieldLabels($includerelations = true) {
+        $labels = parent::fieldLabels($includerelations);
+     
+        $labels['Title'] = _t(__CLASS__.'.TitleLabel','Titel');
+        $labels['ImageThumbnail'] = _t(__CLASS__.'.Image', 'Bild');
+        $labels['getSummary'] = _t(__CLASS__.'.Summary',  'Inhalt');
+     
+        return $labels;
+    }
+
+
+    public function getCMSFields()
+    {
+        $fields = parent::getCMSFields();
+        $fields->removeByName('ParentID');
+        $fields->FieldByName('Root.Main.Content')->setRows(3);
+        $fields->dataFieldByName('Image')->setFolderName($this->getFolderName());
+        $fields->addFieldToTab('Root.Settings',DropdownField::create('Effect',_t(__CLASS__.'.Effect','Effekt'), $this->getTranslatedSourceFor(__CLASS__,'effects')));
+        $fields->addFieldToTab('Root.Settings',TextField::create('EffectOptions',_t(__CLASS__.'.EffectOptions','Effekt Optionen')));
+        $fields->FieldByName('Root.Main')->setTitle(_t(__CLASS__.'.Main','Inhalt'));
+        $fields->FieldByName('Root.Settings')->setTitle(_t(__CLASS__.'.Settings','Einstellungen'));
+        return $fields;
+    }
+
+    public function BoxTitle(){
+        $o = new DBHTMLText();
+        $o->setValue($this->Title);
+        return $o;
+    }
+
+    public function ImageThumbnail(){
+        $o = new DBHTMLText();
+        $html = ($this->Image() && $this->Image()->exists()) ? (($this->Image()->getExtension() == "svg" ) ? '<img src="'.$this->Image()->URL.'" width="250" height="200" />' : '<img src="'.$this->Image()->Fill(250,200)->URL.'" />') : _t(__CLASS__.'.NoBild','(keine)');
+        $o->setValue($html);
+        return $o;
+    }
+
+    public function getFolderName(){
+        return $this->Parent()->getFolderName();
+    }
+
+    public function getSummary()
+    {
+        return DBField::create_field('HTMLText', $this->Content)->Summary(20);
+    }
+
+    public function getPage(){
+        return $this->Parent()->getPage();
+    }
+
+    public function onAfterPublish(){
+        if ($this->Parent()){
+            $this->Parent()->publishSingle();
+        }
+        if ($this->getPage()){
+             $this->getPage()->publishSingle();
+        }
+    }
+
+
+/************* TRANLSATIONS *******************/
+    public function provideI18nEntities(){
+        $entities = [];
+        foreach($this->stat('effects') as $key => $value) {
+          $entities[__CLASS__.".effects_{$key}"] = $value;
+        }
+       
+        return $entities;
+    }
+
+/************* END TRANLSATIONS *******************/
+
+}
