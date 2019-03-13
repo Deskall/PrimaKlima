45dcd49a411079acb62c813e952a8d38e4d657df
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-03-13 08:18:05 +0100

    Update MenuConfig.php

--- deskall-restaurant-menu/src/MenuConfig.php
+++ deskall-restaurant-menu/src/MenuConfig.php
@@ -7,32 +7,22 @@ use SilverStripe\Assets\File;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Security\Group;
 
-class EventConfig extends DataObject
+class MenuConfig extends DataObject
 {
     private static $db = array(
-        'Title' => 'Varchar',
-        'BillPayLabel' => 'HTMLText',
-        'OnlinePayLabel' => 'HTMLText',
-        'PaymentConfirmedLabel' => 'HTMLText',
-        'BillEmailSubject' => 'Varchar',
-        'BillEmailBody' => 'HTMLText',
-        'PaymentEmailSubject' => 'Varchar',
-        'PaymentEmailBody' =>  'HTMLText',
-        'OrderNumberOffset' => 'Int'
+        'Title' => 'Varchar'
     );
 
     private static $singular_name = "Einstellungen";
     private static $plural_name = "Einstellungen";
 
     private static $has_one = [
-       'BillFile' => File::class,
-       'AGBFile' => File::class,
-       'ReceiptFile' => File::class
+       'CarteFile' => File::class
+      
     ];
 
     private static $owns = [
-        'BillFile',
-        'AGBFile'
+        'CarteFile'
     ];
 
     private static $summary_fields = [
@@ -43,17 +33,7 @@ class EventConfig extends DataObject
     public function fieldLabels($includerelation = true){
     $labels = parent::fieldLabels($includerelation);
     $labels['Title'] = _t(__CLASS__.'.Title','Titel');
-    $labels['BillFile'] = _t(__CLASS__.'.BillFile','Rechnung Vorlage');
-    $labels['BillPayLabel'] = _t(__CLASS__.'.BillPayLabel','Text für die Zahlung mit Rechnung');
-    $labels['OnlinePayLabel'] = _t(__CLASS__.'.OnlinePayLabel','Text für die Zahlung mit PayPal oder Kreditkarte');
-    $labels['PaymentConfirmedLabel'] = _t(__CLASS__.'.PaymentConfirmedLabel','Zahlungsbestätigungstext');
-    $labels['BillEmailSubject'] = _t(__CLASS__.'.BillEmailSubject','Rechnungsemail Betreff');
-    $labels['BillEmailBody'] = _t(__CLASS__.'.BillEmailBody','Rechnungsemail Inhalt');
-    $labels['PaymentEmailSubject'] = _t(__CLASS__.'.PaymentEmailSubject','Zahlunsbestätigungsemail Betreff');
-    $labels['PaymentEmailBody'] = _t(__CLASS__.'.PaymentEmailBody','Zahlunsbestätigungsemail Inhalt');
-    $labels['AGBFile'] = _t(__CLASS__.'.AGBFile','AGB Datei');
-    $labels['ReceiptFile'] = _t(__CLASS__.'.ReceiptFile','Quittung Vorlage');
-
+    $labels['CarteFile'] = _t(__CLASS__.'.CarteFile','Karte Vorlage');
     
     return $labels;
     }
@@ -78,24 +58,12 @@ class EventConfig extends DataObject
     {
        $fields = parent::getCMSFields();
 
-       $fields->addFieldToTab('Root.Main',UploadField::create('BillFile',$this->fieldLabels()['BillFile'])->setFolderName('Uploads/Vorlagen'));
-        $fields->addFieldToTab('Root.Main',UploadField::create('ReceiptFile',$this->fieldLabels()['ReceiptFile'])->setFolderName('Uploads/Vorlagen'));
-       $fields->addFieldToTab('Root.Main',UploadField::create('AGBFile',$this->fieldLabels()['AGBFile'])->setIsMultiUpload(false)->setFolderName('Uploads/Vorlagen'));
-      
+       $fields->addFieldToTab('Root.Main',UploadField::create('CarteFile',$this->fieldLabels()['CarteFile'])->setFolderName('Uploads/Vorlagen'));
+    
 
        return $fields;
     }
 
-    public function parseString($string)
-    {
-        $variables = array(
-            '$CookAccountLink' => MemberProfilePage::get()->filter('GroupID',Group::get()->filter('Code','mietkoeche')->first()->ID)->first()->Link()
-        );
-        
-
-        return str_replace(array_keys($variables), array_values($variables), $string);
-    }
-    
 
 
 }
