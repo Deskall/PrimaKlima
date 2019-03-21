616dc61ee6b87ee51decc36ca071f6ea9b53fbc4
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2019-03-21 13:41:58 +0100

    e

--- deskall-restaurant-menu/src/Models/MenuCarte.php
+++ deskall-restaurant-menu/src/Models/MenuCarte.php
@@ -93,19 +93,19 @@ class MenuCarte extends DataObject{
 
     public function printPDF(){
 
-      $pdf = new Fpdi();
+      // $pdf = new Fpdi();
 
-      $pdf->AddPage();
+      // $pdf->AddPage();
 
-      $html = '
-      <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
-      <i>This is the first example of TCPDF library.</i>
-      <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
-      <p>Please check the source code documentation and other examples for further information.</p>
-      <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>';
+      // $html = '
+      // <h1>Welcome to <a href="http://www.tcpdf.org" style="text-decoration:none;background-color:#CC0000;color:black;">&nbsp;<span style="color:black;">TC</span><span style="color:white;">PDF</span>&nbsp;</a>!</h1>
+      // <i>This is the first example of TCPDF library.</i>
+      // <p>This text is printed using the <i>writeHTMLCell()</i> method but you can also use: <i>Multicell(), writeHTML(), Write(), Cell() and Text()</i>.</p>
+      // <p>Please check the source code documentation and other examples for further information.</p>
+      // <p style="color:#CC0000;">TO IMPROVE AND EXPAND TCPDF I NEED YOUR SUPPORT, PLEASE <a href="http://sourceforge.net/donate/index.php?group_id=128076">MAKE A DONATION!</a></p>';
 
-      // Print text using writeHTMLCell()
-      $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+      // // Print text using writeHTMLCell()
+      // $pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
 
       // $pdf->Addfont('Stone sans ITC','','stonesansitc.php');
       // $pdf->Addfont('Lato','','lato.php');
@@ -164,19 +164,19 @@ class MenuCarte extends DataObject{
       //       $pdf->setXY(150,$y + $i);
       //       $pdf->WriteHtml("Kunde Unterschrift: ");
       // }
-      $pdf->Output($output,'F');
+      // $pdf->Output($output,'F');
       
 
 
-      $tmpFolder = "Uploads/Menus/".$this->ID;
-      $folder = Folder::find_or_make($tmpFolder);
-      $file = File::create();
-      $file->ParentID = $folder->ID;
-      $file->setFromLocalFile($output, 'Uploads/Menus/'.$this->ID.'/'.$this->Title.'.pdf');
-      $file->write();
-      $file->publishSingle();
-      $this->FileID = $file->ID;
-      $this->write();
+      // $tmpFolder = "Uploads/Menus/".$this->ID;
+      // $folder = Folder::find_or_make($tmpFolder);
+      // $file = File::create();
+      // $file->ParentID = $folder->ID;
+      // $file->setFromLocalFile($output, 'Uploads/Menus/'.$this->ID.'/'.$this->Title.'.pdf');
+      // $file->write();
+      // $file->publishSingle();
+      // $this->FileID = $file->ID;
+      // $this->write();
       
     }
 }
