ce439c75b0fc312071cdbdd90f039efa0a2971e7
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2019-03-22 14:12:17 +0100

    e

--- deskall-page-blocks/src/Model/Slide.php
+++ deskall-page-blocks/src/Model/Slide.php
@@ -272,7 +272,7 @@ class Slide extends DataObject
 
     public function ImageForTemplate(){
 
-        return $this->Image()->hasExtension('ImageExtension');
+        return $this->Image()->hasMethod('AltTag');
 
 
         // $html = '<img ';
