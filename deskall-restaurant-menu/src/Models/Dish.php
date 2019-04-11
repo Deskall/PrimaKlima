47bb2905298fd2cf73f7affb15207b012d354a15
Youpixxl <guillaume.pacilly@mylibrary-online.com>
2019-04-11 09:57:27 +0200

    w

--- deskall-restaurant-menu/src/Models/Dish.php
+++ deskall-restaurant-menu/src/Models/Dish.php
@@ -54,7 +54,7 @@ class Dish extends DataObject{
         'PrintPrice'
     ];
 
-    private static $searchable_fields = ['Title'];
+    private static $searchable_fields = ['Title', 'Category'];
 
     function fieldLabels($includerelations = true) {
         $labels = parent::fieldLabels($includerelations);
