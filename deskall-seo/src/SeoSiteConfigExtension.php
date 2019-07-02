e8f594a379b13f0b77233d4e6a33cf3d99c0abac
Guillaume <31889656+Deskall@users.noreply.github.com>
2019-07-02 15:27:04 +0200

    Update SeoSiteConfigExtension.php

--- deskall-seo/src/SeoSiteConfigExtension.php
+++ deskall-seo/src/SeoSiteConfigExtension.php
@@ -7,7 +7,7 @@ use SilverStripe\Forms\TextareaField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Assets\Image;
 use SilverStripe\AssetAdmin\Forms\UploadField;
-use SilverStripe\AssetAdmin\Forms\HeaderField;
+use SilverStripe\Forms\HeaderField;
 
 /**
  * SeoSiteConfig
