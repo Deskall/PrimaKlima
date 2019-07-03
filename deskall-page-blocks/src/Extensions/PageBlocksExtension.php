0641f3abdb477194e66bdbe0b23075e6b62dcfce
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-07-03 15:17:19 +0200

    Update PageBlocksExtension.php

--- deskall-page-blocks/src/Extensions/PageBlocksExtension.php
+++ deskall-page-blocks/src/Extensions/PageBlocksExtension.php
@@ -58,7 +58,7 @@ class PageBlocksExtension extends DataExtension {
 		$fields->addFieldToTab('Root.Layout',CompositeField::create(
 		    // HTMLOptionsetField::create('BlocksPerLine',_t(__CLASS__.'.BlocksPerLine','Blöcke per Linie'),$this->stat('blocks_per_line')),
 		    HTMLOptionsetField::create('BlockAlignment',_t(__CLASS__.'.BlockAlignment','Blockausrichtung'),$this->owner->stat('block_alignments'))->setDescription(_t(__CLASS__.'.BlockAlignmentLabel','Nur gültig wenn die Blöcke per Linie nehmen nicht die ganze Breite.'))
-		);
+		));
 	}
 
 	public function checkLead(){
