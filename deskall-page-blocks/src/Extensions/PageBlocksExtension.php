0acc587d99be5d03bbac282e120af6dea7e70314
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-07-04 18:13:14 +0200

    Update PageBlocksExtension.php

--- deskall-page-blocks/src/Extensions/PageBlocksExtension.php
+++ deskall-page-blocks/src/Extensions/PageBlocksExtension.php
@@ -68,7 +68,7 @@ class PageBlocksExtension extends DataExtension {
 		    // HTMLOptionsetField::create('BlocksPerLine',_t(__CLASS__.'.BlocksPerLine','Blöcke per Linie'),$this->stat('blocks_per_line')),
 		    HTMLOptionsetField::create('BlockAlignment',_t(__CLASS__.'.BlockAlignment','Blockausrichtung'),$this->owner->stat('block_alignments'))->setDescription(_t(__CLASS__.'.BlockAlignmentLabel','Nur gültig wenn die Blöcke per Linie nehmen nicht die ganze Breite.')),
 		    CheckboxField::create('useMasonry',_t(__CLASS__.'.useMasonry','Wenn die Blöcken unterschiedliche Höhen haben, wird ein Layout ohne Lücken erstellt')),
-		    TextField::create('Parallax',_t(__CLASS__.'.Parallax','mit Parallax Effekt'))->displayIf('useMasonry')->isChecked()->end()
+		    TextField::create('Parallax',_t(__CLASS__.'.Parallax','mit Parallax Effekt'))
 		));
 	}
 
