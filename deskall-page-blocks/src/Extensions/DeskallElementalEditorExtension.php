7e8de46aec9277f62ba15034a8cb8b1e1a625f60
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-07-07 17:54:20 +0200

    Update DeskallElementalEditorExtension.php

--- deskall-page-blocks/src/Extensions/DeskallElementalEditorExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallElementalEditorExtension.php
@@ -10,7 +10,7 @@ class DeskallElementalEditorExtension extends DataExtension
     public function updateGetTypes(&$types){
 
         if ($this->owner->getArea()->getOwnerPage() && $this->owner->getArea()->getOwnerPage()->ClassName == "ParentBlock" && $this->owner->getArea()->getOwnerPage()->CollapsableChildren){
-            $allowed = Config::inst()->get('BaseBlockExtension')->stat('collapsable_blocks');
+            $allowed = $this->owner->getArea()->getOwnerPage()->stat('allowed_collapsed_blocks');
             foreach ($types as $key => $value) {
                 if (!in_array($key,$allowed)){
                     unset($types[$key]);
