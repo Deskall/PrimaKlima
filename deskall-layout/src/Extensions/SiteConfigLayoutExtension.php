63b9372b49dbd16ceb574441ab62f0e703800fe0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-07-18 11:27:27 +0200

    Update SiteConfigLayoutExtension.php

--- deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
+++ deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
@@ -454,6 +454,8 @@ class SiteConfigLayoutExtension extends DataExtension
       file_put_contents($fullpath, "\n".".".$c->Code.'{background-color:#'.$c->Color.';color:#'.$c->FontColor.';.dk-text-content a, .calltoaction-container *{color:#'.$c->LinkColor.';&:after{background-color:#'.$c->LinkColor.';}&:active,&:hover{color:#'.$c->LinkHoverColor.';*{color:#'.$c->LinkHoverColor.';}&:after{background-color:#'.$c->LinkHoverColor.';}}}*{color:#'.$c->FontColor.';}}',FILE_APPEND);
       /** CSS Class for Call To Action Link **/
       file_put_contents($fullpath, "\n".".calltoaction-container .uk-button.button-".$c->Code.'{background-color:#'.$c->Color.';color:#'.$c->LinkColor.';*{color:#'.$c->LinkColor.';}&:hover,&:focus,&:active{color:#'.$c->LinkHoverColor.';*{color:#'.$c->LinkHoverColor.';}}}',FILE_APPEND);
+      /** CSS Class for Call To Form Button **/
+      file_put_contents($fullpath, "\n".".userform .button-".$c->Code.' .uk-button{background-color:#'.$c->Color.';color:#'.$c->LinkColor.';*{color:#'.$c->LinkColor.';}&:hover,&:focus,&:active{color:#'.$c->LinkHoverColor.';*{color:#'.$c->LinkHoverColor.';}}}',FILE_APPEND);
       /*** Css class for Slideshow controls **/
       file_put_contents($fullpath,
         "\n".'.'.$c->Code.' .uk-dotnav > * > *{background-color:transparent;border-color:#'.$c->FontColor.';}' 
