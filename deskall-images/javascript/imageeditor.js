28086ca75bfdd08eab2450c973135cca05d5f4f6
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-08-09 15:20:37 +0200

    Update imageeditor.js

--- deskall-images/javascript/imageeditor.js
+++ deskall-images/javascript/imageeditor.js
@@ -1,10 +1,10 @@
 
-var ImageEditor = require('tui-image-editor');
-var instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
-    cssMaxWidth: 700,
-    cssMaxHeight: 500,
-    selectionStyle: {
-        cornerSize: 20,
-        rotatingPointOffset: 70
-    }
-});
+// var ImageEditor = require('tui-image-editor');
+// var instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+//     cssMaxWidth: 700,
+//     cssMaxHeight: 500,
+//     selectionStyle: {
+//         cornerSize: 20,
+//         rotatingPointOffset: 70
+//     }
+// });
