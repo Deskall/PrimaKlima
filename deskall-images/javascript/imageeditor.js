f9e8a1ca703e7c39931a05a959533d1575187c47
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-08-09 15:21:59 +0200

    Update imageeditor.js

--- deskall-images/javascript/imageeditor.js
+++ deskall-images/javascript/imageeditor.js
@@ -1,10 +1,26 @@
 
-// var ImageEditor = require('tui-image-editor');
-// var instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
-//     cssMaxWidth: 700,
-//     cssMaxHeight: 500,
-//     selectionStyle: {
-//         cornerSize: 20,
-//         rotatingPointOffset: 70
-//     }
-// });
+
+var blackTheme = require('./js/theme/black-theme.js');
+var locale_ru_RU = { // override default English locale to your custom
+    'Crop': 'Обзрезать',
+    'Delete-all': 'Удалить всё'
+    // etc...
+};
+var instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+     includeUI: {
+         loadImage: {
+             path: 'img/sampleImage.jpg',
+             name: 'SampleImage'
+         },
+         locale: locale_ru_RU,
+         theme: blackTheme, // or whiteTheme
+         initMenu: 'filter',
+         menuBarPosition: 'bottom'
+     },
+    cssMaxWidth: 700,
+    cssMaxHeight: 500,
+    selectionStyle: {
+        cornerSize: 20,
+        rotatingPointOffset: 70
+    }
+});
