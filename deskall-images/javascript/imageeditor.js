d3babb90946acb077e7d20dba79213bc5d0a0088
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-08-09 15:26:56 +0200

    w

--- deskall-images/javascript/imageeditor.js
+++ deskall-images/javascript/imageeditor.js
@@ -1,26 +1,7 @@
 
-
-var blackTheme = require('./js/theme/black-theme.js');
-var locale_ru_RU = { // override default English locale to your custom
-    'Crop': 'Обзрезать',
-    'Delete-all': 'Удалить всё'
-    // etc...
-};
-var instance = new ImageEditor(document.querySelector('#tui-image-editor'), {
+var imageEditor = new tui.ImageEditor('#tui-image-editor-container', {
      includeUI: {
-         loadImage: {
-             path: 'img/sampleImage.jpg',
-             name: 'SampleImage'
-         },
-         locale: locale_ru_RU,
-         theme: blackTheme, // or whiteTheme
-         initMenu: 'filter',
-         menuBarPosition: 'bottom'
-     },
-    cssMaxWidth: 700,
-    cssMaxHeight: 500,
-    selectionStyle: {
-        cornerSize: 20,
-        rotatingPointOffset: 70
-    }
+         menu: ['shape', 'crop']
+         
+     }
 });
--- deskall-images/src/DeskallImageAssetFormFactoryExtension.php
+++ deskall-images/src/DeskallImageAssetFormFactoryExtension.php
@@ -27,7 +27,7 @@ class DeskallImageAssetFormFactoryExtension extends Extension
             
             $fields->insertBefore(
                 'Title',
-                 LiteralField::create('Edit','<button id="edit-image" data-id="'.$image->ID.'">Bild bearbeiten</button><div id="tui-image-editor"></div>')
+                 LiteralField::create('Edit','<button id="edit-image" data-id="'.$image->ID.'">Bild bearbeiten</button><div id="tui-image-editor-container"></div>')
             );
             $fields->insertAfter(
                 'Title',
