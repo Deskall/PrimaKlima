c453490b287e19dda2479f78b0e1dbd68232d54f
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-02 15:54:14 +0200

    Update DeskallPageControllerExtension.php

--- deskall-divers/src/Extensions/DeskallPageControllerExtension.php
+++ deskall-divers/src/Extensions/DeskallPageControllerExtension.php
@@ -3,6 +3,7 @@
 use SilverStripe\Core\Extension;
 use SilverStripe\Control\Director;
 use SilverStripe\ORM\FieldType\DBHTMLText;
+use SilverStripe\SiteConfig\SiteConfig;
 
 class DeskallPageControllerExtension extends Extension
 {
@@ -12,7 +13,7 @@ class DeskallPageControllerExtension extends Extension
     }
 
     public function Css(){
-    	$css_compiled = file_get_contents(Director::baseFolder().'/deskall-layout/templates/Includes/Css.ss');
+    	$css_compiled = file_get_contents(Director::baseFolder().SiteConfig::current_site_config()->getCurrentThemeDir().'/templates/Includes/Css.ss');
         $css = new DBHTMLText();
         $css->setValue($css_compiled);
         return $css;
