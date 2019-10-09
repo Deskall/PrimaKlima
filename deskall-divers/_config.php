3012e112b41588e3349b94442c3da86071b644d9
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-09 10:39:59 +0200

    e

--- deskall-divers/_config.php
+++ deskall-divers/_config.php
@@ -1,3 +1,10 @@
 <?php
 
 /*** deskall diverse custom code **/
+use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
+use SilverStripe\SiteConfig\SiteConfig;
+
+$editorCss = SiteConfig::current_site_config()->getCurrentThemeDir().'css/editor.css';
+
+TinyMCEConfig::config()->set('editor_css', $editorCss);
+
