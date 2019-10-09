d0d6bba0d2ac4009c7371395793c7d88ccdb2cec
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-09 12:02:23 +0200

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
+	resizeMain();
+});
+$( window ).resize(function() {
+	resizeMain();
+});
+
+function resizeMain(){
 	var screen = $( window ).height();
 	var h = $('header').outerHeight() + $('footer').outerHeight();
 	$('main').css({ minHeight: `${screen - h}px` });
-});
+}
