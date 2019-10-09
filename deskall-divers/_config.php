d593849f7726f12d9ebe28aaea89c728512dd2f3
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-09 11:00:02 +0200

    Update _config.php

--- deskall-divers/_config.php
+++ deskall-divers/_config.php
@@ -4,12 +4,12 @@
 use SilverStripe\Forms\HTMLEditor\TinyMCEConfig;
 use SilverStripe\SiteConfig\SiteConfig;
 
-$editorCss = SiteConfig::current_site_config()->getCurrentThemeDir().'/css/editor.css';
+// $editorCss = SiteConfig::current_site_config()->getCurrentThemeDir().'/css/editor.css';
 
-ob_start();
-			print_r($editorCss);
-			$result = ob_get_clean();
-			file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
+// ob_start();
+// 			print_r($editorCss);
+// 			$result = ob_get_clean();
+// 			file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
 
-TinyMCEConfig::config()->merge('editor_css', $editorCss);
+// TinyMCEConfig::config()->merge('editor_css', $editorCss);
 
