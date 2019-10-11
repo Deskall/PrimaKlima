001f5116f3f33188694d5a6895eb1e3993398dac
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-11 17:39:20 +0200

    Update MenuSectionLink.php

--- mysite/src/Models/MenuSectionLink.php
+++ mysite/src/Models/MenuSectionLink.php
@@ -2,6 +2,8 @@
 
 use SilverStripe\Forms\FieldList;
 use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Assets\Image;
+use SilverStripe\AssetAdmin\Forms\UploadField;
 
 class MenuSectionLink extends MenuLink{
 	private static $db = [
@@ -9,12 +11,13 @@ class MenuSectionLink extends MenuLink{
 		'Background' => 'Varchar'
 	];
 
-	private static $has_one = ['Parent' => MenuSection::class];
+	private static $has_one = ['Parent' => MenuSection::class, 'Image' => Image::class];
 
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
 		$fields->removeByName('Background');
 		$fields->push(HTMLDropdownField::create('Background',_t(__CLASS__.'.BackgroundColor','Label Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
+		$fields->push(UploadField::create('Image',_t(__CLASS__.'.Image','Bild / Icon'))->setFolderName($this->Parent()->Page()->generateFolderName()));
 		// $fields->FieldByName('Background')->displayIf('Label')->isNotNull()->end();
 		return $fields;
 	}
