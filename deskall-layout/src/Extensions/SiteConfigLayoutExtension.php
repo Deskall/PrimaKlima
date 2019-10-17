164ec9906c084670defe4bb525985d09fe66ecf2
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-17 17:23:09 +0200

    Update SiteConfigLayoutExtension.php

--- deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
+++ deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
@@ -522,7 +522,7 @@ class SiteConfigLayoutExtension extends DataExtension
   public function RegenerateCss(){
     $files = ['body.min.css','head.min.css','editortocompile.css'];
     foreach ($files as $key => $value) {
-      $url = $this->owner->getAbsoluteCurrentThemeDir().$value;
+      $url = $this->owner->getAbsoluteCurrentThemeDir().'/css/'.$value;
       if ($this->owner->hasExtension('SilverStripe\Subsites\Extensions\SiteConfigSubsites')){
           if ($this->owner->SubsiteID > 0){
               $url = Director::AbsoluteURL('themes/'.$this->owner->Subsite()->Theme.'/css/'.$value);
