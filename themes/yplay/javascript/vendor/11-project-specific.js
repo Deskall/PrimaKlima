d0a0270f922c03914b823880ac23a5ba883008e3
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-18 12:38:30 +0200

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 	if ($(".sidebar-menu").length > 0){
 		var sidebarWidth = $(".sidebar-menu").width();
-		$(".sidebar-menu").css("transform-origin", '~"calc(50% + '+sidebarWidth+'px)" 0');
+		console.log(sidebarWidth);
+		$(".sidebar-menu").css("transform-origin", 'calc(50% + '+sidebarWidth+'px) 0');
 		$(".sidebar-menu").show();
 	}
 });
