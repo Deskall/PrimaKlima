c0b010e8dde39bb3ea068ce471cd87a1a4823fc7
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-18 11:36:17 +0200

    Update LateralSection.php

--- mysite/src/Models/LateralSection.php
+++ mysite/src/Models/LateralSection.php
@@ -8,14 +8,15 @@ class LateralSection extends MenuSection{
 
 	private static $db = [
 		'ButtonTitle' => 'Varchar(255)',
-		'ButtonFarbe' => 'Varchar(255)'
+		'ButtonFarbe' => 'Varchar(255)',
+		'ButtonIcon' => 'Varchar(255)'
 	];
 
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
 		$fields->removeByName('Image');
 		$fields->insertAfter('ButtonTitle',HTMLDropdownField::create('ButtonFarbe',_t('Form.ButtonBackground','Button Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
-
+		$fields->insertAfter('ButtonFarbe',HTMLDropdownField::create('ButtonIcon', _t(__CLASS__.'.Icone','button Icon'), HTMLDropdownField::getSourceIcones(), 'check')->addExtraClass('columns'));
 		return $fields;
 	}
 
