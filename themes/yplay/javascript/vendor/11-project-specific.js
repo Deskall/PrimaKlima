9496728ac3a03e8366d25ce13444382adab3179c
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-18 12:44:44 +0200

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	if ($(".sidebar-menu").length > 0){
-		var sidebarWidth = Math.ceil($(".sidebar-menu").width()) / 4;
+		var sidebarWidth = Math.ceil($(".sidebar-menu").innerWidth()) / 4;
 		console.log(sidebarWidth);
 		$(".sidebar-menu").css("transform-origin", 'calc(50% + '+sidebarWidth+'px) 0');
 		$(".sidebar-menu").show();
