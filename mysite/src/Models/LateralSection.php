f2355a06b11c42df7d515fa346be2e8b9ee98f25
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-18 11:23:13 +0200

    Update LateralSection.php

--- mysite/src/Models/LateralSection.php
+++ mysite/src/Models/LateralSection.php
@@ -1,9 +1,22 @@
 <?php
-
+use SilverStripe\SiteConfig\SiteConfig;
 
 class LateralSection extends MenuSection{
 
 	private static $singular_name = 'Sidebar Menu';
 	private static $plural_name = 'Sidebar Menus';
+
+	private static $db = [
+		'ButtonTitle' => 'Varchar(255)',
+		'ButtonFarbe' => 'Varchar(255)'
+	];
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->removeByName('Image');
+		$fields->insertAfter('ButtonTitle',HTMLDropdownField::create('ButtonBackground',_t('Form.ButtonBackground','Button Hintergrundfarbe'),SiteConfig::current_site_config()->getBackgroundColors())->addExtraClass('colors'));
+
+		return $fields;
+	}
 
 }
