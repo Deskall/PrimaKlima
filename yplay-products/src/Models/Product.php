1fc1199a314991630c48fb2224798e3473a2a2a1
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-10-24 13:00:39 +0200

    Update Product.php

--- yplay-products/src/Models/Product.php
+++ yplay-products/src/Models/Product.php
@@ -1,13 +1,17 @@
 <?php
 
 use SilverStripe\ORM\DataObject;
+use SilverStripe\ORM\FieldType\DBText;
 
 class Product extends DataObject {
 	private static $db = [
 	'ProductCode' => 'Varchar',
 	'Title' => 'Varchar',
-	'Price' => 'Currency',
+	'MonthlyPrice' => 'Currency',
+	'UniquePrice' => 'Currency',
+	'UniquePriceLabel' => 'Varchar',
 	'Unit' => 'Varchar',
+	'RecurringPrice' => 'Boolean(1)',
 	'Subtitle' => 'Text',
 	'Description' => 'HTMLText'
 	];
@@ -20,4 +24,42 @@ class Product extends DataObject {
 		'Sortable',
 		'Activable'
 	];
+
+	private static $summary_fields = [
+		'Title',
+		'Subtitle',
+		'PrintPriceString'
+	];
+
+	public function onBeforeWrite(){
+	    if (!$this->ProductCode){
+	    	$this->ProductCode = URLSegmentFilter::create()->filter($this->Title);
+	    }
+		parent::onBeforeWrite();
+	}
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->removeByName('ProductCode');
+		return $fields;
+	}
+
+	public function fieldLabels($includerelation = true){
+		$labels = parent::fieldLabels($includerelation);
+		$labels['Title'] = 'Name';
+		$labels['Subtitle'] = 'Untertitel';
+		$labels['Description'] = 'Beschreibung';
+		$labels['UniquePrice'] = ($this->RecurringPrice) ? 'Einmaliger Preis' : 'Preis';
+		$labels['MonthlyPrice'] = 'Montalicher Preis';
+		$labels['Unit'] = 'Einheit';
+		$labels['RecurringPrice'] = 'Monatlicher Preis?';
+
+		return $labels;
+	}
+
+	public function PrintPriceString(){
+		if ($this->RecurringPrice){
+			return DBText::create()->setValue('CHF '.$this->MonthlyPrice.' / Mt.');
+		}
+	}
 }
