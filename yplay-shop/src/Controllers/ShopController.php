42b1c7b6c69e99158c9eb6704edd270ee12b4a0b
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-01 15:27:33 +0100

    Update ShopController.php

--- yplay-shop/src/Controllers/ShopController.php
+++ yplay-shop/src/Controllers/ShopController.php
@@ -16,6 +16,6 @@ class ShopController extends PageController
    private static $allowed_actions = ['fetchPackages']; 
 
    public function fetchPackages(){
-   	return Package::get()->count();
+   	return Package::get()->toArray();
    } 
 }
