ea25dabbe980282b347662150e657dec9a3413d7
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-05 17:16:05 +0100

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -93,6 +93,7 @@ $(document).ready(function(){
 	};
 
 	function UpdateOrderPreview(packageID, products){
+		console.log(products);
 		$.ajax({
 			url: '/shop-functions/fetchCart',
 			method: 'POST',
