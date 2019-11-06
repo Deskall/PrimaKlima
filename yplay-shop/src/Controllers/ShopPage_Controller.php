0261269f80ecc8b274d4c72b77e68d0e398a7d19
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-06 16:08:19 +0100

    Update ShopPage_Controller.php

--- yplay-shop/src/Controllers/ShopPage_Controller.php
+++ yplay-shop/src/Controllers/ShopPage_Controller.php
@@ -13,6 +13,10 @@ class ShopPage_Controller extends PageController
          $cart = ShopCart::get()->byId($id);
       }
       if (!$cart ){
+         ob_start();
+                  print_r('ici');
+                  $result = ob_get_clean();
+                  file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
          return $this->redirect($this->ConfiguratorPage()->Link(), 302);
       }
    }
