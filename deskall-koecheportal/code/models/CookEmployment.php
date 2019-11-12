3cff658b145c95f3d0774b914d686e451c697cb0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-12 17:24:46 +0100

    Update CookEmployment.php

--- deskall-koecheportal/code/models/CookEmployment.php
+++ deskall-koecheportal/code/models/CookEmployment.php
@@ -49,7 +49,7 @@ class CookEmployment extends DataObject {
 		return true;
 	}
 
-	public function canCreate( $member = NULL, $context = Array ){
+	public function canCreate( $member = NULL, $context = array() ){
 		return true;
 	}
 
