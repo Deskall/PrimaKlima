9236a5fa8bb7ae6899b2cc7beb2ef0ec9a488ff9
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-13 12:24:44 +0100

    Update ConfiguratorPage.php

--- yplay-shop/src/Models/ConfiguratorPage.php
+++ yplay-shop/src/Models/ConfiguratorPage.php
@@ -3,6 +3,12 @@
 class ConfiguratorPage extends Page {
 	private static $singular_name = "Abo Konfigurator";
 
+	private static $db = [
+		'OtherOffersTitle' => 'Varchar',
+		'OtherOffersLabel' => 'HTMLText',
+		'ConditionsText' => 'HTMLText'
+	];
+
 	public function canCreate($member = null, $context = array()){
 		return ConfiguratorPage::get()->count() == 0;
 	}
