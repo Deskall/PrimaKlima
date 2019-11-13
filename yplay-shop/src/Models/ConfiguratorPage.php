4047d575c668d11fa9f3c69389e94c9623a98d90
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-13 13:19:42 +0100

    w

--- yplay-shop/src/Models/ConfiguratorPage.php
+++ yplay-shop/src/Models/ConfiguratorPage.php
@@ -1,5 +1,8 @@
 <?php
 
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
+
 class ConfiguratorPage extends Page {
 	private static $singular_name = "Abo Konfigurator";
 
@@ -9,11 +12,31 @@ class ConfiguratorPage extends Page {
 		'ConditionsText' => 'HTMLText'
 	];
 
+	public function fieldLabels($includerelation = true){
+		$labels = parent::fieldLabels($includerelation);
+		$labels['OtherOffersTitle'] = 'Weitere Angebote Titel';
+		$labels['OtherOffersLabel'] = 'Weitere Angebote Text';
+		$labels['ConditionsText'] = 'Konditionen';
+
+		return $labels;
+	}
+
 	public function canCreate($member = null, $context = array()){
 		return ConfiguratorPage::get()->count() == 0;
 	}
 
 	public function activeCategories(){
 		return ProductCategory::get()->filter('isVisible',1);
+	}
+
+	public function getCMSFields(){
+		$fields = parent::getCMSFields();
+		$fields->removeByName('BlockTitle');
+		$fields->removeByName('ElementalArea');
+		$fields->addFieldsToTab('Root.Main',[
+			TextField::create('OtherOffersTitle',$this->fieldLabels()['OtherOffersTitle']),
+			HTMLEditorField::create('OtherOffersLabel',$this->fieldLabels()['OtherOffersLabel']),
+			HTMLEditorField::create('ConditionsText',$this->fieldLabels()['ConditionsText'])
+		]);
 	}
 }
--- yplay-shop/src/Models/Product.php
+++ yplay-shop/src/Models/Product.php
@@ -82,7 +82,7 @@ class Product extends DataObject {
 
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
-		// $fields->removeByName('ProductCode');
+		$fields->removeByName('ProductCode');
 		$fields->removeByName('CategoryID');
 		$fields->removeByName('FooterText');
 
