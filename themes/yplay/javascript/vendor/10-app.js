0d82659c445129071d4a1ae62b7651ca7c370538
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-13 11:19:51 +0100

    Update 10-app.js

--- themes/yplay/javascript/vendor/10-app.js
+++ themes/yplay/javascript/vendor/10-app.js
@@ -206,5 +206,6 @@ $( window ).resize(function() {
 function resizeMain(){
   var screen = $( window ).height();
   var h = $('header').outerHeight() + $('footer').outerHeight();
+  console.log(h);
   $('main').css({ minHeight: `${screen - h}px` });
 }
