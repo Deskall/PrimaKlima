93e51c6626141a9e5c9791126c2dc73f76ad8e54
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-13 09:13:41 +0100

    r

---
+++ deskall-koecheportal/code/blocks/SearchBarBlock.php
@@ -0,0 +1,19 @@
+<?php
+
+use DNADesign\Elemental\Models\BaseElement;
+use SilverStripe\ORM\FieldType\DBField;
+
+class SearchBarBlock extends BaseElement {
+	private static $icon = 'font-icon-search';
+	
+	private static $controller_template = 'BlockHolder';
+
+	private static $controller_class = BlockController::class;
+
+	private static $help_text = "Job Suchbar";
+
+	public function getType()
+	{
+	    return _t(__CLASS__ . '.BlockType', 'Job Suche');
+	}
+}
