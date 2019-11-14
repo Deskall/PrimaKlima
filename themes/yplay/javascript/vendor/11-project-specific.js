7596626a90874565b8e14a881c2e16b1427ccdfb
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-14 16:30:59 +0100

    w

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -131,12 +131,7 @@ $(document).ready(function(){
 			if ($(this).attr('data-index') > 0){
 				index = parseInt($(this).find('li[data-product-id="'+$(this).attr('data-index')+'"]').attr('data-index')) - 1;
 			}
-			
-			options = {
-				center: true,
-				index: index
-			};
-			UIkit.slider("#"+$(this).attr('id'),options).show(index);
+			UIkit.slider($(this)).show(index);
 		});
 	}
 
