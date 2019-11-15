9046af186248910e89ea2db13c03a7417a476fda
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-15 16:01:42 +0100

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -172,9 +172,12 @@ $(document).ready(function(){
 			UIkit.switcher("#order-nav-switcher").show($(this).attr('data-target'));
 			$("#order-nav").find('li.uk-active').removeClass('uk-active');
 			var nav = $("#order-nav").find('li[data-nav="'+$(this).attr('data-nav')+'"]');
-			nav.removeClass('dk-inactive');
-			//Update cart steps
-			UpdateCartStep(nav.attr('data-nav'));
+			if (nav.hasClass('dk-inactive')){
+				nav.removeClass('dk-inactive');
+				//Update cart steps
+				UpdateCartStep(nav.attr('data-nav'));
+			}
+		
 			
 			if (!nav.hasClass('uk-active')){
 				nav.addClass('uk-active');
