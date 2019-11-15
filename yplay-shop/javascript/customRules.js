302f021cdceb5bf39c071bf90f4d835d08be8867
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-15 18:46:31 +0100

    Update customRules.js

--- yplay-shop/javascript/customRules.js
+++ yplay-shop/javascript/customRules.js
@@ -17,9 +17,11 @@ $.validator.addMethod("minDate", function(value, element,attr) {
 
 $.validator.addMethod("intlTelNumber", function(value, element) {
     phone_number = value.replace( /\s+/g, "" );
-    console.log(phone_number);
-return phone_number == "" ||
-    phone_number.match( /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{1,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/ ) ||
-    phone_number.match( /^(0|0041|\+41)?[1-9\s][0-9\s]{1,12}$/ )
-    ;
+    if (phone_number){
+    	return 
+	    phone_number.match( /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{1,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/ ) ||
+	    phone_number.match( /^(0|0041|\+41)?[1-9\s][0-9\s]{1,12}$/ )
+	    ;
+    }
+    return true;
 }, "Bitte geben Sie eine gültige schweizer oder deutsche Telefonnummer ein");
