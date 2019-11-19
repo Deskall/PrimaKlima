83765cbf2461e1292661fb138b22a670d7cb865d
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-19 10:49:06 +0100

    Update customRules.js

--- yplay-shop/javascript/customRules.js
+++ yplay-shop/javascript/customRules.js
@@ -1,27 +1,27 @@
-$.validator.addMethod("maxDate", function(value, element,attr) {
-    var maxDate = new Date(attr);
-    var inputDate = new Date(value);
-    if (inputDate > maxDate)
-        return false;
-    return true;
-}, 'Sie müssen mindestens 18 Jahre alt sein, um auf unserer Website bestellen zu können');
-$.validator.addMethod("minDate", function(value, element,attr) {
-    var maxDate = new Date(attr);
-    var inputDate = new Date(value);
-    if (inputDate < minDate)
-        return false;
-    return true;
-}, 'Ungültiges Datum');
+// $.validator.addMethod("maxDate", function(value, element,attr) {
+//     var maxDate = new Date(attr);
+//     var inputDate = new Date(value);
+//     if (inputDate > maxDate)
+//         return false;
+//     return true;
+// }, 'Sie müssen mindestens 18 Jahre alt sein, um auf unserer Website bestellen zu können');
+// $.validator.addMethod("minDate", function(value, element,attr) {
+//     var maxDate = new Date(attr);
+//     var inputDate = new Date(value);
+//     if (inputDate < minDate)
+//         return false;
+//     return true;
+// }, 'Ungültiges Datum');
 
 
 
-$.validator.addMethod("intlTelNumber", function(value, element) {
-    phone_number = value.replace( /\s+/g, "" );
-    if (phone_number){
-    	return 
-	    phone_number.match( /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{1,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/ ) ||
-	    phone_number.match( /^(0|0041|\+41)?[1-9\s][0-9\s]{1,12}$/ )
-	    ;
-    }
-    return true;
-}, "Bitte geben Sie eine gültige schweizer oder deutsche Telefonnummer ein");
+// $.validator.addMethod("intlTelNumber", function(value, element) {
+//     phone_number = value.replace( /\s+/g, "" );
+//     if (phone_number){
+//     	return 
+// 	    phone_number.match( /^([\+][0-9]{1,3}[ \.\-])?([\(]{1}[0-9]{1,6}[\)])?([0-9 \.\-\/]{3,20})((x|ext|extension)[ ]?[0-9]{1,4})?$/ ) ||
+// 	    phone_number.match( /^(0|0041|\+41)?[1-9\s][0-9\s]{1,12}$/ )
+// 	    ;
+//     }
+//     return true;
+// }, "Bitte geben Sie eine gültige schweizer oder deutsche Telefonnummer ein");
