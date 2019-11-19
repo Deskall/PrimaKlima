cd35a5dda0bce58f1b8e7cd133ff7cf7d24d3320
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-19 11:32:08 +0100

    Update customRules.js

--- yplay-shop/javascript/customRules.js
+++ yplay-shop/javascript/customRules.js
@@ -17,9 +17,9 @@ $.validator.addMethod("minDate", function(value, element,attr) {
 
 $.validator.addMethod("intlTelNumber", function(value, element) {
     phone_number = value.replace( /\s+/g, "" );
-    if(){
-        console.log('OK');
-    }
+    // if(){
+    //     console.log('OK');
+    // }
     if (phone_number){
     	return phone_number.match(/^(\+41|0041|0){1}(\(0\))?[0-9]{9}$/)
 	    // phone_number.match( /^(0|0041|\+41)?[1-9\s][0-9\s]{1,12}$/ )
