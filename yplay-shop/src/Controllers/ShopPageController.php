4c99c61912d85c39b8efa81bc333d3e12bb24f1d
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-19 14:43:54 +0100

    Update ShopPageController.php

--- yplay-shop/src/Controllers/ShopPageController.php
+++ yplay-shop/src/Controllers/ShopPageController.php
@@ -50,7 +50,7 @@ class ShopPageController extends PageController
       Requirements::javascript('yplay-shop/javascript/messages_de.min.js');
       Requirements::javascript('yplay-shop/javascript/customRules.js');
       $date = new \DateTime();
-      $date->modify('-18 years');
+      $val = $date->modify('-18 years');
       $max = $date->modify('+ 1 day');
       $form = new Form(
          $this,
@@ -63,7 +63,7 @@ class ShopPageController extends PageController
                TextField::create('FirstName','Vorname')->setAttribute('class','uk-input'),
                EmailField::create('Email','E-Mail')->setAttribute('class','uk-input'),
                TextField::create('Phone','Tel.')->setAttribute('class','uk-input')->setAttribute('intlTelNumber',true),
-               DateField::create('Birthday','Geburstdatum')->setValue($date->format('Y-m-d'))->setAttribute('minDate','1900.01.01')->setAttribute('maxDate',$max->format('Y.m.d'))->setAttribute('class','uk-input'),
+               DateField::create('Birthday','Geburstdatum')->setValue($val->format('Y-m-d'))->setAttribute('minDate','1900.01.01')->setAttribute('maxDate',$max->format('Y.m.d'))->setAttribute('class','uk-input'),
                TextField::create('Address','Adresse')->setAttribute('class','uk-input'),
                TextField::create('PostalCode','PLZ')->setAttribute('class','uk-input'),
                TextField::create('City','Stadt')->setAttribute('class','uk-input'),
