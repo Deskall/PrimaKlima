507e8a416db22f31d207502d09409f18bf07282a
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-20 13:32:58 +0100

    Update Product.php

--- yplay-shop/src/Models/Product.php
+++ yplay-shop/src/Models/Product.php
@@ -117,7 +117,7 @@ class Product extends DataObject {
 
 	public function getCMSFields(){
 		$fields = parent::getCMSFields();
-		$fields->removeByName('ProductCode');
+		// $fields->removeByName('ProductCode');
 		$fields->removeByName('CategoryID');
 		$fields->removeByName('FooterText');
 		
