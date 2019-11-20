f4d12a17843b4a590ba843f61a9827e6729f5282
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-20 08:43:05 +0100

    Update ProductAdmin.php

--- yplay-shop/src/Models/ProductAdmin.php
+++ yplay-shop/src/Models/ProductAdmin.php
@@ -66,6 +66,9 @@ class ProductAdmin extends ModelAdmin{
 		if($this->modelClass == 'Product') {
 	      $list = $list->filter('ClassName','Product');
 	    }
+	    if($this->modelClass == 'ShopCart') {
+	      $list = $list->filter('Purchased',0);
+	    }
 		return $list;
 	}
 
