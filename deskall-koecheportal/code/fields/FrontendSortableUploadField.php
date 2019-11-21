5d842bda5903409f8eed0a9f2074a8bef2235718
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-21 12:14:33 +0100

    w

--- deskall-koecheportal/code/fields/FrontendSortableUploadField.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    class FrontendSortableUploadField extends FrontendUploadField {
-
-
-
-	}
--- deskall-koecheportal/code/models/EmployerConfigItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-use SilverStripe\ORM\DataObject;
-class EmloyerConfigItem extends DataObject{
-	private static $db = array(
-		'Title__de_DE' => 'Varchar(255)'
-	);
-}
-
-
