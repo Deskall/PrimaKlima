e1875d4b33abbf4c9aa5e64b14998e210273e1ab
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-21 15:05:36 +0100

    Update TopJobBlock.php

--- deskall-job-portal/src/blocks/TopJobBlock.php
+++ deskall-job-portal/src/blocks/TopJobBlock.php
@@ -13,36 +13,36 @@ class TopJobBlock extends BaseElement implements Searchable{
 
 	public function Jobs(){
 
-		$AllAdvertisements = EmployerAdvertisement::get()->filter(array(
-			'isPaid' => true, 
-			'State'  => 'live'
-		))->sort('RAND()');
+		// $AllAdvertisements = EmployerAdvertisement::get()->filter(array(
+		// 	'isPaid' => true, 
+		// 	'State'  => 'live'
+		// ))->sort('RAND()');
 
-		$NonFlatRateAds = $AllAdvertisements->filter(array(
-			'PackageID' => 2,
-			'StartDate:LessThanOrEqual' => date("Y-m-d"),
-			'EndDate:GreaterThanOrEqual' => date("Y-m-d"),
-		))->sort('RAND()');
+		// $NonFlatRateAds = $AllAdvertisements->filter(array(
+		// 	'PackageID' => 2,
+		// 	'StartDate:LessThanOrEqual' => date("Y-m-d"),
+		// 	'EndDate:GreaterThanOrEqual' => date("Y-m-d"),
+		// ))->sort('RAND()');
 
-		$FlatRateAds = $AllAdvertisements->filter(array(
-			'PackageID' => 3,
-			'StartDate:LessThanOrEqual' => date("Y-m-d"),
-			'Employer.FlatrateEndDate:GreaterThanOrEqual' => date("Y-m-d"),
-		))->sort('RAND()');
-
-
-		$Advertisements = new ArrayList;
-
-		foreach( $NonFlatRateAds as $ad){$Advertisements->push($ad);}
-		foreach( $FlatRateAds as $ad){$Advertisements->push($ad);};
+		// $FlatRateAds = $AllAdvertisements->filter(array(
+		// 	'PackageID' => 3,
+		// 	'StartDate:LessThanOrEqual' => date("Y-m-d"),
+		// 	'Employer.FlatrateEndDate:GreaterThanOrEqual' => date("Y-m-d"),
+		// ))->sort('RAND()');
 
 
+		// $Advertisements = new ArrayList;
+
+		// foreach( $NonFlatRateAds as $ad){$Advertisements->push($ad);}
+		// foreach( $FlatRateAds as $ad){$Advertisements->push($ad);};
 
 
 
 
 
-		return $Advertisements->sort('RAND()');
+
+
+		// return $Advertisements->sort('RAND()');
 	}
 
 
