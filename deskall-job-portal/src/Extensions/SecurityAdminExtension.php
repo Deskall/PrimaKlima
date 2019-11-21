18e797c0359d07e6dd426583c71d13ff58dfc8bd
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-21 14:49:58 +0100

    w

--- deskall-job-portal/src/Extensions/SecurityAdminExtension.php
+++ deskall-job-portal/src/Extensions/SecurityAdminExtension.php
@@ -9,9 +9,14 @@ class SecurityAdminExtension extends Extension {
 	public function updateEditForm($form){
 		$members = $form->Fields()->FieldByName('Root.Users.Members');
 		//exclude other members group
-		$idsCandidat = Group::get()->filter('Code','kandidaten')->first()->Members()->column('ID');
-		$idsCustomer = Group::get()->filter('Code','arbeitgeber')->first()->Members()->column('ID');
-		$ids = array_merge($idsCandidat,$idsCustomer);
+		//Candidat
+		$groupCandidat = Group::get()->filter('Code','kandidaten')->first();
+		$idsCandidat = ($groupCandidat && $groupCandidat->Members()->exists()) ? $groupCandidat->Members()->column('ID') : [];
+		//Employer
+		$groupEmployer = Group::get()->filter('Code','arbeitgeber')->first();
+		$idsEmployer = ($groupEmployer  && $groupEmployer ->Members()->exists()) ? $groupEmployer->Members()->column('ID') : [];
+	
+		$ids = array_merge($idsCandidat,$idsEmployer);
 		$members->setList(Member::get()->exclude('ID',$ids));
 	}
 }
