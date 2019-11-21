7bb3d85cb4713ac03225b3add48048d9d2accc5d
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-21 15:41:50 +0100

    Update JobParameter.php

--- deskall-job-portal/src/Model/JobParameter.php
+++ deskall-job-portal/src/Model/JobParameter.php
@@ -37,6 +37,11 @@ class JobParameter extends DataObject
        'Title'
     ];
 
+    private static $extensions = [
+        'Sortable',
+        'Activable'
+    ];
+
 
     public function fieldLabels($includerelation = true){
     $labels = parent::fieldLabels($includerelation);
