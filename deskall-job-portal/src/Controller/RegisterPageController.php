749334daa233ebf578cc22dde92e5f9a44debb0b
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-22 08:57:39 +0100

    Update RegisterPageController.php

--- deskall-job-portal/src/Controller/RegisterPageController.php
+++ deskall-job-portal/src/Controller/RegisterPageController.php
@@ -47,7 +47,7 @@ class RegisterPageController extends PageController{
 			'RegisterForm',
 			$fields,
 			new FieldList(
-				FormAction::create('register', _t('MemberProfiles.REGISTER', 'Jetzt registrieren'))->addExtraClass('uk-button')
+				FormAction::create('register', _t('MemberProfiles.REGISTER', 'Jetzt registrieren'))->addExtraClass('uk-button uk-float-right')
 			)
 		);
 
