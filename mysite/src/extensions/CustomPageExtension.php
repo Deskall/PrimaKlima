cf76efad38a5c55f6907f9aa0455d57f55a86be0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-11-23 14:48:33 +0100

    Update CustomPageExtension.php

--- mysite/src/extensions/CustomPageExtension.php
+++ mysite/src/extensions/CustomPageExtension.php
@@ -7,7 +7,8 @@ use SilverStripe\Security\Security;
 use SilverStripe\Subsites\Model\Subsite;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\GridField\GridField;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
+use SilverStripe\Forms\GridField\GridFieldConfig_RelationEditor;
+use SilverStripe\Forms\GridField\GridFieldDeleteAction;
 use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
 
 class CustomPageExtension extends DataExtension
@@ -20,13 +21,13 @@ class CustomPageExtension extends DataExtension
         $fields->removeByName('MenuSections');
         if ($this->owner->ShowInMainMenu){
             $fields->addFieldToTab('Root.Menu',
-                GridField::create('MenuSections','Menu Sektionen',$this->owner->MenuSections()->filter('ClassName',MenuSection::class),GridFieldConfig_RecordEditor::create()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction()))
+                GridField::create('MenuSections','Menu Sektionen',$this->owner->MenuSections()->filter('ClassName',MenuSection::class),GridFieldConfig_RelationEditor::create()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction()))
             );
         }
 
         $fields->removeByName('LateralSections');
         $fields->addFieldToTab('Root.Sidebar',
-             GridField::create('LateralSections','Sidebar Menu Sektionen',$this->owner->LateralSections(),GridFieldConfig_RecordEditor::create()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction()))
+             GridField::create('LateralSections','Sidebar Menu Sektionen',$this->owner->LateralSections(),GridFieldConfig_RelationEditor::create()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction())->addComponent(new GridFieldDeleteAction()))
         );
         
     }
