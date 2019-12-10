2e21d03d0f3137272482314e90aa045d27e2a7fe
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-10 15:04:08 +0100

    Update PackageConfigItem.php

--- deskall-shop/src/Models/PackageConfigItem.php
+++ deskall-shop/src/Models/PackageConfigItem.php
@@ -3,13 +3,13 @@ use SilverStripe\ORM\DataObject;
 
 class PackageConfigItem extends DataObject{
 	private static $db = array(
-		'Title__de_DE' => 'Varchar(255)',
+		'Title' => 'Varchar(255)',
 		'SortOrder' => 'Int',
-		'Description__de_DE' => 'HTMLText'
+		'Description' => 'HTMLText'
 	);
 
 	private static $summary_fields = array(
-		'Title__de_DE' => 'Titel',
+		'Title' => 'Titel',
 	);
 
 
