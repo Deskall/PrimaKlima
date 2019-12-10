61a6f219bac2699c0ba89d26041fd43151aefaef
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-10 15:05:37 +0100

    Update PackageConfigItem.php

--- deskall-shop/src/Models/PackageConfigItem.php
+++ deskall-shop/src/Models/PackageConfigItem.php
@@ -3,18 +3,29 @@ use SilverStripe\ORM\DataObject;
 
 class PackageConfigItem extends DataObject{
 	private static $db = array(
-		'Title' => 'Varchar(255)',
-		'SortOrder' => 'Int',
-		'Description' => 'HTMLText'
+		'Title__de_DE' => 'Varchar(255)',
+		'Description__de_DE' => 'HTMLText'
 	);
 
 	private static $summary_fields = array(
-		'Title' => 'Titel',
+		'Title__de_DE' => 'Titel',
 	);
+
+	private static $extensions = [
+		'Activable',
+		'Sortable'
+	];
 
 
 	private static $singular_name = 'Feature';
 	private static $plural_name = 'Features';
+
+	public function fieldLabels($includerelation = true){
+		$labels = parent::fieldLabels($includerelation);
+		$labels['Title__de_DE'] = 'Titel';
+		$labels['Description__de_DE'] = 'Beschreibung';
+		return $labels;
+	}
 
 
 }
