b0590c522cb9c53618643c9dbb1e043379a80ab0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-11 10:51:53 +0100

    Update ShopPageController.php

--- deskall-shop/src/Controllers/ShopPageController.php
+++ deskall-shop/src/Controllers/ShopPageController.php
@@ -44,7 +44,7 @@ class ShopPageController extends PageController{
 
 	public function init(){
 		parent::init();
-		Requirements::javascript("https://www.paypal.com/sdk/js?client-id=".$config->PayPalClientID."&currency=EUR&locale=de_DE");
+		Requirements::javascript("https://www.paypal.com/sdk/js?client-id=".SiteConfig::current_site_config()->PayPalClientID."&currency=EUR&locale=de_DE");
 	}
 
 	public function ProductDetails(HTTPRequest $request){
