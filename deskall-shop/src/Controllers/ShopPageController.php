9c0bc8dbd5b7e006bcfd5fae246a657d7dfa00c1
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-12 16:02:33 +0100

    Update ShopPageController.php

--- deskall-shop/src/Controllers/ShopPageController.php
+++ deskall-shop/src/Controllers/ShopPageController.php
@@ -120,7 +120,7 @@ class ShopPageController extends PageController{
 				DropdownField::create('BillingAddressCountry',_t(__CLASS__.'.BillingAddressCountry','Land'))->setSource(i18n::getData()->getCountries())->setValue('de')->setAttribute('class','uk-select')->setEmptyString(_t(__CLASS__.'.CountryLabel','Land wählen'))
 			)->setName('BillFields'),
 			CompositeField::create(
-				TextareaField::create('Comments','Bemerkungen')
+				TextareaField::create('Comments','Bemerkungen'),
 				CheckboxField::create('AGB',_t(__CLASS__.'.AGB','Hiermit bestätige ich, dass ich sowohl die <a href="{link}" target="_blank">Datenschutzerklärung</a> wie auch die <a href="{link2}" target="_blank">AGB</a> gelesen habe und mit beiden einverstanden bin. *', ['link' => 'services/datenschutzerklaerung', 'link2' => 'services/agb']),
 				NocaptchaField::create('Captcha')
 				
