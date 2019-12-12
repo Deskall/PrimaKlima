6f25dd5e8260e7bf9362e823eaf5cb8334e5dcf5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-12 17:07:36 +0100

    Update shop.js

--- deskall-shop/javascript/shop.js
+++ deskall-shop/javascript/shop.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
 	//Package Selection
 	$(document).on("click","[data-package-choice]",function(){
-		$("#Form_CheckoutForm_PackageID").val($(this).attr('data-package-choice'));
+		$("#Form_CheckoutForm_ProductID").val($(this).attr('data-package-choice'));
 		$("#summary-package-"+$(this).attr('data-package-choice')).attr('hidden',false);
 		UIkit.tab('#tab-switcher').show(1);
 	});
