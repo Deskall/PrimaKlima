8ec38a68260a7efdf4b2fd4b93720e660118e618
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-17 17:29:30 +0100

    Update jobportal.js

--- deskall-job-portal/javascript/jobportal.js
+++ deskall-job-portal/javascript/jobportal.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
 		var id = $(this).attr('data-edit-offer');
 		$.ajax({
 			url: window.location.pathname+'JobOfferForm',
-			dataType: 'html'
+			dataType: 'html',
+			data: {offerId: id},
+			method: 'POST'
 		}).done(function(response){
 			$('.toggle-new-offer').attr('hidden','hidden');
 			$("#edit-form-container").empty().append(response).attr('hidden',false);
