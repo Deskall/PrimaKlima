f58c10e70d978c2bf47d46ff21a498da3fb0d18c
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-17 13:20:33 +0100

    w

--- deskall-job-portal/src/Controller/OfferPageController.php
+++ deskall-job-portal/src/Controller/OfferPageController.php
@@ -47,9 +47,10 @@ use SilverStripe\Forms\CheckboxSetField;
 
 class OfferPageController extends PageController{
 
-	private static $allowed_actions = ['OfferForm','confirmMission','candidate'];
+	private static $allowed_actions = ['OfferForm','confirmMission','candidate','JobOffer'];
 
 	private static $url_handlers = [
+		'details/$ID' => 'JobOffer',
 		'bestaetigung/$ID' => 'confirmMission',
 		'bewerben/$ID' => 'candidate'
 	];
@@ -65,171 +66,181 @@ class OfferPageController extends PageController{
     {
         return ModalController::create($this, "Modals");
     }
+
+    public function JobOffer(HTTPRequest $request){
+		$id = $request->param('ID');
+		if ($id){
+			$offer = Mission::get()->byId($id);
+			if ($offer){
+				return ['Offer' => $offer];
+			} 
+		return $this->httpError(404);
+    }
 	
-	public function OfferForm(){
-		Requirements::javascript('silverstripe/admin: client/dist/js/vendor.js');
-		Requirements::javascript('silverstripe/admin: client/dist/js/bundle.js');
+	// public function OfferForm(){
+	// 	Requirements::javascript('silverstripe/admin: client/dist/js/vendor.js');
+	// 	Requirements::javascript('silverstripe/admin: client/dist/js/bundle.js');
 		
-		$mission = Mission::create();
-		$customer = Customer::create();
+	// 	$mission = Mission::create();
+	// 	$customer = Customer::create();
 		
-		$fields = $mission->getFrontEndFields();
-		$fields->removeByName('Status');
-		$fields->removeByName('Title');
-		$fields->removeByName('CustomerID');
-		$fields->removeByName('JobID');
-		$fields->removeByName('CookID');
-		$fields->removeByName('Position');
-		$fields->removeByName('Price');
-		$fields->removeByName('Country');
-		$fields->removeByName('AdminComments');
-		$fields->removeByName('Price');
-		$fields->removeByName('Anfahrt');
-		$fields->removeByName('CostAndHousing');
-		$fields->removeByName('Period');
-		$fields->fieldByName('Start')->setAttribute('class','flatpickr')->setAttribute('type','text');
-		$fields->fieldByName('End')->setAttribute('class','flatpickr')->setAttribute('type','text');
-		$fields->removeByName('Access');
-		$fields->fieldByName('Others')->setRows(3);
-		$sources = CookJob::get()->map('ID','FullTitle');
-		$sources->push('other',_t('CookJob.Other','Andere'));
-		$fields->insertBefore('Start',DropdownField::create('Position','Position',$sources)->setAttribute('class','uk-select')->setEmptyString('Position wählen'));
+	// 	$fields = $mission->getFrontEndFields();
+	// 	$fields->removeByName('Status');
+	// 	$fields->removeByName('Title');
+	// 	$fields->removeByName('CustomerID');
+	// 	$fields->removeByName('JobID');
+	// 	$fields->removeByName('CookID');
+	// 	$fields->removeByName('Position');
+	// 	$fields->removeByName('Price');
+	// 	$fields->removeByName('Country');
+	// 	$fields->removeByName('AdminComments');
+	// 	$fields->removeByName('Price');
+	// 	$fields->removeByName('Anfahrt');
+	// 	$fields->removeByName('CostAndHousing');
+	// 	$fields->removeByName('Period');
+	// 	$fields->fieldByName('Start')->setAttribute('class','flatpickr')->setAttribute('type','text');
+	// 	$fields->fieldByName('End')->setAttribute('class','flatpickr')->setAttribute('type','text');
+	// 	$fields->removeByName('Access');
+	// 	$fields->fieldByName('Others')->setRows(3);
+	// 	$sources = CookJob::get()->map('ID','FullTitle');
+	// 	$sources->push('other',_t('CookJob.Other','Andere'));
+	// 	$fields->insertBefore('Start',DropdownField::create('Position','Position',$sources)->setAttribute('class','uk-select')->setEmptyString('Position wählen'));
 		
-		$fields->insertAfter('Position',TextField::create('OtherJob','Position angeben')->displayIf('Position')->isEqualTo('other')->end());
-		$fields->push(DropdownField::create('Gender',$customer->fieldLabels(false)['Gender'],['Herr' => 'Herr','Frau' => 'Frau'])->setEmptyString('Bitte wählen')->setAttribute('class','uk-select'));
-		$fields->push(TextField::create('FirstName','Vorname'));
-		$fields->push(TextField::create('Surname','Name')->addExtraClass('second'));
+	// 	$fields->insertAfter('Position',TextField::create('OtherJob','Position angeben')->displayIf('Position')->isEqualTo('other')->end());
+	// 	$fields->push(DropdownField::create('Gender',$customer->fieldLabels(false)['Gender'],['Herr' => 'Herr','Frau' => 'Frau'])->setEmptyString('Bitte wählen')->setAttribute('class','uk-select'));
+	// 	$fields->push(TextField::create('FirstName','Vorname'));
+	// 	$fields->push(TextField::create('Surname','Name')->addExtraClass('second'));
 
-		$fields->replaceField('Email',EmailField::create('Email',_t('OfferForm.EmailLabel','E-Mail-Adresse')));
+	// 	$fields->replaceField('Email',EmailField::create('Email',_t('OfferForm.EmailLabel','E-Mail-Adresse')));
 
 
-		$actions = new FieldList(FormAction::create('save', _t('Offer.SAVE', 'Auftrag erstellen'))->addExtraClass('uk-button PrimaryBackground')->setUseButtonTag(true)->setButtonContent('<i class="fa fa-check uk-margin-small-right"></i>'. _t('Offer.SAVE', 'absenden')));
+	// 	$actions = new FieldList(FormAction::create('save', _t('Offer.SAVE', 'Auftrag erstellen'))->addExtraClass('uk-button PrimaryBackground')->setUseButtonTag(true)->setButtonContent('<i class="fa fa-check uk-margin-small-right"></i>'. _t('Offer.SAVE', 'absenden')));
 		
-		$required = RequiredFields::create(['Gender','FirstName','Surname','Position','Email','PostalCode','City','Phone','Place','Start','End','JobID']);
+	// 	$required = RequiredFields::create(['Gender','FirstName','Surname','Position','Email','PostalCode','City','Phone','Place','Start','End','JobID']);
 
 
-		//Job Options
-		$jobs = CookJob::get();
-		$options = [];
-		foreach ($jobs as $job) {
-			if ($job->Options()->exists()){
-				$options[] = CheckboxSetField::create('Options-'.$job->ID,'Optionen',$job->Options()->map('ID','Title')->toArray())->displayIf('Position')->isEqualTo($job->ID)->end();
-			}
-		}
-		$fields->insertAfter('Position',CompositeField::create($options)->setName('Options'));
+	// 	//Job Options
+	// 	$jobs = CookJob::get();
+	// 	$options = [];
+	// 	foreach ($jobs as $job) {
+	// 		if ($job->Options()->exists()){
+	// 			$options[] = CheckboxSetField::create('Options-'.$job->ID,'Optionen',$job->Options()->map('ID','Title')->toArray())->displayIf('Position')->isEqualTo($job->ID)->end();
+	// 		}
+	// 	}
+	// 	$fields->insertAfter('Position',CompositeField::create($options)->setName('Options'));
 
-		$form = new Form(
-			$this,
-			'OfferForm',
-			$fields,
-			$actions,
-			$required
-		);
+	// 	$form = new Form(
+	// 		$this,
+	// 		'OfferForm',
+	// 		$fields,
+	// 		$actions,
+	// 		$required
+	// 	);
 		
-		$form->setTemplate('Forms/OfferForm');
-		$form->addExtraClass('uk-form-horizontal form-std');
-		
-
-		return $form;
-	}
-
-	public function save($data, Form $form)
-	{
+	// 	$form->setTemplate('Forms/OfferForm');
+	// 	$form->addExtraClass('uk-form-horizontal form-std');
 		
 
-		//$member = Security::getCurrentUser();
-		$mission = new Mission();
-		$form->saveInto($mission);
-		// if($member){
-		// 	$mission->CustomerID = $member->ID;
-		// }
-		// else {
-			//check for existing customer
-			$member = Member::get()->filter('Email' , $data['Email'])->first();
-			if (!$member){
-				$member = new Member();
-				$form->saveInto($member);
-				$member->write();
+	// 	return $form;
+	// }
+
+	// public function save($data, Form $form)
+	// {
+		
+
+	// 	//$member = Security::getCurrentUser();
+	// 	$mission = new Mission();
+	// 	$form->saveInto($mission);
+	// 	// if($member){
+	// 	// 	$mission->CustomerID = $member->ID;
+	// 	// }
+	// 	// else {
+	// 		//check for existing customer
+	// 		$member = Member::get()->filter('Email' , $data['Email'])->first();
+	// 		if (!$member){
+	// 			$member = new Member();
+	// 			$form->saveInto($member);
+	// 			$member->write();
 				
-			}
-			$member->addToGroupByCode('kunden');
-			$customer = ($member) ? Customer::get()->filter('memberID' , $member->ID)->first() : null;
-			if (!$customer){
-				$customer = Customer::create();
-				$form->saveInto($customer);
-				$customer->MemberID = $member->ID;
-				$customer->write();
-			}
-		// }
-		if (isset($data['Start'])){
-			$start = new \DateTime($data['Start']);
-			$mission->Start = $start->format('Y-m-d');
-		}
-		if (isset($data['End'])){
-			$end = new \DateTime($data['End']);
-			$mission->End = $end->format('Y-m-d');
-		} 
-		if ($data['Position'] == "other"){
-			$job = CookJob::create();
-			$job->Title = $data['OtherJob'];
-			$job->write();
-			$mission->Position = $data['OtherJob'];
-		}
-		else{
-			$job = CookJob::get()->byID($data['Position']);
-		}
+	// 		}
+	// 		$member->addToGroupByCode('kunden');
+	// 		$customer = ($member) ? Customer::get()->filter('memberID' , $member->ID)->first() : null;
+	// 		if (!$customer){
+	// 			$customer = Customer::create();
+	// 			$form->saveInto($customer);
+	// 			$customer->MemberID = $member->ID;
+	// 			$customer->write();
+	// 		}
+	// 	// }
+	// 	if (isset($data['Start'])){
+	// 		$start = new \DateTime($data['Start']);
+	// 		$mission->Start = $start->format('Y-m-d');
+	// 	}
+	// 	if (isset($data['End'])){
+	// 		$end = new \DateTime($data['End']);
+	// 		$mission->End = $end->format('Y-m-d');
+	// 	} 
+	// 	if ($data['Position'] == "other"){
+	// 		$job = CookJob::create();
+	// 		$job->Title = $data['OtherJob'];
+	// 		$job->write();
+	// 		$mission->Position = $data['OtherJob'];
+	// 	}
+	// 	else{
+	// 		$job = CookJob::get()->byID($data['Position']);
+	// 	}
 
-		//Options
-		$options = array_filter($data, function($key) {
-		    return strpos($key, 'Options-') === 0;
-		},ARRAY_FILTER_USE_KEY);
+	// 	//Options
+	// 	$options = array_filter($data, function($key) {
+	// 	    return strpos($key, 'Options-') === 0;
+	// 	},ARRAY_FILTER_USE_KEY);
 
-		foreach($options as $key => $value){
-			$obj = CookJobOption::get()->byID($value);
-			if ($obj){
-				$mission->Options()->add($obj);
-			}
-		}
+	// 	foreach($options as $key => $value){
+	// 		$obj = CookJobOption::get()->byID($value);
+	// 		if ($obj){
+	// 			$mission->Options()->add($obj);
+	// 		}
+	// 	}
 
-		$mission->JobID = $job->ID;
-		$mission->CustomerID = $customer->ID;
-		$mission->Status = "created";
-		$config = $mission->getCookConfig();
-		try {
-			$mission->write();
-			if (!$mission->Price){
-			    $mission->Price = $mission->calculatePrice();
-			    $mission->CustomerPrice = $mission->calculateCustomerPrice();
-			    if ($mission->Price && $mission->CustomerPrice){
-			        $mission->createOffer();
+	// 	$mission->JobID = $job->ID;
+	// 	$mission->CustomerID = $customer->ID;
+	// 	$mission->Status = "created";
+	// 	$config = $mission->getCookConfig();
+	// 	try {
+	// 		$mission->write();
+	// 		if (!$mission->Price){
+	// 		    $mission->Price = $mission->calculatePrice();
+	// 		    $mission->CustomerPrice = $mission->calculateCustomerPrice();
+	// 		    if ($mission->Price && $mission->CustomerPrice){
+	// 		        $mission->createOffer();
 			       
-			        if ($config->AutoOffer){
-			        	$mission->sendOfferMail();
-			        	$mission->Status = "sentToCustomer";
-			        }
-			        else{
-			        	$mission->sendAdminMail();
-			        }
-			        $mission->write();
-			    }
-			    else{
-			        $mission->sendAdminMail();
+	// 		        if ($config->AutoOffer){
+	// 		        	$mission->sendOfferMail();
+	// 		        	$mission->Status = "sentToCustomer";
+	// 		        }
+	// 		        else{
+	// 		        	$mission->sendAdminMail();
+	// 		        }
+	// 		        $mission->write();
+	// 		    }
+	// 		    else{
+	// 		        $mission->sendAdminMail();
 
-			    }
-			}      
-		} catch (ValidationException $e) {
-			$validationMessages = '';
-			foreach($e->getResult()->getMessages() as $error){
-				$validationMessages .= $error['message']."\n";
-			}
-			$form->sessionMessage($validationMessages, 'bad');
-			return $this->redirectBack();
-		}
-		if ($config->AutoOffer){
-			return $this->redirect($this->RedirectWithOffer()->Link());
-		}
-		return $this->redirect($this->RedirectWithoutOffer()->Link());
-	}
+	// 		    }
+	// 		}      
+	// 	} catch (ValidationException $e) {
+	// 		$validationMessages = '';
+	// 		foreach($e->getResult()->getMessages() as $error){
+	// 			$validationMessages .= $error['message']."\n";
+	// 		}
+	// 		$form->sessionMessage($validationMessages, 'bad');
+	// 		return $this->redirectBack();
+	// 	}
+	// 	if ($config->AutoOffer){
+	// 		return $this->redirect($this->RedirectWithOffer()->Link());
+	// 	}
+	// 	return $this->redirect($this->RedirectWithoutOffer()->Link());
+	// }
 
 	public function confirmMission(HTTPRequest $request){
 		$id = $request->param('ID');
