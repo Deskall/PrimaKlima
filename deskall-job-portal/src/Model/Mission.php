eb21e9e57741c1cb7e4bea7bf52d1a8a32044f8f
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-18 10:40:39 +0100

    Update Mission.php

--- deskall-job-portal/src/Model/Mission.php
+++ deskall-job-portal/src/Model/Mission.php
@@ -325,8 +325,8 @@ class Mission extends DataObject
                     $field = ListboxField::create($p->Title,$p->Title,$p->Values()->map('ID','Title'))->setAttribute('class','uk-input');
                     break;
             }
-           if ($this->Parameters()->find('Title',$p->Title)){
-               $field->setValue($this->Parameters()->find('Title',$p->Title)->Value);
+           if ($this->Parameters()->filter('Title',$p->Title)->first()){
+               $field->setValue($this->Parameters()->filter('Title',$p->Title)->first()->Value);
            }
            $parameters->push($field);
         }
