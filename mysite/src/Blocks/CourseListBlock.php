bf1da0fe96c3ba7cc182a4d8d3c7d8eb16be5f14
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2019-12-20 14:14:04 +0100

    w

--- mysite/src/Blocks/CourseListBlock.php
+++ mysite/src/Blocks/CourseListBlock.php
@@ -1,12 +1,14 @@
 <?php
 
-use SilverStripe\Forms\HTMLEditor\HtmlEditorField;
 use SilverStripe\Forms\DropdownField;
-use SilverStripe\Forms\TextareaField;
 use SilverStripe\ORM\FieldType\DBField;
 use DNADesign\Elemental\Models\BaseElement;
 use SilverStripe\Assets\Image;
 use g4b0\SearchableDataObjects\Searchable;
+use SilverStripe\Control\Director;
+use SilverStripe\Forms\TextField;
+use SilverStripe\View\ArrayData;
+use SilverStripe\ORM\ArrayList;
 
 class CourseListBlock extends BaseElement implements Searchable
 {
@@ -26,15 +28,86 @@ class CourseListBlock extends BaseElement implements Searchable
 
     private static $description = 'Kurse List';
 
+    private static $db = array(
+        'GroupID' => 'Varchar(255)'
+    );
 
-
-    public function getCMSFields()
-    {
-        
+    public function getCMSFields(){
         $fields = parent::getCMSFields();
-        
+        $fields->addFieldToTab('Root.Main', TextField::create('GroupID', 'Beyond GruppID'), 'Title');
+      
         return $fields;
     }
+
+    public function getGroupData(){
+        if ($this->GroupID){
+            $Api = new beyond_jsonKurse();
+            $data = $Api->getKursGruppen($this->GroupID);
+            if (is_array($data) && isset($data[0])){
+                return new ArrayData($data[0]);
+            }
+        }
+        return null;
+    }
+
+    public function getMainGroup(){
+        if ($this->GroupID){
+            $Api = new beyond_jsonKurse();
+            $data = $Api->getKursStruktur($this->GroupID);
+
+            if (is_array($data) && isset($data[0])){
+              
+                $MainGroupID = $data[0]->HauptGruppenID;
+                $MainGroup = $Api->getKursGruppen($MainGroupID);
+                if (is_array($MainGroup) && isset($MainGroup[0])){
+                    return new ArrayData($MainGroup[0]);
+                }
+            }
+        }
+        return null;
+    }
+
+    public function getLessons(){
+        $Api = new beyond_jsonKurse();
+        $KursStruktur = $this->getKursStruktur(); 
+
+       
+       
+        $list = array();
+        foreach ($KursStruktur as $key => $group) {
+            $data = $Api->getKurse(null,null,null,null,$group->GruppenID);
+            if (is_array($data)){
+                foreach ($data as $row)
+                $list[] = new ArrayData($row);
+            }
+        }
+      
+            
+            
+        return new ArrayList($list);
+      
+    }
+
+    public function getKursStruktur(){
+       if ($this->GroupID){
+            $Api = new beyond_jsonKurse();
+            $data = $Api->getKursStruktur($this->GroupID);
+            $list = array();
+            if (is_array($data)){
+                foreach ($data as $row){
+                     $list[] = new ArrayData($row);
+                }
+            }
+            return new ArrayList($list);
+        }
+       return null;
+    }
+
+    public function NiceDate($string){
+        return date('d-m-Y',strtotime($string));
+    }
+
+   
 
     public function getType()
     {
