2aa31a1447c3c21e08e77967f741c716dc711496
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-09 15:39:35 +0100

    Update jobsearch.js

--- deskall-job-portal/javascript/jobsearch.js
+++ deskall-job-portal/javascript/jobsearch.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	if ($("body").hasClass("OfferPage")){
 		var filter = [];
 		$(document).on("click",'[data-filter]',function(){
-			filter[] = {filter: $(this).attr('data-filter'), value: $(this).attr('data-filter-value')};
+			filter.push({filter: $(this).attr('data-filter'), value: $(this).attr('data-filter-value')});
 			console.log(filter);
 		});
 
