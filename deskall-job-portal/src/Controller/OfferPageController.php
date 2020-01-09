f080514207de2748b91f94f14cf6669051dbedb5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-09 16:17:24 +0100

    Update OfferPageController.php

--- deskall-job-portal/src/Controller/OfferPageController.php
+++ deskall-job-portal/src/Controller/OfferPageController.php
@@ -95,11 +95,7 @@ class OfferPageController extends PageController{
 			$offers = Mission::get()->filter('isActive',1);
 			foreach($filters as $key => $filter){
 				$ids = AssignedJobParameter::get()->filter(['Title' => $filter['filter'], 'Value' => $filter['value']])->column('MissionID');
-				ob_start();
-				print_r($ids);
-				$result = ob_get_clean();
-				file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result,FILE_APPEND);
-				array_merge($filteredIDS,$ids);
+				$filteredIDS = array_merge($ids,$filteredIDS);
 			}
 			ob_start();
 				print_r($filteredIDS);
