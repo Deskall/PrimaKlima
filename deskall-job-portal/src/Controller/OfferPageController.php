4d7b06c7202c523b3273d9714b31dc4998dcfc4e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-09 17:40:47 +0100

    Update OfferPageController.php

--- deskall-job-portal/src/Controller/OfferPageController.php
+++ deskall-job-portal/src/Controller/OfferPageController.php
@@ -95,7 +95,7 @@ class OfferPageController extends PageController{
 				if ($filter['dataType'] == "parameter"){
 					$ids = AssignedJobParameter::get()->filter(['Title' => $filter['filter'], 'Value' => $filter['value']])->column('MissionID');
 				}
-				if ($filter['dataType'] == "parameter"){
+				if ($filter['dataType'] == "data"){
 					$ids = Mission::get()->filter($filter['filter'], $filter['value'])->column('ID');
 				}
 				
