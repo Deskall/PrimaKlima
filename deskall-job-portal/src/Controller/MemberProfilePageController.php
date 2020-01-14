325618b1c80c88394f20df294e87df279f67cc65
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-14 14:53:19 +0100

    Update MemberProfilePageController.php

--- deskall-job-portal/src/Controller/MemberProfilePageController.php
+++ deskall-job-portal/src/Controller/MemberProfilePageController.php
@@ -44,7 +44,7 @@ use SilverStripe\Security\DefaultAdminService;
 
 class MemberProfilePageController extends PageController{
 
-	private static $allowed_actions = ['upload', 'UploadForm', 'UpdateExistingDocument','DeleteObject', 'saveFolder', 'EditFile', 'DeleteFile', 'acceptContract','SaveTimeSheet','DeleteTimeSheet','ProfilForm', 'AccountForm','JobOfferForm','EditJobOffer','DeleteJobOffer','PublishJobOffer','RegisterForm'];
+	private static $allowed_actions = ['upload', 'UploadForm', 'UpdateExistingDocument','DeleteObject', 'saveFolder', 'EditFile', 'DeleteFile', 'acceptContract','SaveTimeSheet','DeleteTimeSheet','ProfilForm', 'AccountForm','JobOfferForm','EditJobOffer','DeleteJobOffer','PublishJobOffer'];
 
 	private static $url_handlers = [
 		'delete-object/$ID/$OBJECT' => 'DeleteObject',
@@ -101,7 +101,7 @@ class MemberProfilePageController extends PageController{
 		return [];
 	}
 
-	
+
 
 	
 	public function ProfilForm(){
