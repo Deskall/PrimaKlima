78674224c3cef551b0f03566e473df7f09135dc6
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-14 14:40:26 +0100

    Update DeskallJobPortalPageExtension.php

--- deskall-job-portal/src/Extensions/DeskallJobPortalPageExtension.php
+++ deskall-job-portal/src/Extensions/DeskallJobPortalPageExtension.php
@@ -5,12 +5,8 @@ use SilverStripe\ORM\DataExtension;
 
 class DeskallJobPortalPageExtension extends DataExtension
 {
-    public function RegisterPage($groupcode){
-    	$group = Group::get()->filter('Code',$groupcode)->first();
-    	if ($group){
-    		return RegisterPage::get()->filter('GroupID',$group)->first();
-    	}
-        return null;
+    public function RegisterPage(){
+    	return RegisterPage::get()->first();
     }
 
     public function CurrentCustomer(){
