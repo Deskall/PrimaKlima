3533008ce14bf2e2f21fd6275e19504fd681d8c3
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-16 09:59:41 +0100

    w

--- deskall-job-portal/javascript/lang/fr.js
+++ deskall-job-portal/javascript/lang/fr.js
@@ -1,10 +1,9 @@
+
+
 if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
   console.error('Class ss.i18n not defined');
 } else {
   ss.i18n.addDictionary('fr', {
-    'USERFILE.DELETE' : "Êtes-vous sur de vouloir supprimer ce fichier ?",
-    'USERFILE.DELETEBYTYPE': "Êtes-vous sur de vouloir supprimer ce {type} ?",
-    'USERFILE.FILE': 'Fichier',
-    'USERFILE.FOLDER': 'Dossier'
+    'Admin.DELETECONFIRMMESSAGE' : "Êtes-vous sur de vouloir supprimer ?",
   });
 }
