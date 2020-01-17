dc880098e4fa484a74cc7909a0d0a20fe887941e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-17 14:55:24 +0100

    Update jobportal.js

--- deskall-job-portal/javascript/jobportal.js
+++ deskall-job-portal/javascript/jobportal.js
@@ -14,7 +14,14 @@ $(document).ready(function(){
 	$(document).on("click","[data-delete]",function(){
 		var id = $(this).attr('data-delete');
 		var modal = $("#delete-modal");
-		modal.find('a').attr('href',window.location.pathname+'/bewerbung-loeschen/'+id);
+		modal.find('a').attr('href',cleanUrl(window.location.pathname)+'/bewerbung-loeschen/'+id);
 		UIkit.modal(modal).show();
 	});
 });
+
+function cleanUrl(url){
+    if (url.substring(url.length-1) == "/"){
+        url = url.substring(0, url.length-1);
+    }
+    return url;
+}
