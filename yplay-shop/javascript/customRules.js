71a516ade1114527c8ef945258057b5e0e5ca1de
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-21 16:11:44 +0100

    Update customRules.js

--- yplay-shop/javascript/customRules.js
+++ yplay-shop/javascript/customRules.js
@@ -1,14 +1,14 @@
 $.validator.addMethod("maxDate", function(value, element,attr) {
     var maxDate = new Date(attr);
     var inputDate = new Date(value);
-    if (inputDate > maxDate)
+    if (value && inputDate > maxDate)
         return false;
     return true;
 }, 'Sie müssen mindestens 18 Jahre alt sein, um auf unserer Website bestellen zu können');
 $.validator.addMethod("minDate", function(value, element,attr) {
     var minDate = new Date(attr);
     var inputDate = new Date(value);
-    if (inputDate < minDate)
+    if (value && inputDate < minDate)
         return false;
     return true;
 }, 'Ungültiges Datum');
