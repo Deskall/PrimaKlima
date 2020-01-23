a0a061f0f2b4f4556f758e4a7687fa436883f74c
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-01-23 17:24:44 +0100

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -164,6 +164,7 @@ $(document).ready(function(){
 		});
 		UIkit.util.on(".slider-products",'itemhidden',function(){
 			var active = $(this).parents('.uk-slider-items').find('li.uk-active');
+			console.log(active.attr('data-value'));
 			$(this).parents('.category').find('[data-product-choice]').val(active.attr('data-value')).trigger('change');
 		});
 		UpdateOrder();
