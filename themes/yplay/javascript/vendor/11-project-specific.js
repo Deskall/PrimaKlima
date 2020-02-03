3b20e32833fe0bbc8eb84ee32b9b3ea5a127591e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-03 16:55:15 +0100

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -229,7 +229,7 @@ $(document).ready(function(){
 		InitStep();
 		var validator = $("#Form_OrderForm").validate({
 			errorPlacement: function(error, element) {
-			    error.appendTo( element.parent("div") );
+			    error.appendTo( element.parents(".field") );
 			}
 		});
 
