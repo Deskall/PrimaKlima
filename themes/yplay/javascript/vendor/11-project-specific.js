eaa7908a2f505387bfd2ddda982d197b2cf2ae17
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-04 14:49:18 +0100

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -421,7 +421,7 @@ $(document).ready(function(){
 	}
 
 	function UpdateCart(options){
-		$(".order-preview").addClass('loading').append('<span data-uk-spinner="ratio: 3"></span>');
+		$(".order-preview").addClass('loading').append('<div class="uk-position-center"><span data-uk-spinner="ratio: 3"></span></div>');
 		$.ajax({
 			url: '/shop-functions/updateCartOptions',
 			method: 'POST',
