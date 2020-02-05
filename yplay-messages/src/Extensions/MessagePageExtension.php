33488cf808b24f95f580feed4ef38d70165039d5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-05 18:56:13 +0100

    Update MessagePageExtension.php

--- yplay-messages/src/Extensions/MessagePageExtension.php
+++ yplay-messages/src/Extensions/MessagePageExtension.php
@@ -12,7 +12,7 @@ class MessagePageExtension extends DataExtension {
 		$session = $request->getSession();
 		if ($session->get('active_plz')){
 			// we take only global and related to code
-			$activeNews = $activeNews->filterByCallback(function($item, $list) { return $item->PostalCodes()->count() == 0 || $item->PostalCodes()->filter('Code',$session->get('active_plz'))->count() > 0; });
+			$activeNews = $activeNews->filterByCallback(function($item, $list) use $session { return $item->PostalCodes()->count() == 0 || $item->PostalCodes()->filter('Code',$session->get('active_plz'))->count() > 0; });
 		}
 		return $activeNews;
 	}
