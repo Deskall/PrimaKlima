290bc94dee2b42a9232de85c26f9a6bf5181e2ca
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-05 18:54:54 +0100

    Update MessagePageExtension.php

--- yplay-messages/src/Extensions/MessagePageExtension.php
+++ yplay-messages/src/Extensions/MessagePageExtension.php
@@ -1,10 +1,19 @@
 <?php
 
 use SilverStripe\ORM\DataExtension;
-
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Injector\Injector;
 
 class MessagePageExtension extends DataExtension {
 	public function activeMessages(){
-		return News::get()->filter('isVisible',1);
+		$activeNews = News::get()->filter('isVisible',1);
+		//Postal Code in Session
+		$request = Injector::inst()->get(HTTPRequest::class);
+		$session = $request->getSession();
+		if ($session->get('active_plz')){
+			// we take only global and related to code
+			$activeNews = $activeNews->filterByCallback(function($item, $list) { return $item->PostalCodes() == 0 || $item->PostalCodes()->filter('Code',$session->get('active_plz'))->count() > 0; });
+		}
+		return $activeNews;
 	}
 }
