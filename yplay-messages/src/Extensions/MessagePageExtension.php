97938f7ba2a6f254ffc659988bbd546a281ebc76
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-05 17:44:39 +0100

    Update MessagePageExtension.php

--- yplay-messages/src/Extensions/MessagePageExtension.php
+++ yplay-messages/src/Extensions/MessagePageExtension.php
@@ -5,6 +5,6 @@ use SilverStripe\ORM\DataExtension;
 
 class MessagePageExtension extends DataExtension {
 	public function activeMessages(){
-		return News::get()->filter('isActive',1);
+		return News::get()->filter('isVisible',1);
 	}
 }
