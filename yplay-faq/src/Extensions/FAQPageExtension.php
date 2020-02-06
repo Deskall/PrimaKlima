b031a16eb849b8f0f4a2f546e26f1cd5d9ad7c66
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-06 09:39:49 +0100

    Update FAQPageExtension.php

--- yplay-faq/src/Extensions/FAQPageExtension.php
+++ yplay-faq/src/Extensions/FAQPageExtension.php
@@ -1,10 +1,9 @@
 <?php
 
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\Control\HTTPRequest;
-use SilverStripe\Core\Injector\Injector;
 
-class MessagePageExtension extends DataExtension {
+
+class FAQPageExtension extends DataExtension {
 	public function activeFAQS(){
 		$items = FAQItem::get()->filter('DisplayInSearchModal',1);
 		
