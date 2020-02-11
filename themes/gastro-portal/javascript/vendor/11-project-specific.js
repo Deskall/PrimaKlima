9670b378bf5a48170f8b49a234ec128490791c5c
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-11 13:43:31 +0100

    Update 11-project-specific.js

--- themes/gastro-portal/javascript/vendor/11-project-specific.js
+++ themes/gastro-portal/javascript/vendor/11-project-specific.js
@@ -201,7 +201,7 @@
 	});
 
 		$(document).on("click","[data-toggle-login-switcher]",function(){
-			var index = parseINT($(this).attr('data-toggle-login-switcher')) + 1;
+			var index = parseInt($(this).attr('data-toggle-login-switcher')) + 1;
 			$("#login-tab-switcher li:nth-child("+index+")").find('a').trigger('click');
 			// UIkit.switcher('#login-tab-switcher').show($(this).attr('data-toggle-login-switcher'));
 		});
