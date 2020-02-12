526d0ba3229fc3730e85efe943ce3c050ffe1b42
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-12 10:03:50 +0100

    Create PayPalClient.php

---
+++ deskall-shop/src/Models/PayPalClient.php
@@ -0,0 +1,46 @@
+<?php
+
+use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Core\ProductionEnvironment;
+use SilverStripe\SiteConfig\SiteConfig;
+use SilverStripe\Control\Director;
+
+ini_set('error_reporting', E_ALL); // or error_reporting(E_ALL);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+
+class PayPalClient
+{
+    /**
+     * Returns PayPal HTTP client instance with environment that has access
+     * credentials context. Use this instance to invoke PayPal APIs, provided the
+     * credentials have access.
+     */
+    public static function client()
+    {
+        return new PayPalHttpClient(self::environment());
+    }
+
+    /**
+     * Set up and return PayPal PHP SDK environment with PayPal access credentials.
+     * This sample uses SandboxEnvironment. In production, use ProductionEnvironment.
+     */
+    public static function environment()
+    {
+        $clientId = SiteConfig::current_site_config()->PayPalClientID;
+        $clientSecret = SiteConfig::current_site_config()->PayPalSecret;
+        if (Director::AbsoluteBaseUrl() == "https://www.schneider-hotel-gastrojobs.com/" ){
+            ob_start();
+            print_r('live');
+            $result = ob_get_clean();
+            file_put_contents($_SERVER['DOCUMENT_ROOT']."/logi.txt", $result);
+            return new ProductionEnvironment($clientId, $clientSecret);
+        }
+            ob_start();
+            print_r('sandbox');
+            $result = ob_get_clean();
+            file_put_contents($_SERVER['DOCUMENT_ROOT']."/logi.txt", $result);
+        return new SandboxEnvironment($clientId, $clientSecret);
+    }
+}
