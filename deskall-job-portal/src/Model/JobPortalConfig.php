2d647dc3bbcab0e05cf1588a508e4bc6ec1303c3
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-02-19 08:25:20 +0100

    Update JobPortalConfig.php

--- deskall-job-portal/src/Model/JobPortalConfig.php
+++ deskall-job-portal/src/Model/JobPortalConfig.php
@@ -27,34 +27,10 @@ class JobPortalConfig extends DataObject
 {
     private static $db = array(
         'Title' => 'Varchar',
-        'AutoOffer' => 'Boolean(0)',
-        'Usage' => 'Varchar',
-        'TransportCost' => 'Varchar',
-        'CostAndHousing' => 'Varchar',
-        'AgentCost' => 'Varchar',
-        'OfferValidityText' => 'Varchar',
-        'OfferValidity' => 'Decimal',
-        'ContractValidityText' => 'Varchar',
-        'ContractValidity' => 'Decimal', 
-        'Intro' => 'HTMLText',
-        'Diverse' => 'HTMLText',
-        'Conditions' => 'HTMLText',
-        'IntroContract' => 'HTMLText',
-        'DiverseContract' => 'HTMLText',
-        'ConditionsContract' => 'HTMLText',
-        'CustomerOfferEmailBody' => 'HTMLText',
-        'OfferConfirmedText' => 'HTMLText',
-        'OfferAlreadyConfirmedText' => 'HTMLText',
-        'CookOffersEmailBody' =>  'HTMLText',
         'CandidatureHelpTitle' => 'Varchar',
         'CandidatureHelpText' => 'HTMLText',
         'CandidatureSentText' =>  'HTMLText',
         'CandidatureAlreadySentText' =>  'HTMLText',
-        'ReminderOfferEmailSubject' => 'Varchar',
-        'ReminderOfferEmailBody' => 'HTMLText',
-        'ChosenCookEmailBody' => 'HTMLText',
-        'CookContractSignedText' => 'HTMLText',
-        'CustomerContractSignedEmailBody' =>  'HTMLText',
         'EmailSignature' => 'HTMLText',
 
         //Registration
@@ -96,10 +72,7 @@ class JobPortalConfig extends DataObject
     private static $plural_name = "Einstellungen";
 
     private static $has_one = [
-        'OfferFile' => File::class ,
-        'ContractFile' => File::class,
-        'AGBCustomerFile' => File::class,
-        'AGBCookFile' => File::class,
+        'OfferFile' => File::class 
     ];
 
     private static $has_many = [
@@ -107,10 +80,7 @@ class JobPortalConfig extends DataObject
     ];
 
     private static $owns = [
-        'OfferFile',
-        'ContractFile',
-        'AGBCookFile',
-        'AGBCustomerFile'
+        'OfferFile'
     ];
 
     private static $summary_fields = [
@@ -121,39 +91,13 @@ class JobPortalConfig extends DataObject
     public function fieldLabels($includerelation = true){
     $labels = parent::fieldLabels($includerelation);
     $labels['Title'] = _t(__CLASS__.'.Title','Titel');
-    $labels['Usage'] = _t(__CLASS__.'.Usage','Einsatz');
-    $labels['OfferValidityText'] = _t(__CLASS__.'.Title','Gültigkeit des Angebots (Text)');
-    $labels['OfferValidity'] = _t(__CLASS__.'.Usage','Gültigkeit des Angebots (Tag)');
-    $labels['ContractValidityText'] = _t(__CLASS__.'.Title','Gültigkeit des Vertrages (Text)');
-    $labels['ContractValidity'] = _t(__CLASS__.'.Usage','Gültigkeit des Vertrages (Tag)');
-    $labels['TransportCost'] = _t(__CLASS__.'.TransportCost','Fahrtkosten');
-    $labels['CostAndHousing'] = _t(__CLASS__.'.CostAndHousing','Kost&Logis');
-    $labels['AgentCost'] = _t(__CLASS__.'.AgentCost','Agentur-Gebühr');
+   
     $labels['OfferFile'] = _t(__CLASS__.'.OfferFile','Angebot Vorlage');
-    $labels['Intro'] = _t(__CLASS__.'.Intro','Angebot Einstiegtext');
-    $labels['Diverse'] = _t(__CLASS__.'.Diverse','Angebot Details');
-    $labels['Conditions'] = _t(__CLASS__.'.Conditions','Angebot Konditionen');
-    $labels['IntroContract'] = _t(__CLASS__.'.IntroContract','Vertrag Einstiegtext');
-    $labels['DiverseContract'] = _t(__CLASS__.'.DiverseContract','Vertrag Details');
-    $labels['ConditionsContract'] = _t(__CLASS__.'.ConditionsContract','Vertrag Konditionen');
-    $labels['CustomerOfferEmailBody'] = _t(__CLASS__.'.CustomerOfferEmailBody','Kunde Angebot : Inhalt der E-Mail');
-    $labels['OfferConfirmedText'] = _t(__CLASS__.'.OfferConfirmedText','Text für den Angebot-bestätigung Text');
-    $labels['OfferAlreadyConfirmedText'] = _t(__CLASS__.'.OfferAlreadyConfirmedText','Angebot bereits bestätigt');
-    $labels['CookOffersEmailBody'] = _t(__CLASS__.'.CookOffersEmailBody','Neue Angebot: Inhalt der Email für alle Köche');
     $labels['CandidatureSentText'] = _t(__CLASS__.'.CandidatureSentText','Bewerbung gesendet');
     $labels['CandidatureAlreadySentText'] = _t(__CLASS__.'.CandidatureAlreadySentText','Bewerbung bereits gesendet');
-    $labels['CustomerContractSignedEmailBody'] = _t(__CLASS__.'.CustomerContractSignedEmailBody','Kunde Vertrag Bestätigung : Inhalt der Email');
-    $labels['ReminderOfferEmailSubject'] = _t(__CLASS__.'.ReminderOfferEmailSubject','Betreff der Angebot-Erinnerungsemail');
-    $labels['ReminderOfferEmailBody'] = _t(__CLASS__.'.ReminderOfferEmailBody','Inhalt der Angebot-Erinnerungsemail');
-    $labels['ContractFile'] = _t(__CLASS__.'.ContractFile','Auftrag Vorlage');
-    $labels['ChosenCookEmailBody'] = _t(__CLASS__.'.ChosenCookEmailBody','Inhalt der Email für den gewählt Koch (mit Auftrag)');
     $labels['CandidatureHelpTitle'] = _t(__CLASS__.'.CandidatureHelpTitle','Titel der Bewerbung Pop-up Fenster');
     $labels['CandidatureHelpText'] = _t(__CLASS__.'.CandidatureHelpText','Inhalt der Bewerbung Pop-up Fenster');
-    $labels['CookContractSignedText'] = _t(__CLASS__.'.CookContractSignedText','Text nach Auftrag Genehmigung durch Koch');
-    $labels['AGBCustomerFile'] = _t(__CLASS__.'.AGBCustomerFile','AGB Datei (Kunde)');
-    $labels['AGBCookFile'] = _t(__CLASS__.'.AGBCookFile','AGB Datei (Koch)');
     $labels['EmailSignature'] = _t(__CLASS__.'.EmailSignature','E-Mail Signatur');
-    $labels['AutoOffer'] = _t(__CLASS__.'.AutoOffer','E-Mail mit Angebot an Kunde nach Anfrage senden?');
     $labels['AfterRefusalCandidatureEmailBody'] = _t(__CLASS__.'.AfterRefusalCandidatureEmailBody','Inhalt der Email - Bewerbung abgelehnt');
     $labels['AfterRefusalCandidatureEmailSubject'] = _t(__CLASS__.'.AfterRefusalCandidatureEmailSubject','Titel der Email - Bewerbung abgelehnt');
     
@@ -238,10 +182,7 @@ class JobPortalConfig extends DataObject
 
     
        $fields->addFieldToTab('Root.Main',UploadField::create('OfferFile',$this->fieldLabels()['OfferFile'])->setFolderName('Uploads/Vorlagen'));
-       $fields->addFieldToTab('Root.Main',UploadField::create('ContractFile',$this->fieldLabels()['ContractFile'])->setIsMultiUpload(false)->setFolderName('Uploads/Vorlagen'));
-       $fields->addFieldToTab('Root.Main',UploadField::create('AGBCustomerFile',$this->fieldLabels()['AGBCustomerFile'])->setIsMultiUpload(false)->setFolderName('Uploads/Vorlagen'));
-       $fields->addFieldToTab('Root.Main',UploadField::create('AGBCookFile',$this->fieldLabels()['AGBCookFile'])->setIsMultiUpload(false)->setFolderName('Uploads/Vorlagen'));
-
+      
        $fields->dataFieldByName('Parameters')->getConfig()->addComponent(new GridFieldOrderableRows('Sort'));
 
         
