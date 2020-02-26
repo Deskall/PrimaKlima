8749518052e551aa04520045c4e1219c0440e18d
Guillaume <31889656+Deskall@users.noreply.github.com>
2020-02-26 15:56:22 +0100

    Update customRules.js

--- yplay-shop/javascript/customRules.js
+++ yplay-shop/javascript/customRules.js
@@ -26,5 +26,6 @@ $.validator.addMethod("intlTelNumber", function(value, element) {
 }, "Bitte geben Sie eine gültige schweizer oder deutsche Telefonnummer ein");
 
 $.validator.addMethod("validateEmail", function(value, element) {
+    console.log( $("input[name='email']").val());
     return value == $("input[name='email']").val();
 }, "E-Mail-Adressen stimmen nicht überein");
