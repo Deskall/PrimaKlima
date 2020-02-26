dd6c759a2a9475ef7a938e6b574761c70c59a619
Guillaume <31889656+Deskall@users.noreply.github.com>
2020-02-26 14:58:44 +0100

    Update ProductOption.php

--- yplay-shop/src/Models/ProductOption.php
+++ yplay-shop/src/Models/ProductOption.php
@@ -46,6 +46,7 @@ class ProductOption extends Product {
 		$fields->dataFieldByName('ActivationPrice')->hideIf('hasOptions')->isChecked();
 		$fields->dataFieldByName('UniquePriceLabel')->hideIf('hasOptions')->isChecked();
 		$fields->dataFieldByName('ActivationPriceLabel')->hideIf('hasOptions')->isChecked();
+		$fields->dataFieldByName('Multiple')->hideIf('hasOptions')->isChecked();
 		$fields->dataFieldByName('Single')->displayIf('hasOptions')->isChecked();
 		$fields->dataFieldByName('Unit')->hideIf('hasOptions')->isChecked();
 		
