28a7a76bb622d8b86a6ee18769284de8af1d8d35
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-03-23 13:27:35 +0100

    Update ProductCategory.php

--- deskall-shop/src/Models/ProductCategory.php
+++ deskall-shop/src/Models/ProductCategory.php
@@ -16,6 +16,8 @@ class ProductCategory extends DataObject {
 
     private static $cascade_duplicates = ['Products'];
 
+    private static $cascade_delete = ['Products'];
+
     private static $summary_fields = array(
         'Title' => 'Titel',
     );
@@ -37,17 +39,40 @@ class ProductCategory extends DataObject {
         'Sortable'
     ];
 
+
     public function onBeforeWrite(){
         parent::onBeforeWrite();
         $this->URLSegment = URLSegmentFilter::create()->filter($this->Title);
-        if ($this->isChanged('URLSegment') && ($changedFields['URLSegment']['before'] != $changedFields['URLSegment']['after']) ){
-                $oldFolderPath = 'Uploads/Webshop/'.$changedFields['URLSegment']['before'];
+        if ($this->isChanged('Title') && ($changedFields['Title']['before'] != $changedFields['Title']['after']) ){
+                $oldFolderPath = 'Uploads/Webshop/'.URLSegmentFilter::create()->filter($changedFields['Title']['before']);
                 $newFolder = Folder::find_or_make($oldFolderPath);
-                $newFolder->Name = $changedFields['URLSegment']['after'];
-                $newFolder->Title = $changedFields['URLSegment']['after'];
+                $newFolder->Name = $changedFields['Title']['after'];
+                $newFolder->Title = $changedFields['Title']['after'];
                 $newFolder->write();
             
         }
+        if ($this->Image()->exists()){
+            $folder = Folder::find_or_make($this->getFolderName());
+            $this->Image()->ParentID = $folder->ID;
+            $this->Image()->write();
+            $this->Image()->publishSingle();
+        }
+    }
+
+    public function onAfterWrite()
+    {
+        if ($this->isChanged('ImageID')){
+            $changedFields = $this->getChangedFields();
+            $oldPicture = Image::get()->byId($changedFields['ImageID']['before']);
+            if ($oldPicture){
+                $oldPicture->File->deleteFile();
+                DB::prepared_query('DELETE FROM "File" WHERE "File"."ID" = ?', array($oldPicture->ID));
+                $oldPicture->delete();
+            }
+        }
+        
+        parent::onAfterWrite();
+       
     }
 
     public function getCMSFields() {
@@ -59,7 +84,13 @@ class ProductCategory extends DataObject {
     }
 
     public function getFolderName(){
-        return 'Uploads/Webshop/'.$this->URLSegment;
+        if($this->URLSegment){
+            return 'Uploads/Webshop/'.$this->URLSegment;
+        }
+        else{
+            return 'Uploads/Webshop/tmp';
+        }
+        
     }
 
 }
