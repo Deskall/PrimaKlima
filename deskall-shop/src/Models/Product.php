7f57ea6f3397ad958d518a9c58af1bf912a7127c
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-03-23 15:45:33 +0100

    Update Product.php

--- deskall-shop/src/Models/Product.php
+++ deskall-shop/src/Models/Product.php
@@ -31,10 +31,6 @@ class Product extends DataObject {
         'TransportPrice' => 'Currency'
     );
 
-    
-
-    private static $cascade_duplicates = ['Features'];
-
 
     private static $summary_fields = array(
         'Title' => 'Titel',
@@ -52,102 +48,24 @@ class Product extends DataObject {
         'Sortable'
     ];
 
+    public function fieldLabels($includerelation = true){
+        $labels = parent::fieldLabels($includerelation);
+        $labels['Title'] = _t('Product.Title', 'Titel');
+       
+
+        return $labels;
+    }
+
 
 
     public function getCMSFields() {
-        $fields = new FieldList();
-        $fields->push(new TabSet('Root'));
+        $fields = parent::getCMSFields();
 
-        $fields->addFieldToTab('Root.Global', TextField::create('Title', _t('Package.Title', 'Titel')) );
-        $fields->addFieldToTab('Root.Global', NumericField::create('Price', _t('Package.Price', 'Preis')) );
-        $fields->addFieldToTab('Root.Global', NumericField::create('SalePrice', _t('Package.SalePrice', 'Aktionspreis')) );
-
-        $fields->addFieldToTab('Root.Global', CheckboxField::create('isFlatrate', _t('Package.isFlatrate', 'Flatrate')) );
-
-
-        $fields->addFieldToTab('Root.Global', NumericField::create('NumOfAds', _t('Package.NumOfAds', 'Anzahl Stellenanzeigen')) );
-
-        $fields->addFieldToTab('Root.Global', NumericField::create('RunTime', _t('Package.RunTime', 'Laufzeit')) );
-        $fields->addFieldToTab('Root.Global', DropdownField::create('RunTimeCurrency',_t('PACKAGE.RunTimeCurrency', 'Laufzeit Einheit'), array(
-            'week' => 'Wochen',
-            'month' => 'Monate',
-        )));
-
-
-        $fields->addFieldToTab('Root.Global', ListboxField::create('Features', _t('Package.Features', 'Features'), PackageConfigItem::get()->map('ID','Title')->toArray())->setAttribute('data-placeholder', _t('Package.Choose', 'Bitte Wählen')) );
-
-        $OptionField = new GridField(
-            'PackegeOptions',
-            _t('PACKAGE.PackegeOptions', 'Optionen'),
-            $this->PackegeOptions(),
-            GridFieldConfig::create()
-                ->addComponent(new GridFieldButtonRow('before'))
-                ->addComponent(new GridFieldTitleHeader())
-                ->addComponent(new GridFieldEditableColumns())
-                ->addComponent(new GridFieldDeleteAction())
-                ->addComponent(new GridFieldAddNewInlineButton())
-                ->addComponent(new GridFieldOrderableRows('SortOrder'))
-        );
-
-        $OptionField->getConfig()->getComponentByType(GridFieldEditableColumns::class)->setDisplayFields(array(
-            'Title__de_DE' => array(
-                'title' => _t('PACKAGE.Title', 'Titel'),
-                'callback' => function ($record, $column, $holiDayGridfield){
-                    return TextField::create('Title__de_DE', _t('PACKAGE.Title', 'Titel'));
-                }
-            ),
-            'RunTime' => array(
-                'title' => _t('PACKAGE.RunTime', 'Laufzeit') ,
-                'callback' => function ($record, $column, $holiDayGridfield){
-                    return NumericField::create('RunTime',_t('PACKAGE.RunTime', 'Laufzeit') );
-                }
-            ),
-            'RunTimeCurrency' => array(
-                'title' => _t('PACKAGE.RunTimeCurrency', 'Laufzeit Einheit') ,
-                'callback' => function ($record, $column, $holiDayGridfield){
-                    return DropdownField::create('RunTimeCurrency',_t('PACKAGE.RunTimeCurrency', 'Laufzeit Einheit'), array(
-                        'week' => 'Wochen',
-                        'month' => 'Monate',
-                    ));
-                }
-            ),
-            'Price' => array(
-                'title' => _t('PACKAGE.Price', 'Preis') ,
-                'callback' => function ($record, $column, $holiDayGridfield){
-                    return NumericField::create('Price',_t('PACKAGE.Price', 'Preis') );
-                }
-            ),
-
-        ));
-
-        $fields->addFieldToTab('Root.Global', $OptionField );
-
-
-        $fields->addFieldToTab('Root.Global', HTMLEditorField::create('Description', _t('Package.Beschreibung', 'Beschreibung'))->setRows(5) );
-        $fields->addFieldToTab('Root.Global', TextField::create('NumOfAdsTitle', _t('Package.NumOfAds', 'Anzahl Stellenanzeigen')) );
-        $fields->addFieldToTab('Root.Global', TextField::create('RunTimeTitle', _t('Package.RunTime', 'Laufzeit Anzeige')) );
-
-
+      
 
         return $fields;
     }
 
-    public function currentPrice(){
-        if( $this->SalePrice > 0 ){
-            return $this->SalePrice;
-        }else{
-            return $this->Price;
-        }
-    }
-
-    public function getParameters(){
-        $parameters = [];
-        $features = PackageConfigItem::get()->filter('isVisible',1);
-        foreach ($features as $f) {
-            $parameters[$f->ID] = ['title' => $f->Title, 'included' => $this->Features()->byId($f->ID)];
-        }
-        return new ArrayList($parameters);
-    }
 
     public function OrderLink(){
         return ShopPage::get()->first()->Link();
