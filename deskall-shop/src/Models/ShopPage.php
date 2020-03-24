6260f7a859959d181214c2dbc30808f5d90443b8
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-03-24 15:53:11 +0100

    Update ShopPage.php

--- deskall-shop/src/Models/ShopPage.php
+++ deskall-shop/src/Models/ShopPage.php
@@ -6,16 +6,11 @@ use SilverStripe\AssetAdmin\Forms\UploadField;
 
 class ShopPage extends Page {
 
-	public function getProductConfig(){
-		return ProductConfig::get()->first();
+
+	public function activeProducts(){
+		return Product::get()->filter('isVisible',1);
 	}
 
-	public function activePackages(){
-		return Package::get()->filter('isVisible',1);
-	}
-
-	public function activeParameters(){
-	    return PackageConfigItem::get()->filter('isVisible',1);
-	}
+	
 
 }
