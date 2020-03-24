e8b7eedb481730eb148b07c71e1522a62117d6b0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-03-24 13:19:36 +0100

    Update _config.php

--- deskall-shop/_config.php
+++ deskall-shop/_config.php
@@ -1,6 +1,9 @@
 <?php
-
 /* Deskall Products Module
 * @author: Desall kommunikation  AG, G.Pacilly
 * 2019
 */
+
+use SilverStripe\ORM\FieldType\DBCurrency;
+
+DBCurrency::setCurrencySymbol('CHF');
