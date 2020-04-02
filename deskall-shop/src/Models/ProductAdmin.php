ffe4b2c09387400ab8193dc076f3cb5e88064dc7
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-02 15:36:42 +0200

    Update ProductAdmin.php

--- deskall-shop/src/Models/ProductAdmin.php
+++ deskall-shop/src/Models/ProductAdmin.php
@@ -44,7 +44,7 @@ class ProductAdmin extends ModelAdmin{
 	       $gridField->getConfig()->addComponent(new GridFieldOrderableRows('Sort'))->addComponent(new GridFieldShowHideAction())->addComponent(new GridFieldDuplicateAction());
 	    }
 	     if($this->modelClass == 'ShopCart' && $gridField=$form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass))) {
-	       $gridField->getConfig()->removeComponentsByType([GridFieldAddExistingAutocompleter::class,GridFieldDeleteAction::class])->addComponent(new GridFieldDeleteAction())->addComponent(new GridFieldDeleteAllAction('before'));
+	       $gridField->getConfig()->removeComponentsByType([GridFieldAddExistingAutocompleter::class,GridFieldDeleteAction::class,GridFieldAddNewButton::class ])->addComponent(new GridFieldDeleteAction())->addComponent(new GridFieldDeleteAllAction('before'));
 	    }
 
 	    return $form;
