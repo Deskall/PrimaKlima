2d05a96144ef2d558295d29e8b4f6998778384f8
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-02 17:48:40 +0200

    Update DeskallShopPageExtension.php

--- deskall-shop/src/Extensions/DeskallShopPageExtension.php
+++ deskall-shop/src/Extensions/DeskallShopPageExtension.php
@@ -1,11 +1,29 @@
 <?php
 
 use SilverStripe\ORM\DataExtension;
-
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Injector\Injector;
 
 class DeskallShopPageExtension extends DataExtension
 {
     public function ShopPage(){
         return ShopPage::get()->first();
     }
+
+    public function activeCart(){
+    	$request = Injector::inst()->get(HTTPRequest::class);
+    	$session = $request->getSession();
+    	if ($session->get('shopcart_id') && ShopCart::get()->byId($session->get('shopcart_id'))){
+    		$cart = ShopCart::get()->byId($session->get('shopcart_id'));
+    	}
+    	else{
+    		$cart = new ShopCart();
+    		$cart->IP = $request->getIp();
+    		$cart->write();
+    		$session->set('shopcart_id',$cart->ID);
+    	}
+    	
+    	return $cart;
+    }
+
 }
