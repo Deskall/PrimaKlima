0b8f802c29493986ffa53106cd62d2a07232ac9e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-02 13:57:31 +0200

    q

--- deskall-shop/src/Extensions/ShopMenuBlockExtension.php
+++ deskall-shop/src/Extensions/ShopMenuBlockExtension.php
@@ -2,7 +2,8 @@
 
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\SiteConfig\SiteConfig;
-
+use SilverStripe\Control\HTTPRequest;
+use SilverStripe\Core\Injector\Injector;
 
 class ShopMenuBlockExtension extends DataExtension{
 	private static $block_types = [
@@ -11,6 +12,17 @@ class ShopMenuBlockExtension extends DataExtension{
 
 	public function CheckoutPage(){
 	    return SiteConfig::current_site_config()->ShopPage();
+	}
+
+	public function activeCart(){
+		$request = Injector::inst()->get(HTTPRequest::class);
+		$session = $request->getSession();
+		if ($id = $session->get('shopcart_id')){
+		
+		   $cart = ShopCart::get()->byId($id);
+		   return $cart;
+		}
+		return null;
 	}
 
 }
