6d210c7c5c26604c3130e4c0d2cce90e59f9eced
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-03 15:08:16 +0200

    Update ShopMenuBlockExtension.php

--- deskall-shop/src/Extensions/ShopMenuBlockExtension.php
+++ deskall-shop/src/Extensions/ShopMenuBlockExtension.php
@@ -18,20 +18,20 @@ class ShopMenuBlockExtension extends DataExtension{
 	    return SiteConfig::current_site_config()->ShopPage();
 	}
 
-	// public function activeCart(){
-	// 	$request = Injector::inst()->get(HTTPRequest::class);
-	// 	$session = $request->getSession();
-	// 	if ($session->get('shopcart_id') && ShopCart::get()->byId($session->get('shopcart_id'))){
-	// 		$cart = ShopCart::get()->byId($session->get('shopcart_id'));
-	// 	}
-	// 	else{
-	// 		$cart = new ShopCart();
-	// 		$cart->IP = $request->getIp();
-	// 		$cart->write();
-	// 		$session->set('shopcart_id',$cart->ID);
-	// 	}
+	public function activeCart(){
+		$request = Injector::inst()->get(HTTPRequest::class);
+		$session = $request->getSession();
+		if ($session->get('shopcart_id') && ShopCart::get()->byId($session->get('shopcart_id'))){
+			$cart = ShopCart::get()->byId($session->get('shopcart_id'));
+		}
+		else{
+			$cart = new ShopCart();
+			$cart->IP = $request->getIp();
+			$cart->write();
+			$session->set('shopcart_id',$cart->ID);
+		}
 		
-	// 	return $cart;
-	// }
+		return $cart;
+	}
 
 }
