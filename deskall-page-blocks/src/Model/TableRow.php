54d953feb3f8aa3dcdd7040ea3011585090fd8da
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-06 19:38:53 +0200

    Update TableRow.php

--- deskall-page-blocks/src/Model/TableRow.php
+++ deskall-page-blocks/src/Model/TableRow.php
@@ -21,17 +21,43 @@ class TableRow extends DataObject
         'Sortable'
     ];
 
-    public function onBeforeWrite(){
-        parent::onBeforeWrite();
+    public function onAfterWrite(){
+        parent::onAfterWrite();
         $this->saveCells();
     }
 
     public function saveCells(){
+        $exclude = ['SecurityID','ParentID'];
         $changed = $this->getChangedFields();
         ob_start();
             print_r($changed);
             $result = ob_get_clean();
             file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
+        foreach ($changed as $field => $value) {
+            //save only header
+            if (!in_array($field,$exclude)){
+                $header = $this->Parent()->Headers()->byId($key);
+                if ($header){
+                    //If new create
+                    if (empty($value['before'])){
+                        $cell = new TableCell();
+                        $cell->RowID = $this->ID;
+                        $cell->HeaderID = $key;
+                        $cell->Sort = $header->Sort;
+                        $cell->Value = $value['after'];
+                    }
+                    //else update
+                    else{
+                        $cell = $this->Cells()->filter(['HeaderID' => $key,'RowID' => $this->ID])->first();
+                        if ($cell){
+                            $cell->Value = $value['after'];
+                        }
+                    }
+                    $cell->write();
+                }
+                
+            }
+        }
     }
 
 
