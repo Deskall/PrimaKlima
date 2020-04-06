7f4ff6268d3af46b13ffdcb93ab9a37940aa2c21
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-06 16:25:15 +0200

    Update TableBlock.php

--- deskall-page-blocks/src/Model/TableBlock.php
+++ deskall-page-blocks/src/Model/TableBlock.php
@@ -109,12 +109,18 @@ class TableBlock extends BaseElement implements Searchable
                     'title' => 'Titel',
                     'field' => TextField::class
                 ),
-                'Format' => function ($record, $column, $grid) {
-                    return DropdownField::create($column,$record->fieldLabels()['Format'],$record->stat('formats'));
-                },
-                'TextAlign' => function ($record, $column, $grid) {
-                    return HTMLOptionsetField::create($column,$this->fieldLabels()[$column],$record->stat('text_alignments'));
-                },
+                'Format' => array(
+                    'title' => 'Breite',
+                    'callback' => function ($record, $column, $grid) {
+                        return DropdownField::create($column,$record->fieldLabels()['Format'],$record->stat('formats'));
+                    }
+                ),
+                'TextAlign' => array(
+                    'title' => 'Ausricthung',
+                    'callback' => function ($record, $column, $grid) {
+                        return HTMLOptionsetField::create($column,$this->fieldLabels()[$column],$record->stat('text_alignments'));
+                    }
+                )
             ));
 
             $headersField = new GridField('Headers',$this->fieldLabels()['Headers'],$this->Headers(),$config);
