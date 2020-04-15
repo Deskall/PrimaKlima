addf842dbf554043f2b94aab5a7c1cceb9a5099e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-15 14:40:49 +0200

    Update ProductOverviewPageController.php

--- bak-products/src/Controllers/ProductOverviewPageController.php
+++ bak-products/src/Controllers/ProductOverviewPageController.php
@@ -11,6 +11,7 @@ use SilverStripe\CMS\Model\SiteTree;
 use Bak\Products\Models\Product;
 use Bak\Products\Models\ProductCategory;
 use Bak\Products\Models\ProductUseArea;
+use Bak\Products\Models\ProductUsage;
 use SilverStripe\SiteConfig\SiteConfig;
 use SilverStripe\View\Requirements;
 use SilverStripe\Control\HTTPRequest;
