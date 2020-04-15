8c3a0b01009de55bf6a2b375054279f62915ce76
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-15 08:41:27 +0200

    Update bak-products.js

--- bak-products/javascript/bak-products.js
+++ bak-products/javascript/bak-products.js
@@ -3,7 +3,12 @@ var products,
 
 $(document).ready(function(){  
     UIkit.util.on('#products-switcher', 'beforeshow', function () {
-       console.log('ca marche');
+        $('[data-search-products]').val("");
+        history.pushState({'filterList': $(this).attr('data-show-filter') }, $(this).attr('data-filter-name'), $(this).attr('href'));
+        ga('set', 'page', $(this).attr('href'));
+        ga('send', 'pageview');
+        $('h1').text($('h1').attr('data-title-orig'));
+        $('title').text($('h1').attr('data-title-orig'));
     });
     if( window.location.pathname.split("/")[1] == 'produkte' || window.location.pathname.split("/")[1] == 'products'){
         jQuery.ajax({
