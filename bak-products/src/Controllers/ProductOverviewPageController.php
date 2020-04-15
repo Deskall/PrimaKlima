715d19b1e5e9bfc5463d77c63e124bf51b2ee3d5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-15 07:12:56 +0200

    Update ProductOverviewPageController.php

--- bak-products/src/Controllers/ProductOverviewPageController.php
+++ bak-products/src/Controllers/ProductOverviewPageController.php
@@ -19,7 +19,7 @@ class ProductOverviewPageController extends PageController
 
     public function init(){
         parent::init();
-        Requirements::javascript('bak-products/javascript/handlebar.min.js');
+        Requirements::javascript('bak-products/javascript/handlebars.min.js');
         Requirements::javascript('bak-products/javascript/bak-products.js');
     }
 
