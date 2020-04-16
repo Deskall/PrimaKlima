ccedb42b98af95f89851c8dafe488d322421783e
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-04-16 13:30:34 +0200

    Update bak-products.js

--- bak-products/javascript/bak-products.js
+++ bak-products/javascript/bak-products.js
@@ -11,6 +11,33 @@ $(document).ready(function(){
         $(this).addClass("uk-table uk-table-small uk-table-striped");
     });
 
+    $("[data-open-dropdown]").click( function() {
+        if( $(this).hasClass("active") ){
+            $(this).parents("[data-dropdown]").find(".dropdown-content").addClass("hidden");
+            if( $(this).parents("[data-dropdown]").find("input[type='checkbox']:checked").length > 1 ){
+                $(this).parents("[data-dropdown]").find('[data-dropdown-checked-options]').removeClass("hidden");
+                $(this).parents("[data-dropdown]").find('.product-name-holder').addClass("hidden");
+            }else{
+                $(this).parents("[data-dropdown]").find('[data-dropdown-checked-options]').addClass("hidden");
+                $(this).parents("[data-dropdown]").find('.product-name-holder').removeClass("hidden");
+                $(this).parents("[data-dropdown]").find('.product-name-holder').text( $(this).parents("[data-dropdown]").find("input[type='checkbox']:checked").val() );
+            }
+            $(this).removeClass("active");
+        }else{
+            $(this).parents("[data-dropdown]").find(".dropdown-content").removeClass("hidden");
+            $(this).parents("[data-dropdown]").find('[data-dropdown-checked-options]').addClass("hidden");
+            $(this).addClass("active");
+        }
+    });
+
+    $("[data-dropdown] input[type='checkbox']").on("change", function(){
+        var listHTML = '';
+        $(this).parents("[data-dropdown]").find("input[type='checkbox']:checked").each(function(){
+            listHTML += '<li>' + $(this).val() + '</li>';
+        });
+        $(this).parents("[data-dropdown]").find(".checked-options").html(listHTML);
+    });
+
     if( window.location.pathname.split("/")[1] == 'produkte' || window.location.pathname.split("/")[1] == 'products'){
         jQuery.ajax({
             type: "GET",
