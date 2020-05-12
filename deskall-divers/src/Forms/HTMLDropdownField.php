53a1d1ed08d5aab75ec09629c4e2177489f291c2
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-12 17:18:16 +0200

    Update HTMLDropdownField.php

--- deskall-divers/src/Forms/HTMLDropdownField.php
+++ deskall-divers/src/Forms/HTMLDropdownField.php
@@ -14,6 +14,7 @@ class HTMLDropdownField extends DropdownField{
 	protected $extraClasses = [];
 
 	private static $iconsList = [
+		'none' => 'keine',
 		'500px' => '500px',
 		'album' => 'album',
 		'arrow-down' => 'arrow-down',
