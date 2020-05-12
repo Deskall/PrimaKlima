69bdc437293de085b485bf6a5901fae311d8bf25
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-12 11:00:33 +0200

    Update BAKSiteConfigExtension.php

--- bak-products/src/Extensions/BAKSiteConfigExtension.php
+++ bak-products/src/Extensions/BAKSiteConfigExtension.php
@@ -4,6 +4,9 @@ namespace BAK\Products;
 
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\TreeDropdownField;
+use SilverStripe\Forms\TextareaField;
+use SilverStripe\Forms\TextField;
+use SilverStripe\Forms\EmailField;
 use SilverStripe\ORM\DataExtension;
 use SilverStripe\CMS\Model\SiteTree;
 
@@ -12,6 +15,15 @@ class BAKSiteConfigExtension extends DataExtension
 
   private static $db = [
     
+    'SubjectEmail' => 'Varchar(255)',
+    'ContentEmail' => 'Varchar(255)',
+
+    'SubjectEmailConfirmation' => 'Varchar(255)',
+    'ContentEmailConfirmation' => 'Varchar(255)',
+
+
+    'EmailSentFrom' => 'Varchar(255)',
+    'ReceiverEmail' => 'Varchar(255)',
   ];
 
   private static $has_one = [
@@ -23,6 +35,12 @@ class BAKSiteConfigExtension extends DataExtension
 
     //ADDRESS
     $fields->addFieldsToTab('Root.Produkte',[
+      new TextField("ReceiverEmail", "E-Mail Adressen Empfänger (kommagetrennt)"),
+      new TextField("SubjectEmail", "E-Mail Betreff"),
+      new TextareaField("ContentEmail", "E-Mail Inhalt")),
+      new EmailField('EmailSentFrom', 'E-Mail Absender'),
+      new TextField("SubjectEmailConfirmation", "Email Betreff Bestätigung"),
+      new TextareaField("ContentEmailConfirmation", "Email Inhalt Bestätigung"),
       TreeDropdownField::create('ConfirmationPageID','Produktformular Bestätigung Seite',SiteTree::class)
     ]);
     
