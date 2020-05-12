b28a6fc78fdf77aad12c2f807137220585eae303
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-12 22:55:42 +0200

    Update NewsAdmin.php

--- bak-news/src/NewsAdmin.php
+++ bak-news/src/NewsAdmin.php
@@ -29,10 +29,9 @@ class NewsAdmin extends ModelAdmin {
         $form = parent::getEditForm($id, $fields);
         if($this->modelClass== News::class && $gridField=$form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass)))
         {
-            print_r($form->Fields);
-            // $form->Fields()->fieldByName("News")->getConfig()->removeComponentsByType(GridFieldExportButton::class);
-            // $form->Fields()->fieldByName("News")->getConfig()->removeComponentsByType(GridFieldPrintButton::class);  
-            // $form->Fields()->fieldByName("News")->getConfig()->addComponent(new GridFieldPublishNews());
+            $gridField->getConfig()->removeComponentsByType(GridFieldExportButton::class);
+            $gridField->getConfig()->removeComponentsByType(GridFieldPrintButton::class);  
+            $gridField->getConfig()->addComponent(new GridFieldPublishNews());
         }
         if($this->modelClass== NewsCategory::class && $gridField=$form->Fields()->dataFieldByName($this->sanitiseClassName($this->modelClass))) 
         {
