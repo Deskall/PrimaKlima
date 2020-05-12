e689d2cada2e0872745e5f7c48942281b1ea2dd9
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-12 17:29:40 +0200

    Update FilterLocalExtension.php

--- deskall-translations/src/Extensions/FilterLocalExtension.php
+++ deskall-translations/src/Extensions/FilterLocalExtension.php
@@ -8,7 +8,7 @@ use TractorCow\Fluent\Model\Locale;
 class FilterLocalExtension extends DataExtension
 {
     public function updateCMSFields(FieldList $fields){
-        $fields->addFieldToTab('Root.Main',$fields->FieldByName('Root.Locales.FilteredLocales'));
+        $fields->dataFieldByName('FilteredLocales')->setTitle('Sprachen');
         // print_r('ici');
         //  $locales = $fields->FieldByName('Root.Locales.FilteredLocales');
         //  $fields->removeByName('FilteredLocales');
