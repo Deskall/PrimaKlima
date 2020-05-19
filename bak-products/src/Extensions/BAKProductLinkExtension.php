8f32c6b270a5f4133079d19a88bc6011eb6a8469
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-19 14:51:39 +0200

    s

---
+++ bak-products/src/Extensions/BAKProductLinkExtension.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace BAK\Products;
+
+use SilverStripe\ORM\DataExtension;
+use SilverStripe\Forms\FieldList;
+use BAK\Products\Models\Product;
+use SilverStripe\Forms\DropdownField;
+
+class BAKProductLinkExtension extends DataExtension {
+
+  private static $types = [
+    "Product"
+  ];
+
+  private static $has_one = [
+    "Product" => Product::class
+  ];
+
+  public function updateCMSFields(FieldList $fields) {
+    $products = Product::get()->sort('Name');
+    $fields->addFieldToTab('Root.Main', 
+      DropdownField::create('Product','Produkt',$products->map('ID','Name'))
+      ->setEmptyString('Bitte wählen Sie ein Produkt aus.')
+      ->displayIf('Type')->isEqualTo('Product')->end());
+  }
+  
+}
