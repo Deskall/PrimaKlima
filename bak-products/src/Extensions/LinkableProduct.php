9586d33b0a0035c49293cfa7fb90e113f9727a81
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-19 22:59:26 +0200

    f

---
+++ bak-products/src/Extensions/LinkableProduct.php
@@ -0,0 +1,24 @@
+<?php
+namespace Bak\Products;
+
+use SilverStripe\ORM\DataExtension;
+
+class LinkableProduct extends DataExtension {
+
+	public function updateLinkForm($form){
+		$fields = $form->Fields();
+		$linkType = $fields->dataFieldByName('LinkType');
+		$source = $linkType->getSource();
+		$source['Product'] = "Produktseite";
+		$linkType->setSource($source);
+		$products = Product::get()->sort('Name','ASC');
+		$sourceProductList = array();
+		foreach($products as $product){
+			$sourceProductList['[product_link,id='.$product->ID.']'] = $product->Name;
+		}
+		$productlist = DropdownField::create('ProductID', "Produkt", $sourceProductList);
+		$fields->insertAfter($productlist,'email');
+		$form->setFields($fields);
+		return $form;
+	}
+}
