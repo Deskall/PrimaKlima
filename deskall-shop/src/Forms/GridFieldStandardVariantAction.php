bdecb0327d8ce337d293c30c4759325750314cdf
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-05-26 17:29:43 +0200

    Update GridFieldStandardVariantAction.php

--- deskall-shop/src/Forms/GridFieldStandardVariantAction.php
+++ deskall-shop/src/Forms/GridFieldStandardVariantAction.php
@@ -43,10 +43,13 @@ class GridFieldStandardVariantAction implements GridField_ColumnProvider, GridFi
                 '',
                 "standard",
                 ['RecordID' => $record->ID]
-            )->addExtraClass('grid-field__icon-action font-icon-check-mark-circle btn--icon-large action action-detail')
+            )->addExtraClass('grid-field__icon-action font-icon-check-mark-circle icon-primary btn--icon-large action action-detail')
                 ->setAttribute('title', _t('SiteTree.StandardVariant', 'als standard Variant markieren'))
                 ->setDescription(_t('Global.StandardVariant', 'als standard Variant markieren'));
                 return $field->Field();
+        }
+        else{
+            return 'Standard';
         }
 
         return ;
