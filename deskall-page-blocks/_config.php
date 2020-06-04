2d9faf1db82c0d2be3101801dc8fef39ca1d75b5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-06-04 12:06:07 +0200

    Update _config.php

--- deskall-page-blocks/_config.php
+++ deskall-page-blocks/_config.php
@@ -1,6 +1,2 @@
 <?php
-
-use DNADesign\Elemental\ElementalEditor;
-
-/** Remove DNADesign Exsiting Field input from GRid (replaced by our own Virtual Block)**/
-ElementalEditor::remove_extension('DNADesign\ElementalVirtual\Extensions\ElementalEditorExtension');
+/* Deskall Blocks Module*/
