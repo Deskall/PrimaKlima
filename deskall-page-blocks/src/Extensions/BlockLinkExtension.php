91945f7e8eefdd2b6f6cb390bda70809eaa0494a
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-06-11 12:06:49 +0200

    Update BlockLinkExtension.php

--- deskall-page-blocks/src/Extensions/BlockLinkExtension.php
+++ deskall-page-blocks/src/Extensions/BlockLinkExtension.php
@@ -11,6 +11,7 @@ use DNADesign\Elemental\Models\BaseElement;
 
 class BlockLinkExtension extends DataExtension
 {
+    private static $types = ['block' => 'bestimmt Block von dieser Website'] 
 
     private static $has_one = [
         'Block' => BaseElement::class
@@ -18,32 +19,37 @@ class BlockLinkExtension extends DataExtension
 
 
     function updateFieldLabels(&$labels) {
-        $labels['Block'] = _t(__CLASS__.'.Block', 'Block von dieser Seite');
+        $labels['Block'] = _t(__CLASS__.'.Block', 'bestimmt Block von dieser Website');
     }
 
 
     public function updateCMSFields(FieldList $fields){
         $fields->removeByName('BlockID');
             $blocks = $this->getBlockTree();
-            $fields->addFieldToTab('Root.Main',DropdownField::create('BlockID',_t(__CLASS__.'.Block','Block von dieser Seite'),$blocks)->displayIf('Type')->isEqualTo('SiteTree')->end());
+            $fields->addFieldToTab('Root.Main',DropdownField::create('BlockID',_t(__CLASS__.'.Block','Block von dieser Seite'),$blocks)->displayIf('Type')->isEqualTo('Block')->end());
     }
 
     protected function getBlockTree(){
-        $blockstree = array(0 => _t(__CLASS__.'.Label','Bitte block wählen'));
-        $page = $this->owner->SiteTree();
-        if ($page->exists()){
+        $blockstree = array(0 => _t(__CLASS__.'.Label','Bitte Block wählen'));
+        $Pages = Page::get()->sort('ParentID ASC, Sort ASC');
+        foreach ($Pages as $page) {
             if ($page->ElementalAreaID > 0){
+                $blocks = array();
                 foreach ($page->ElementalArea()->Elements() as $block) {
-                    $blockstree[$block->ID] = $block->singleton($block->ClassName)->getType(). " > ".$block->NiceTitle();
+                    
+                    $blocks[$block->ID] = $block->singleton($block->ClassName)->getType(). " > ".$block->NiceTitle();
                     if ($block->ClassName == "ParentBlock"){
                         foreach ($block->Elements()->Elements() as $underblock) {
-                        $blockstree[$underblock->ID] = "  ".$block->NiceTitle(). " > ".$underblock->singleton($underblock->ClassName)->getType(). " > ".$underblock->NiceTitle();
+                        $blocks[$underblock->ID] = "  ".$block->NiceTitle(). " > ".$underblock->singleton($underblock->ClassName)->getType(). " > ".$underblock->NiceTitle();
                         }
                     }
                 }
+                //build the page unique sitetree strucuture
+                $pageTree = $page->ID.' - '.$page->NestedTitle(4," > ");
+            
+                $blockstree[$pageTree] = $blocks;
             }
         }
-            
         return $blockstree;
     }
 }
