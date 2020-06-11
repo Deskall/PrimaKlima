05e8503c1094b122815b0df277ac453c61cc94c1
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-06-11 11:22:03 +0200

    Update BlockLinkExtension.php

--- deskall-page-blocks/src/Extensions/BlockLinkExtension.php
+++ deskall-page-blocks/src/Extensions/BlockLinkExtension.php
@@ -26,7 +26,7 @@ class BlockLinkExtension extends DataExtension
         $fields->removeByName('BlockID');
         if ($this->owner->ID > 0){
             $blocks = $this->owner->getBlockTree();
-            $fields->addFieldToTab('Root.Main',DropdownField::create('BlockID',_t(__CLASS__.'.Block','Block von dieser Seite'),$blocks)->displayIf('Type')->isEqualTo('SiteTree')->end());
+            $fields->addFieldToTab('Root.Main',DropdownField::create('BlockID',_t(__CLASS__.'.Block','Block von dieser Seite'),$blocks)->displayIf('Type')->isEqualTo('SiteTree')->end(), 'SiteTree');
         }
         $fields->addFieldToTab('Root.Main',LabelField::create('Block',_t(__CLASS__.'.Block','Block von dieser Seite (bitte zuerst Seite Wählen und speichern)'))->displayIf('Type')->isEqualTo('SiteTree')->end(), 'SiteTreeID');
     }
