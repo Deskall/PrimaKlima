1ebae2b550cd705a84423622c5d4d5a72941e396
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-07-03 13:54:48 +0200

    Update CheckEmailField.js

--- mysite/javascript/CheckEmailField.js
+++ mysite/javascript/CheckEmailField.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 	$(document).on("paste","[data-referent-name]",function(e){
 		console.log('ahha try to paste? NO!');
+		e.preventDefault();
 	});
 	$(document).on("change","[data-referent-name]",function(e){
 		console.log('ici');
