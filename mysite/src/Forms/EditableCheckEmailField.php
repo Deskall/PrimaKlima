63f0d24f648c8890b61149f148a915843333dccb
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-07-03 13:44:45 +0200

    Update EditableCheckEmailField.php

--- mysite/src/Forms/EditableCheckEmailField.php
+++ mysite/src/Forms/EditableCheckEmailField.php
@@ -3,6 +3,7 @@
 use SilverStripe\UserForms\Model\EditableFormField\EditableEmailField;
 use SilverStripe\View\Requirements;
 use SilverStripe\Forms\EmailField;
+use SilverStripe\Forms\DropdownField;
 use SilverStripe\UserForms\Model\EditableFormField;
 
 /**
@@ -22,6 +23,16 @@ class EditableCheckEmailField extends EditableEmailField
     private static $has_one = [
         'Referent' => EditableEmailField::class
     ];
+
+    public function getCMSFields(){
+        $fields = parent::getCMSFields();
+        $referents = $this->Parent()->Fields()->filter('ClassName',EditableEmailField::class);
+        if ($referents){
+            $fields->insertAfter('Title',DropdownField::create('Referent','Referenz',$referents->map('ID','Title'))->setEmptyString('Bitte E-Mail Feld auswählen'));
+        }
+        
+        return $fields;
+    }
 
     public function getSetsOwnError()
     {
@@ -50,5 +61,40 @@ class EditableCheckEmailField extends EditableEmailField
         parent::updateFormField($field);
 
         $field->setAttribute('data-rule-email', true);
+    }
+
+    public function validateField($data, $form)
+    {
+        $formField = $this->getFormField();
+        $formField->setForm($form);
+
+        if (isset($data[$this->Name])) {
+            $formField->setValue($data[$this->Name]);
+        }
+
+        $validator = $form->getValidator();
+        if (!$formField->validate($validator)) {
+            $errors = $validator->getErrors();
+            $foundError = false;
+
+            // field validate implementation may not add error to validator
+            if (count($errors) > 0) {
+                // check if error already added from fields' validate method
+                foreach ($errors as $error) {
+                    if ($error['fieldName'] == $this->Name) {
+                        $foundError = $error;
+                        break;
+                    }
+                }
+            }
+
+            if ($foundError !== false) {
+                // use error messaging already set from validate method
+                $form->sessionMessage($foundError['message'], $foundError['messageType']);
+            } else {
+                // fallback to custom message set in CMS or default message if none set
+                $form->sessionError($this->getErrorMessage()->HTML());
+            }
+        }
     }
 }
