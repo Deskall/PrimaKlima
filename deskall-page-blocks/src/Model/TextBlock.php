e5285522193fa6ca04dd485daaa1ac3af2ac95ef
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-06 15:17:34 +0200

    Update TextBlock.php

--- deskall-page-blocks/src/Model/TextBlock.php
+++ deskall-page-blocks/src/Model/TextBlock.php
@@ -53,22 +53,22 @@ class TextBlock extends BaseElement implements Searchable
         'left' => [
             'value' => 'left',
             'title' => 'Links',
-            'icon' => '/deskall-page-blocks/images/icon-text-left.svg'
+            'icon' => '/_resources/deskall-page-blocks/images/icon-text-left.svg'
         ],
         'right' => [
             'value' => 'right',
             'title' => 'Rechts',
-            'icon' => '/deskall-page-blocks/images/icon-text-right.svg'
+            'icon' => '/_resources/deskall-page-blocks/images/icon-text-right.svg'
         ],
         'under' => [
             'value' => 'under',
             'title' => 'Unten',
-            'icon' => '/deskall-page-blocks/images/icon-text-under.svg'
+            'icon' => '/_resources/deskall-page-blocks/images/icon-text-under.svg'
         ],
         'above' => [
             'value' => 'above',
             'title' => 'Oben',
-            'icon' => '/deskall-page-blocks/images/icon-text-above.svg'
+            'icon' => '/_resources/deskall-page-blocks/images/icon-text-above.svg'
         ],
     ];
 
