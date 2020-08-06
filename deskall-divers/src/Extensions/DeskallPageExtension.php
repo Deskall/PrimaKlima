6dd8a08cf9d5fcc43aca07911473453ce1f755e0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-06 16:03:42 +0200

    Update DeskallPageExtension.php

--- deskall-divers/src/Extensions/DeskallPageExtension.php
+++ deskall-divers/src/Extensions/DeskallPageExtension.php
@@ -41,7 +41,7 @@ class DeskallPageExtension extends DataExtension
     }
 
     public function LastChangeJS(){
-        $srcDir = Director::baseFolder().$this->owner->ThemeDir().'/javascript/vendor';
+        $srcDir = Director::baseFolder().'themes/standard/javascript/vendor';
         $srcFiles = array_diff(scandir($srcDir), array('.', '..'));
         $filetime = 0;
         foreach($srcFiles as $key => $file) {
