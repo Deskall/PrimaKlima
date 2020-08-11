e812ffd172bc4109bfb88249e19425777421b5fd
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-11 11:15:55 +0200

    Update HomePage.php

--- deskall-divers/src/Models/HomePage.php
+++ deskall-divers/src/Models/HomePage.php
@@ -1,8 +1,10 @@
 <?php
 
 class HomePage extends Page {
-	private static $icon = "font-icon-home";
+	// private static $icon = "font-icon-home";
 
+	private static $icon_class = "font-icon-home";
+	
 	private static $description = "Startseite";
 
 	public function canCreate($member = null, $context = null){
