91f8e92d2368385b12aee38d482d9e403f87750a
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-11 15:16:51 +0200

    e

--- deskall-page-blocks/src/Model/ActionBlock.php
+++ deskall-page-blocks/src/Model/ActionBlock.php
@@ -13,6 +13,8 @@ use g4b0\SearchableDataObjects\Searchable;
 
 class ActionBlock extends BaseElement implements Searchable
 {
+    private static $inline_editable = false;
+    
     private static $icon = 'font-icon-plus-circled';
     
     private static $controller_template = 'BlockHolder';
