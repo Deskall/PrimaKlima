022906ca083da627dab718aee114f78e44a32ef0
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-13 11:42:17 +0200

    w

--- deskall-images/src/DeskallImageOptimizer.php
+++ deskall-images/src/DeskallImageOptimizer.php
@@ -1,28 +1,32 @@
 <?php
 
+use SilverStripe\Core\Environment;
+
 class DeskallImageOptimiser {
-	protected $api_key = 'gG1Tujhnd39tTlK5Wg8fKwbN6a3HVDA4';
+	public function getApiKey(){
+		return Environment::getEnv('APP_TINYPNG_APIKEY');
+	}
 
 	public function Optimise($url, $path){
 		try {
-    		\Tinify\setKey($this->api_key);
+    		\Tinify\setKey($this->getApiKey());
 			$source = \Tinify\fromUrl($url);
 			$source->toFile($path);
 		} catch(\Tinify\AccountException $e) {
-		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","\n"."account: " . $e->getMessage(), FILE_APPEND);
+		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log-tiny-image.txt","\n"."account: " . $e->getMessage(), FILE_APPEND);
 		    // Verify your API key and account limit.
 		} catch(\Tinify\ClientException $e) {
 		    // Check your source image and request options.
-		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","\n"."client: " . $e->getMessage(), FILE_APPEND);
+		    file_put_contents($_SERVER['DOCUMENT_ROOT']."/log-tiny-image.txt","\n"."client: " . $e->getMessage(), FILE_APPEND);
 		} catch(\Tinify\ServerException $e) {
 		    // Temporary issue with the Tinify API.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","\n"."tiny server: " . $e->getMessage(), FILE_APPEND);
+		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log-tiny-image.txt","\n"."tiny server: " . $e->getMessage(), FILE_APPEND);
 		} catch(\Tinify\ConnectionException $e) {
 		    // A network connection error occurred.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","\n"."connection: " . $e->getMessage(), FILE_APPEND);
+		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log-tiny-image.txt","\n"."connection: " . $e->getMessage(), FILE_APPEND);
 		} catch(Exception $e) {
 		    // Something else went wrong, unrelated to the Tinify API.
-		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt","\n"."other: " . $e->getMessage(), FILE_APPEND);
+		     file_put_contents($_SERVER['DOCUMENT_ROOT']."/log-tiny-image.txt","\n"."other: " . $e->getMessage(), FILE_APPEND);
 		}
 	}
 } 
