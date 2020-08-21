5dbf04561493ae93b08ac8559c29fd7735e00935
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-21 19:05:32 +0200

    Update ShopController.php

--- yplay-shop/src/Controllers/ShopController.php
+++ yplay-shop/src/Controllers/ShopController.php
@@ -77,7 +77,7 @@ class ShopController extends PageController
       if ($id){
          $cart = ShopCart::get()->byId($id);
       }
-      if (!$cart && ($products || $packageID > 0)){
+      if (!$cart && (count($products) > 0 || $packageID > 0)){
          $cart = new ShopCart();
          $cart->IP = $this->getRequest()->getIp();
          if ($this->activePLZ()->exists()){
@@ -88,7 +88,7 @@ class ShopController extends PageController
          $this->getRequest()->getSession()->set('shopcart_id',$cart->ID);
       }
 
-      if ($cart && ($products || $packageID > 0)){
+      if ($cart && (count($products) > 0 || $packageID > 0)){
          //apply package and product
          $cart->PackageID = $packageID;
          if ($cart->PackageID > 0){
