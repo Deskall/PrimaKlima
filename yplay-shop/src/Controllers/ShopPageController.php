1eb8e91bd57dc27334e887a7c98641f46f0c9892
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-08-27 15:52:38 +0200

    Update ShopPageController.php

--- yplay-shop/src/Controllers/ShopPageController.php
+++ yplay-shop/src/Controllers/ShopPageController.php
@@ -193,7 +193,7 @@ class ShopPageController extends PageController
             )->setName('BillFields'),
             CompositeField::create(
                HeaderField::create('OtherTitle','Weitere Angaben',3),
-               DropdownField::create('PreviousProvider','Bisheriger Anbieter',['YplaY' => 'YplaY', 'Swisscom' => 'Swisscom', 'Sunrise' => 'Sunrise','UPC' => 'UPC','Andere' => 'Andere'])->setEmptyString('Bitte wählen'),
+               DropdownField::create('PreviousProvider','Bisheriger Anbieter',['YplaY' => 'YplaY', 'Swisscom' => 'Swisscom', 'Sunrise' => 'Sunrise','UPC' => 'UPC','Andere' => 'Andere'])->setEmptyString('Bitte wählen')->setAttribute('class','uk-select'),
                TextareaField::create('Comments','Bemerkungen'),
                // CheckboxField::create('Newsletter','Ich abonniere den Newsletter'),
                CheckboxField::create('AGB',DBHTMLText::create()->setValue('Ich bin mit den <a href="'.$agbpage->Link().'" target="_blank" title="AGB anschauen">AGB</a> einverstanden'))->setAttribute('class','uk-checkbox'),
