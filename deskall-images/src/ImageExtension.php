eb5667b3061663f57af7c03abdc25c6d69809093
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-01 14:14:22 +0200

    Update ImageExtension.php

--- deskall-images/src/ImageExtension.php
+++ deskall-images/src/ImageExtension.php
@@ -60,6 +60,10 @@ class ImageExtension extends Extension
         $file->publishSingle();
         if (Environment::getEnv('APP_OPTIMISE_TINY') && !$file->Optimised){
             //Optimise via TinyPNG API
+            ob_start();
+                        print_r('ici');
+                        $result = ob_get_clean();
+                        file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
             $this->OptimiseImage(Director::absoluteURL($$file->getSourceURL()), $_SERVER['DOCUMENT_ROOT'].$file->getSourceURL());
             $file->Optimised = 1;
             $file->write();
