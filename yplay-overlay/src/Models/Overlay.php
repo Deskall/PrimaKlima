32c1aef5b71690b367d8a7ee71b2a8ceda0087c9
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-07 14:11:17 +0200

    Update Overlay.php

--- yplay-overlay/src/Models/Overlay.php
+++ yplay-overlay/src/Models/Overlay.php
@@ -95,10 +95,10 @@ class Overlay extends DataObject{
 		$fields->fieldByName('Root.Main.Subtitle')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.Content')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.LinkableLinkID')->hideIf('Type')->isEqualTo('Form')->end();
-		$fields->fieldByName('Root.Layout.ValidButtonBackground')->hideIf('Type')->isEqualTo('Form')->orIf('LinkableLinkID')->isNotEqualTo(0)->end();
-		$fields->fieldByName('Root.Main.ValidButtonText')->hideIf('Type')->isEqualTo('Form')->orIf('LinkableLinkID')->isNotEqualTo(0)->end();
+		$fields->fieldByName('Root.Layout.ValidButtonBackground')->hideIf('Type')->isEqualTo('Form')->orIf('LinkableLinkID')->isGreaterThan(0)->end();
+		$fields->fieldByName('Root.Main.ValidButtonText')->hideIf('Type')->isEqualTo('Form')->orIf('LinkableLinkID')->isGreaterThan(0)->end();
 		$fields->fieldByName('Root.Main.FormBlockID')->displayIf('Type')->isEqualTo('Form')->end();
-		
+
 		return $fields;
 	}
 }
