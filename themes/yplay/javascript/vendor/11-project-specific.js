761fc514639e0cc75ccab4f6c6f5cefd17865756
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-07 08:51:51 +0200

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -30,12 +30,19 @@
 $(window).ready(function(){
 	let deferredPrompt;
 
-	window.addEventListener('beforeinstallprompt', (e) => {
-	  // Prevent Chrome 67 and earlier from automatically showing the prompt
-	  e.preventDefault();
-	  // Stash the event so it can be triggered later.
-	  deferredPrompt = e;
-	  $(".smart-app-container").attr("hidden",false);
+	// window.addEventListener('beforeinstallprompt', (e) => {
+	//   // Prevent Chrome 67 and earlier from automatically showing the prompt
+	//   e.preventDefault();
+	//   // Stash the event so it can be triggered later.
+	//   deferredPrompt = e;
+	//   $(".smart-app-container").attr("hidden",false);
+	// });
+
+	$(window).on('beforeinstallprompt', function (e) {
+		e.preventDefault();
+		// Stash the event so it can be triggered later.
+		deferredPrompt = e;
+		$(".smart-app-container").attr("hidden",false);
 	});
 });
 
