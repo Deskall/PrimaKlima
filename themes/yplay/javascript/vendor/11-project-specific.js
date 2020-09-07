7100446d37abf2bec06b3e92e646943243cd0b38
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-07 11:05:17 +0200

    Update 11-project-specific.js

--- themes/yplay/javascript/vendor/11-project-specific.js
+++ themes/yplay/javascript/vendor/11-project-specific.js
@@ -814,11 +814,13 @@ $(document).ready(function(){
 	let overlay;
 	if ($(".overlay-modal").length > 0){
 		overlay = $(".overlay-modal");
-		if (overlay.attr('data-trigger-type') == 'Time' && overlay.attr('data-triggered') == "false"){
-			setTimeout(function(){
-				UIkit.modal("#" + overlay.attr('id')).show();
-				overlay.attr('data-triggered',true);
-			}, overlay.attr('data-trigger-time') * 1000);
+		if (overlay.attr('data-trigger-type') == 'Time') {
+			if (overlay.attr('data-triggered') == "false"){
+				setTimeout(function(){
+					UIkit.modal("#" + overlay.attr('id')).show();
+					overlay.attr('data-triggered',true);
+				}, overlay.attr('data-trigger-time') * 1000);
+			}
 		}
 		else{
 			$(document).bind("mouseleave", function(e) {
