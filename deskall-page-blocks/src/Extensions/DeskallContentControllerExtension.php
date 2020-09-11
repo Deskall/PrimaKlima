afdfe35771b6bd684e368d95ec0a418b9eb7bbba
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-11 11:56:17 +0200

    j

--- deskall-page-blocks/src/Extensions/DeskallContentControllerExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallContentControllerExtension.php
@@ -5,98 +5,55 @@
 use DNADesign\Elemental\Models\ElementalArea;
 use DNADesign\Elemental\Extensions\ElementalAreasExtension;
 use SilverStripe\Core\Extension;
-use DNADesign\Elemental\Models\BaseElement;
 
 class DeskallContentControllerExtension extends Extension
 {
-    // /**
-    //  * @var array
-    //  */
-    // private static $allowed_actions = array(
-    //     'handleChildren'
-    // );
+    /**
+     * @var array
+     */
+    private static $allowed_actions = array(
+        'handleChildren'
+    );
 
-    // public function handleChildren()
-    // {
-    //     $parentId = $this->owner->getRequest()->param('OTHERID');
-    //     $id = $this->owner->getRequest()->param('ID');
-
-    //     if (!$parentId) {
-    //         user_error('No parent ID supplied', E_USER_ERROR);
-    //         return false;
-    //     }
-    //     if (!$id) {
-    //         user_error('No element ID supplied', E_USER_ERROR);
-    //         return false;
-    //     }
-
-    //     /** @var SiteTree $elementOwner */
-    //     $elementOwner = $this->owner->data();
-
-    //     $elementalAreaRelations = $this->owner->getElementalRelations();
-
-    //     if (!$elementalAreaRelations) {
-    //         user_error(get_class($this->owner) . ' has no ElementalArea relationships', E_USER_ERROR);
-    //         return false;
-    //     }
-
-    //     foreach ($elementalAreaRelations as $elementalAreaRelation) {
-    //         $parent = $elementOwner->$elementalAreaRelation()->Elements()
-    //             ->filter('ID', $parentId)
-    //             ->First();
-    //         foreach ($parent->getElementalRelations() as $elementalAreaRelation) {
-    //            $element = $parent->$elementalAreaRelation()->Elements()
-    //             ->filter('ID', $id)
-    //             ->First();
-    //             if ($element) {
-    //                 return $element->getController();
-    //             }
-    //         }
-    //     }
-
-    //     user_error('Parent $parentId not found for this page', E_USER_ERROR);
-    //     return false;
-    // }
-
-    public function handleElement(){
+    public function handleChildren()
+    {
+        $parentId = $this->owner->getRequest()->param('OTHERID');
         $id = $this->owner->getRequest()->param('ID');
 
+        if (!$parentId) {
+            user_error('No parent ID supplied', E_USER_ERROR);
+            return false;
+        }
         if (!$id) {
             user_error('No element ID supplied', E_USER_ERROR);
             return false;
         }
 
-        $element = BaseElement::get()->byId($id);
-        if (!$element) {
-            user_error('No element found', E_USER_ERROR);
+        /** @var SiteTree $elementOwner */
+        $elementOwner = $this->owner->data();
+
+        $elementalAreaRelations = $this->owner->getElementalRelations();
+
+        if (!$elementalAreaRelations) {
+            user_error(get_class($this->owner) . ' has no ElementalArea relationships', E_USER_ERROR);
             return false;
         }
 
-        if ($element) {
-            return $element->getController();
+        foreach ($elementalAreaRelations as $elementalAreaRelation) {
+            $parent = $elementOwner->$elementalAreaRelation()->Elements()
+                ->filter('ID', $parentId)
+                ->First();
+            foreach ($parent->getElementalRelations() as $elementalAreaRelation) {
+               $element = $parent->$elementalAreaRelation()->Elements()
+                ->filter('ID', $id)
+                ->First();
+                if ($element) {
+                    return $element->getController();
+                }
+            }
         }
-        
-        /** @var SiteTree $elementOwner */
-        // $elementOwner = $this->owner->data();
 
-        // $elementalAreaRelations = $this->owner->getElementalRelations();
-
-        // if (!$elementalAreaRelations) {
-        //     user_error(get_class($this->owner) . ' has no ElementalArea relationships', E_USER_ERROR);
-        //     return false;
-        // }
-
-        // foreach ($elementalAreaRelations as $elementalAreaRelation) {
-        //     $element = $elementOwner->$elementalAreaRelation()->Elements()
-        //         ->filter('ID', $id)
-        //         ->First();
-
-        //     if ($element) {
-        //         return $element->getController();
-        //     }
-        // }
-
-        user_error('Element $id not found for this page', E_USER_ERROR);
+        user_error('Parent $parentId not found for this page', E_USER_ERROR);
         return false;
     }
 }
