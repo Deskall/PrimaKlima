3008e26e1b019d5f5ba6b33575db60972e1194b5
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-11 12:45:24 +0200

    Update MigrateFormElement.php

--- deskall-page-blocks/src/Tasks/MigrateFormElement.php
+++ deskall-page-blocks/src/Tasks/MigrateFormElement.php
@@ -5,6 +5,7 @@
 use SilverStripe\Dev\BuildTask;
 use DNADesign\ElementalUserForms\Model\ElementForm;
 use SilverStripe\UserForms\Model\Recipient\EmailRecipient;
+use SilverStripe\UserForms\Model\Submission\SubmittedForm;
 
 class MigrateFormElement extends BuildTask
 {
@@ -25,8 +26,9 @@ class MigrateFormElement extends BuildTask
             $newForm->write();
             $fields = $form->fields();
             $recs = EmailRecipient::get()->filter('FormID',$form->ID);
+            $subms = SubmittedForm::get()->filter('ParentID',$form->ID);
 
-            //Upadte Recipient and Fields
+            //Upadte Recipient and Fields and Submissions
             foreach ($fields as $field) {
                 $field->ParentID = $newForm->ID;
                 $field->ParentClass = $newForm->ClassName;
@@ -36,6 +38,11 @@ class MigrateFormElement extends BuildTask
                 $rec->FormID = $newForm->ID;
                 $rec->FormClass = $newForm->ClassName;
                 $rec->write();
+            }
+            foreach ($subms as $sub) {
+                $sub->ParentID = $newForm->ID;
+                $sub->ParentClass = $newForm->ClassName;
+                $sub->write();
             }
             $newForm->publish();
             $form->delete();
