62e7888b80e5c1ca4f34fa6fe51c28e1b99c1bbc
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-14 09:47:29 +0200

    Update Overlay.php

--- yplay-overlay/src/Models/Overlay.php
+++ yplay-overlay/src/Models/Overlay.php
@@ -114,7 +114,7 @@ class Overlay extends DataObject{
 		$fields->fieldByName('Root.Main.Subtitle')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.Content')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Layout.ValidButtonBackground')->hideIf('Type')->isEqualTo('Form')->end();
-		$fields->fieldByName('Root.Main.ValidButtonText')->hideIf('Type')->isEqualTo('Form')->end();
+		// $fields->fieldByName('Root.Main.ValidButtonText')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.FormBlockID')->displayIf('Type')->isEqualTo('Form')->end();
 
 		return $fields;
