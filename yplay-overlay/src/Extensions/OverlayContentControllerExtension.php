0fef7bcfabdacbef792d5fb29d4fff66f7fdc389
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-14 13:57:20 +0200

    w

--- yplay-overlay/src/Extensions/OverlayContentControllerExtension.php
+++ yplay-overlay/src/Extensions/OverlayContentControllerExtension.php
@@ -17,7 +17,7 @@ use UndefinedOffset\NoCaptcha\Forms\NocaptchaField;
 use SilverStripe\ORM\FieldType\DBHTMLText;
 use SilverStripe\View\Requirements;
 
-class OverlayContentController extends Extension
+class OverlayContentControllerExtension extends Extension
 {
     /**
      * @var array
@@ -29,7 +29,7 @@ class OverlayContentController extends Extension
     );
 
     public function onAfterInit(){
-        if ($this->hasMethod('Overlay') && $this->Overlay()->exists() && $this->Overlay()->Type == "Bewertung"){
+        if ($this->owner->hasMethod('Overlay') && $this->owner->Overlay()->exists() && $this->owner->Overlay()->Type == "Bewertung"){
             Requirements::javascript('yplay-overlay/javascript/jquery.rateyo.min.js');
             Requirements::css('yplay-overlay/css/jquery.rateyo.min.css');
         }
@@ -37,7 +37,7 @@ class OverlayContentController extends Extension
 
     public function handleElement()
     {
-        $id = $this->getRequest()->param('ID');
+        $id = $this->owner->getRequest()->param('ID');
 
         if (!$id) {
             user_error('No element ID supplied', E_USER_ERROR);
@@ -45,9 +45,9 @@ class OverlayContentController extends Extension
         }
 
         /** @var SiteTree $elementOwner */
-        $elementOwner = $this->data();
+        $elementOwner = $this->owner->data();
 
-        $elementalAreaRelations = $this->getElementalRelations();
+        $elementalAreaRelations = $this->owner->getElementalRelations();
 
         if (!$elementalAreaRelations) {
             user_error(get_class($this) . ' has no ElementalArea relationships', E_USER_ERROR);
@@ -79,12 +79,12 @@ class OverlayContentController extends Extension
     public function NewsletterForm(){
         $fields = new FieldList(
             EmailField::create('Email', 'Ihre E-Mail-Adresse')->setAttribute('Class','uk-input'),
-            CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_NewsletterForm_AGB">'.$this->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
+            CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_NewsletterForm_AGB">'.$this->owner->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
         );
 
         $actions = new FieldList(
-            FormAction::create('cancel')->setTitle($this->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
-            FormAction::create('registerToNewsletter')->setTitle($this->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
+            FormAction::create('cancel')->setTitle($this->owner->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->owner->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
+            FormAction::create('registerToNewsletter')->setTitle($this->owner->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
             ->setAttribute('data-uk-icon','chevron-right')
         );
 
@@ -106,22 +106,22 @@ class OverlayContentController extends Extension
         $form->sessionMessage('Email ' . $data['Email'] . ' registriert!', 'success');
 
         //TO DO: Redirect to succss page?
-        return $this->redirect('/');
+        return $this->owner->redirect('/');
     }
 
     public function BewertungForm(){
         $fields = new FieldList(
-            HiddenField::create('OverlayID')->setValue($this->OverlayID),
+            HiddenField::create('OverlayID')->setValue($this->owner->OverlayID),
             HiddenField::create('Datum')->setValue(date('d.m.Y')),
-            HiddenField::create('PLZ')->setValue($this->getRequest()->getSession()->get('active_plz')),
+            HiddenField::create('PLZ')->setValue($this->owner->getRequest()->getSession()->get('active_plz')),
             HiddenField::create('Bewertung'),
             TextareaField::create('Bemerkungen','Bemerkungen')->setAttribute('maxlength',500)->setAttribute('class','uk-textarea')->setRows('5'),
-            CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_BewertungForm_AGB">'.$this->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
+            CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_BewertungForm_AGB">'.$this->owner->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
         );
 
         $actions = new FieldList(
-            FormAction::create('cancel')->setTitle($this->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
-            FormAction::create('doRate')->setTitle($this->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
+            FormAction::create('cancel')->setTitle($this->owner->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->owner->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
+            FormAction::create('doRate')->setTitle($this->owner->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
             ->setAttribute('data-uk-icon','chevron-right')
         );
 
@@ -142,10 +142,10 @@ class OverlayContentController extends Extension
 
             //Send mails
             // $config = SiteConfig::current_site_config();
-            // $str = $this->parseString($config->ProductEmailContent, $data);
+            // $str = $this->owner->parseString($config->ProductEmailContent, $data);
             // $html = new DBHTMLText();
             // $html->setValue($str);
-            // $Body = $this->renderWith('Emails/base_email',array('Subject' => $config->ProductEmailSubject, 'Lead' => '', 'Body' => $html, 'Footer' => '', 'SiteConfig' => $config));
+            // $Body = $this->owner->renderWith('Emails/base_email',array('Subject' => $config->ProductEmailSubject, 'Lead' => '', 'Body' => $html, 'Footer' => '', 'SiteConfig' => $config));
             // $email = new Email($config->Email, $data['Email'],$config->ProductEmailSubject, $Body);
             // $email->setBCC($config->Email);
             // $email->send();
@@ -157,10 +157,10 @@ class OverlayContentController extends Extension
                 $validationMessages .= $error['message']."\n";
             }
             $form->sessionMessage($validationMessages, 'bad');
-            return $this->redirectBack();
+            return $this->owner->redirectBack();
         }
 
-        return $this->redirectBack();
+        return $this->owner->redirectBack();
 
     }
 }
