65725497faf0f5eeb9d17b7961089961e9f7396f
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-14 09:48:55 +0200

    Update Overlay.php

--- yplay-overlay/src/Models/Overlay.php
+++ yplay-overlay/src/Models/Overlay.php
@@ -113,7 +113,7 @@ class Overlay extends DataObject{
 		$fields->fieldByName('Root.Main.CountDown')->displayIf('Type')->isEqualTo('Text')->end();
 		$fields->fieldByName('Root.Main.Subtitle')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.Content')->hideIf('Type')->isEqualTo('Form')->end();
-		// $fields->fieldByName('Root.Layout.ValidButtonBackground')->hideIf('Type')->isEqualTo('Form')->end();
+		$fields->fieldByName('Root.Main.AGB')->displayIf('Type')->isEqualTo('Newsletter')->end();
 		// $fields->fieldByName('Root.Main.ValidButtonText')->hideIf('Type')->isEqualTo('Form')->end();
 		$fields->fieldByName('Root.Main.FormBlockID')->displayIf('Type')->isEqualTo('Form')->end();
 
