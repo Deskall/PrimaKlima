c2e3e57271a1350f5a8064d815ded56438dd7c2d
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-14 13:53:36 +0200

    w

--- yplay-overlay/src/Controller/OverlayContentController.php
+++ yplay-overlay/src/Controller/OverlayContentController.php
@@ -1,6 +1,6 @@
 <?php
 
-
+use SilverStripe\CMS\Controllers\ContentController;
 use DNADesign\Elemental\Models\ElementalArea;
 use DNADesign\Elemental\Extensions\ElementalAreasExtension;
 use SilverStripe\Core\Extension;
@@ -17,7 +17,7 @@ use UndefinedOffset\NoCaptcha\Forms\NocaptchaField;
 use SilverStripe\ORM\FieldType\DBHTMLText;
 use SilverStripe\View\Requirements;
 
-class OverlayContentController extends PageController
+class OverlayContentController extends ContentController
 {
     /**
      * @var array
@@ -109,58 +109,58 @@ class OverlayContentController extends PageController
         return $this->redirect('/');
     }
 
-    // public function BewertungForm(){
-    //     $fields = new FieldList(
-    //         HiddenField::create('OverlayID')->setValue($this->OverlayID),
-    //         HiddenField::create('Datum')->setValue(date('d.m.Y')),
-    //         HiddenField::create('PLZ')->setValue($this->getRequest()->getSession()->get('active_plz')),
-    //         HiddenField::create('Bewertung'),
-    //         TextareaField::create('Bemerkungen','Bemerkungen')->setAttribute('maxlength',500)->setAttribute('class','uk-textarea')->setRows('5'),
-    //         CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_BewertungForm_AGB">'.$this->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
-    //     );
+    public function BewertungForm(){
+        $fields = new FieldList(
+            HiddenField::create('OverlayID')->setValue($this->OverlayID),
+            HiddenField::create('Datum')->setValue(date('d.m.Y')),
+            HiddenField::create('PLZ')->setValue($this->getRequest()->getSession()->get('active_plz')),
+            HiddenField::create('Bewertung'),
+            TextareaField::create('Bemerkungen','Bemerkungen')->setAttribute('maxlength',500)->setAttribute('class','uk-textarea')->setRows('5'),
+            CheckboxField::create('AGB',DBHTMLText::create()->setValue('<label for="Form_BewertungForm_AGB">'.$this->Overlay()->AGBText.'</label>'))->setAttribute('class','uk-checkbox')
+        );
 
-    //     $actions = new FieldList(
-    //         FormAction::create('cancel')->setTitle($this->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
-    //         FormAction::create('doRate')->setTitle($this->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
-    //         ->setAttribute('data-uk-icon','chevron-right')
-    //     );
+        $actions = new FieldList(
+            FormAction::create('cancel')->setTitle($this->Overlay()->CloseButtonText)->addExtraClass('uk-button button-'.$this->Overlay()->CloseButtonBackground.' uk-modal-close')->setUseButtonTag(true),
+            FormAction::create('doRate')->setTitle($this->Overlay()->ValidButtonText)->addExtraClass('uk-button button-PrimaryBackground')->addExtraClass('dk-button-icon')->setUseButtonTag(true)
+            ->setAttribute('data-uk-icon','chevron-right')
+        );
 
-    //     $required = new RequiredFields(['Bewertung','AGB']);
+        $required = new RequiredFields(['Bewertung','AGB']);
 
-    //     $form = new Form($this, 'BewertungForm', $fields, $actions, $required);
-    //     $form->addExtraClass('form-std');
-    //     $form->enableSpamProtection();
-    //     return $form;
-    // }
+        $form = new Form($this, 'BewertungForm', $fields, $actions, $required);
+        $form->addExtraClass('form-std');
+        $form->enableSpamProtection();
+        return $form;
+    }
 
-    // public function doRate($data, Form $form){
-    //     try {
-    //         //Save Rate
-    //         $rate = new Rate();
-    //         $form->saveInto($rate);
-    //         $rate->write();
+    public function doRate($data, Form $form){
+        try {
+            //Save Rate
+            $rate = new Rate();
+            $form->saveInto($rate);
+            $rate->write();
 
-    //         //Send mails
-    //         // $config = SiteConfig::current_site_config();
-    //         // $str = $this->parseString($config->ProductEmailContent, $data);
-    //         // $html = new DBHTMLText();
-    //         // $html->setValue($str);
-    //         // $Body = $this->renderWith('Emails/base_email',array('Subject' => $config->ProductEmailSubject, 'Lead' => '', 'Body' => $html, 'Footer' => '', 'SiteConfig' => $config));
-    //         // $email = new Email($config->Email, $data['Email'],$config->ProductEmailSubject, $Body);
-    //         // $email->setBCC($config->Email);
-    //         // $email->send();
-    //         $form->sessionMessage('Vielen Dank ' . $data['Name']."\n".'Ihre Anfrage wurde erfolgreich gesendet', 'success');
+            //Send mails
+            // $config = SiteConfig::current_site_config();
+            // $str = $this->parseString($config->ProductEmailContent, $data);
+            // $html = new DBHTMLText();
+            // $html->setValue($str);
+            // $Body = $this->renderWith('Emails/base_email',array('Subject' => $config->ProductEmailSubject, 'Lead' => '', 'Body' => $html, 'Footer' => '', 'SiteConfig' => $config));
+            // $email = new Email($config->Email, $data['Email'],$config->ProductEmailSubject, $Body);
+            // $email->setBCC($config->Email);
+            // $email->send();
+            $form->sessionMessage('Vielen Dank ' . $data['Name']."\n".'Ihre Anfrage wurde erfolgreich gesendet', 'success');
 
-    //     } catch (ValidationException $e) {
-    //         $validationMessages = '';
-    //         foreach($e->getResult()->getMessages() as $error){
-    //             $validationMessages .= $error['message']."\n";
-    //         }
-    //         $form->sessionMessage($validationMessages, 'bad');
-    //         return $this->redirectBack();
-    //     }
+        } catch (ValidationException $e) {
+            $validationMessages = '';
+            foreach($e->getResult()->getMessages() as $error){
+                $validationMessages .= $error['message']."\n";
+            }
+            $form->sessionMessage($validationMessages, 'bad');
+            return $this->redirectBack();
+        }
 
-    //     return $this->redirectBack();
+        return $this->redirectBack();
 
-    // }
+    }
 }
