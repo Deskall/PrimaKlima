41510d56e4952c47c366ff3301d88f37efff3ba9
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-14 11:20:54 +0200

    Update OverlayContentControllerExtension.php

--- yplay-overlay/src/Extensions/OverlayContentControllerExtension.php
+++ yplay-overlay/src/Extensions/OverlayContentControllerExtension.php
@@ -30,6 +30,7 @@ class OverlayContentControllerExtension extends Extension
     public function onAfterInit(){
         if ($this->owner->Overlay()->exists() && $this->owner->Overlay()->Type == "Bewertung"){
             Requirements::javascript('yplay-overlay/javascript/jquery.rateyo.min.js');
+            Requirements::css('yplay-overlay/css/jquery.rateyo.min.css');
         }
     }
 
