f140bd2ab74fba109bc21b096ce11232ff2dcd54
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-23 22:41:06 +0200

    Update DeskallElementalAreaConfigExtension.php

--- deskall-page-blocks/src/Extensions/DeskallElementalAreaConfigExtension.php
+++ deskall-page-blocks/src/Extensions/DeskallElementalAreaConfigExtension.php
@@ -5,6 +5,10 @@ use SilverStripe\ORM\DataExtension;
 class DeskallElementalAreaConfigExtension extends DataExtension{
 	
 	public function updateConfig(){
+		ob_start();
+					print_r('ici');
+					$result = ob_get_clean();
+					file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
 		$this->owner->addComponent(new GridFieldShowHideAction());
 	}
 }
