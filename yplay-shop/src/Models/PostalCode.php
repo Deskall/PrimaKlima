d081fb2962d068de916592c0d07b68c96bc9a8d8
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-25 15:31:47 +0200

    wq

--- yplay-shop/src/Models/PostalCode.php
+++ yplay-shop/src/Models/PostalCode.php
@@ -45,7 +45,9 @@ class PostalCode extends DataObject {
 	];
 
 	private static $belongs_many_many= [
-	    'Actions' => PriceDiscount::class
+	    'Actions' => PriceDiscount::class,
+	    'Products' => Product::class,
+	    'Packages' => Packages::class
 	];
 
 	private static $default_sort = 'Code';
