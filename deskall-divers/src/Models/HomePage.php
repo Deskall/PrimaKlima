45288dbe8e3dc1426d13cff4da66068cf8c0dcce
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-25 14:01:50 +0200

    Update HomePage.php

--- deskall-divers/src/Models/HomePage.php
+++ deskall-divers/src/Models/HomePage.php
@@ -1,12 +1,19 @@
 <?php
 
+use SilverStripe\Subsites\State\SubsiteState;
+
 class HomePage extends Page {
 	private static $icon_class = "font-icon-home";
 
 	private static $description = "Startseite";
 
 	public function canCreate($member = null, $context = null){
-		return HomePage::get()->count() == 0;
+		if ($this->hasExtension('SilverStripe\Subsites\Extensions\SiteTreeSubsites')){
+			return HomePage::get()->filter('SubsiteID',SubsiteState::singleton()->getSubsiteId())->count() == 0;
+		}
+		else {
+			return HomePage::get()->count() == 0;
+		}
 	}
 
 	public function getCMSFields(){
