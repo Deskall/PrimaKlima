a11da42dd65bf60d9552756bc5781729c7f13efd
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-28 15:42:24 +0200

    Update ProductCategory.php

--- yplay-shop/src/Models/ProductCategory.php
+++ yplay-shop/src/Models/ProductCategory.php
@@ -182,7 +182,7 @@ class ProductCategory extends DataObject {
 	/** Deactivate product slider if cart does not contain the category */
 	public function isDisabled(){
 		ob_start();
-		print_r($this->Title. '------------'."\n");
+		print_r('------------ ' . $this->Title . ' ------------'."\n");
 		$result = ob_get_clean();
 		file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result, FILE_APPEND);
 		$request = Injector::inst()->get(HTTPRequest::class);
@@ -208,7 +208,7 @@ class ProductCategory extends DataObject {
 		$result = ob_get_clean();
 		file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result, FILE_APPEND);
 		ob_start();
-		print_r($this->Title. '------------'."\n");
+		print_r('--------------------------'."\n");
 		$result = ob_get_clean();
 		file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result, FILE_APPEND);
 		return false;
