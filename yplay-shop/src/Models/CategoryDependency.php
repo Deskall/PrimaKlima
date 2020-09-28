b885747474ec878251885ee0cb9fc414ec4f5f58
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-09-28 16:54:32 +0200

    Update CategoryDependency.php

--- yplay-shop/src/Models/CategoryDependency.php
+++ yplay-shop/src/Models/CategoryDependency.php
@@ -63,10 +63,10 @@ class CategoryDependency extends DataObject {
 	}
 
 	public function Concerned(){
-		return ( $this->Codes()->exists() ) ? $this->Codes()->column('Code') : "Alle";
+		return ( $this->Codes()->exists() ) ? $this->Codes()->column('Code')->join('|') : "Alle";
 	}
 
 	public function Excluded(){
-		return ( $this->ExcludedCodes()->exists() ) ? $this->ExcludedCodes()->column('Code') : "Keine";
+		return ( $this->ExcludedCodes()->exists() ) ? $this->ExcludedCodes()->column('Code')->join('|') : "Keine";
 	}
 }
