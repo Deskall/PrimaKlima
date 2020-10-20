1a0cac030c1c47ebc599ed7d85b4565ee6cd2d05
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-10-20 10:43:31 +0200

    Create DeleteOldFormElement.php

---
+++ deskall-page-blocks/src/Tasks/DeleteOldFormElement.php
@@ -0,0 +1,33 @@
+<?php
+
+/* Delete old ElementForm */
+
+use SilverStripe\Dev\BuildTask;
+use DNADesign\ElementalUserForms\Model\ElementForm;
+use SilverStripe\UserForms\Model\Recipient\EmailRecipient;
+use SilverStripe\UserForms\Model\Submission\SubmittedForm;
+
+class DeleteOldFormElement extends BuildTask
+{
+
+    protected $title = 'DeleteOldFormElement';
+
+    protected $description = 'Alle alte Formulare löschen';
+
+    public function run($request)
+    {
+        $count = 0;
+        $newFormsIds = FormBlock::get()->column('ID');
+        $forms = ElementForm::get();
+        if (!empty($newFormsIds)){
+            $forms->exclude('ID',$newFormsIds);
+        }
+        foreach ($forms as $form) {
+            $form->doArchive();
+            $count ++;
+        }
+
+        
+        echo 'Finished deleting ' . $count . ' forms<br>';
+    }
+}
