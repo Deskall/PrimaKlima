e2b805d358e553750cfd5995f9da59468ca01a5a
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-10-20 09:50:36 +0200

    Update MigrateFormElement.php

--- deskall-page-blocks/src/Tasks/MigrateFormElement.php
+++ deskall-page-blocks/src/Tasks/MigrateFormElement.php
@@ -17,47 +17,44 @@ class MigrateFormElement extends BuildTask
     public function run($request)
     {
         $count = 0;
-        foreach (ElementForm::get() as $form) {
-            $form->doArchive();
+        $forms = ElementForm::get();
+        foreach ($forms as $form) {
+            $data = $form->toMap();
+            unset($data['ID']);
+            unset($data['ClassName']);
+            $newForm = new FormBlock($data);
+            if ($newForm->RedirectPageID == 0){
+                $newForm->RedirectPageID = HomePage::get()->first()->ID;
+            }
+            $newForm->write();
+            $fields = $form->fields();
+            $recs = EmailRecipient::get()->filter('FormID',$form->ID);
+            $subms = SubmittedForm::get()->filter('ParentID',$form->ID);
+
+            //Upadte Recipient and Fields and Submissions
+            foreach ($fields as $field) {
+                $field->ParentID = $newForm->ID;
+                $field->ParentClass = $newForm->ClassName;
+                $field->write();
+            }
+            foreach ($recs as $rec) {
+                $rec->FormID = $newForm->ID;
+                $rec->FormClass = $newForm->ClassName;
+                $rec->write();
+            }
+            foreach ($subms as $sub) {
+                $sub->ParentID = $newForm->ID;
+                $sub->ParentClass = $newForm->ClassName;
+                $sub->write();
+            }
+            // $form->delete();
+            // $newForm->publishSingle();
+            //Publish page
+            $newForm->getRealPage()->publishRecursive();
+            $count ++;
         }
-        // $forms = ElementForm::get();
-        // foreach ($forms as $form) {
-        //     $data = $form->toMap();
-        //     unset($data['ID']);
-        //     unset($data['ClassName']);
-        //     $newForm = new FormBlock($data);
-        //     if ($newForm->RedirectPageID == 0){
-        //         $newForm->RedirectPageID = HomePage::get()->first()->ID;
-        //     }
-        //     $newForm->write();
-        //     $fields = $form->fields();
-        //     $recs = EmailRecipient::get()->filter('FormID',$form->ID);
-        //     $subms = SubmittedForm::get()->filter('ParentID',$form->ID);
 
-        //     //Upadte Recipient and Fields and Submissions
-        //     foreach ($fields as $field) {
-        //         $field->ParentID = $newForm->ID;
-        //         $field->ParentClass = $newForm->ClassName;
-        //         $field->write();
-        //     }
-        //     foreach ($recs as $rec) {
-        //         $rec->FormID = $newForm->ID;
-        //         $rec->FormClass = $newForm->ClassName;
-        //         $rec->write();
-        //     }
-        //     foreach ($subms as $sub) {
-        //         $sub->ParentID = $newForm->ID;
-        //         $sub->ParentClass = $newForm->ClassName;
-        //         $sub->write();
-        //     }
-        //     // $form->delete();
-        //     $newForm->publishRecursive();
-        //     //Publish page
-        //     $newForm->getRealPage()->publishRecursive();
-        //     $count ++;
-        // }
-
-        
+        // $forms->removeAll();
         
         echo 'Finished migrating ' . $count . ' forms<br>';
     }
