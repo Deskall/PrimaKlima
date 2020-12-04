3036c22b187ad5c9a96f77a7c41806fafe1f00a4
Guillaume Pacilly <guillaume.pacilly@deskall.ch>
2020-12-04 10:08:49 +0100

    Update SiteConfigLayoutExtension.php

--- deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
+++ deskall-layout/src/Extensions/SiteConfigLayoutExtension.php
@@ -473,12 +473,14 @@ class SiteConfigLayoutExtension extends DataExtension
     if ($this->owner->hasExtension('SilverStripe\Subsites\Extensions\SiteConfigSubsites')){
       if ($this->owner->SubsiteID > 0){
          $fullpath = Director::baseFolder().'/themes/'.$this->owner->Subsite()->Theme.'/css/src/deskall/theme/user_defined.less';
+         ob_start();
+               print_r($fullpath."\n");
+               print_r($fullpath."\n");
+               $result = ob_get_clean();
+               file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
       }
     }
-    ob_start();
-          print_r($fullpath);
-          $result = ob_get_clean();
-          file_put_contents($_SERVER['DOCUMENT_ROOT']."/log.txt", $result);
+
     file_put_contents($fullpath, '// CREATED FROM SILVERSTRIPE LAYOUT CONFIG --- DO NOT DELETE OR MODIFY');
     foreach($this->owner->Colors() as $c){
       if (isset($this->owner->stat('constants_less')[$c->Code])){
